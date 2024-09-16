06cdd83f1e51b154a079edd1877588d7fc59baa3
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 16:38:30 +0800

    Subject: Body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Login } from './pages/Login';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Dashboard } from './pages/Dashboard';
 
 function App() {
 
@@ -11,6 +12,9 @@ function App() {
       <Switch>
         <Route exact path='/'>
           <Login/>
+        </Route>
+        <Route path='/Dashboard'>
+          <Dashboard/>
         </Route>
       </Switch>
       <Login/>
