b2d28ab0eb3b4f4d65a1268903c282b91c35950f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 15:43:51 +0800

    Subject: Body

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,7 +10,7 @@ export const Login = () => {
     return (
         <>
             <Container fluid>
-                
+                <NavLogin/>
             </Container>
                 <NavLogin/>
             <Container lg>
