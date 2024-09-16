b71a6f4142ce479eb7fdccd66e71b255a1e2be6b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 16:37:40 +0800

    Subject: Body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { Login } from './pages/Login';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 function App() {
 
   return (
-    <>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Login/>
+        </Route>
+      </Switch>
       <Login/>
-    </>
+    </Router>
   )
 }
 
