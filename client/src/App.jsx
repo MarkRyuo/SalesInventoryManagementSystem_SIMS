3ff153802ff8c4d58fcd4592142c8bae9f9ef5fe
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 16:43:51 +0800

    Subject: Body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { Login } from './pages/Login';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Dashboard } from './pages/Dashboard';
 
 function App() {
 
   return (
     <Router>
-      <Switch>
-        <Route exact path='/'>
-          <Login/>
-        </Route>
-        <Route path='/Dashboard'>
-          <Dashboard/>
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/Dashboard' element={<Dashboard/>}/>
+      </Routes>
       <Login/>
     </Router>
   )
