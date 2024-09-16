8072aaa98bdece501a0cece5a5ee980aaa84f2fc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 15:43:31 +0800

    Subject: Body

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import Container from 'react-bootstrap/Container';
-import { LoginCard } from '../components/Login Cards/LoginCard';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
