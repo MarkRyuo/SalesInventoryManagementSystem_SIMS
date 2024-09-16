93055b8513e600080e7d58a5d790080dc47d920e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 15:41:57 +0800

    Subject: Body

--- client/src/components/NavBar/NavLogin.jsx
+++ client/src/components/NavBar/NavLogin.jsx
@@ -1,11 +1,20 @@
-
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { BsBox } from "react-icons/bs";
 
 export const NavLogin = () => {
 
     return (
 
         <>
-            
+            <Navbar bg='light'>
+                    <Container>
+                        <Navbar.Brand href="#home">
+                            <BsBox size={50}/>
+                            Sales Inventory Management System
+                        </Navbar.Brand>
+                    </Container>
+                </Navbar>
         </>
 
     )
