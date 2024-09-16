325fb3772f44a087bfa7a422f7e511e3fb43a0fa
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 15:42:49 +0800

    Subject: Body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { Login } from './pages/Login';
 
 function App() {
 
   return (
     <>
-      
+      <Login/>
     </>
   )
 }
