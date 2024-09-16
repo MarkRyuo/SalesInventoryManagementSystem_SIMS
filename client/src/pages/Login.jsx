e3f36a6164490355735383a8e7c1e3e1d924f53b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-16 15:40:43 +0800

    Subject: Body

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import { LoginCard } from '../components/Login Cards/LoginCard';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 
 export const Login = () => {
 
     return (
-
         <>
-            
+            <Container fluid>
+                
+            </Container>
+
+            <Container lg>
+                <Row>
+                    <Col lg={12}>
+                        <LoginCard/>
+                    </Col>
+                    <Col lg={12}>
+                        <Button variant="primary">Log in</Button>
+                    </Col>
+                </Row>
+
+            </Container>
+
         </>
     )
 }
