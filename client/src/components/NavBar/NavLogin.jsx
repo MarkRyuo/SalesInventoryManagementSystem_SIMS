264a8c3a2b7a4d3cf4b13ffab3d34917570af537
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-19 20:45:50 +0800

    Subject: Body

--- client/src/components/NavBar/NavLogin.jsx
+++ client/src/components/NavBar/NavLogin.jsx
@@ -13,9 +13,9 @@ export const NavLogin = () => {
                             <BsBox size={40}/>
                         </Col>
                         <Col style={{paddingTop:5}}>
-                                <Navbar.Brand href="#home" >SIMS</Navbar.Brand>
-                            </Col>
-                        </Row>
+                            <Navbar.Brand href="#home" >SIMS</Navbar.Brand>
+                        </Col>
+                    </Row>
                         <Button variant="outline-primary">Download App</Button>
                     </Container>
             </Navbar>
