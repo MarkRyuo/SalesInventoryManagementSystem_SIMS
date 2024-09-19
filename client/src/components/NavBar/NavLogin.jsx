0d8bf27c55ba0c1dcd2f3641645e8dd8a6692895
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-19 21:22:30 +0800

    Subject: Body

--- client/src/components/NavBar/NavLogin.jsx
+++ client/src/components/NavBar/NavLogin.jsx
@@ -7,7 +7,7 @@ export const NavLogin = () => {
     return (
 
         <>
-            <Container fluid className="pt-2" style={{borderBottom: "1px solid  #f2f2f2"}}>
+            <Container fluid className="py-2" style={{borderBottom: "1px solid  #f2f2f2"}}>
                 <Navbar>
                     <Container>
                         <Row>
