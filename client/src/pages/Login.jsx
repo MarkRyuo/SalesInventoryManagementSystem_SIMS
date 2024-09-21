89364f044d63ce417f7afe62d8627ad112c5881c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-21 22:13:04 +0800

    Subject : Body

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -24,12 +24,8 @@ export const Login = () => {
 
             <Container fluid='lg' className='d-flex justify-content-center text-center'>
                 <Row style={{width: 450}}>
-                    <Col lg={12}>
-                        <RiBox3Fill size={80} />
-                    </Col>
-                    <Col lg={12}>
-                        <p className='lead fs-5'>Sales Inventory Management System</p>
-                    </Col>
+                    <Col lg={12}> <RiBox3Fill size={80} /> </Col>
+                    <Col lg={12}> <p className='lead fs-5'>Sales Inventory Management System</p> </Col>
                     <Col lg={12}>
                         <LoginCard/>
                     </Col>
