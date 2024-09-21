5d4f6879b090ceb0b3bc80b097c4feb0be7faef8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-21 21:56:09 +0800

    Subject : Body

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,9 @@
+
+//* To one-line exporting
+
 export { default as Product } from './Product';
 export { default as Reports } from './Reports';
 export { default as Accounts } from './Accounts';
 export { default as Dashboard } from './Dashboard';
 export { default as Login } from './Login';
+
