51d19c8f5377bd4b2e036790e5a58a5982634bdd
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 21:35:23 +0800

    Subject : Body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 // * Imported Pages
-// import { Login, DashboardPage, Product, Reports, Accounts } from './pages' ;
 import Login from '../src/pages/Login'
 
 
