9f7a05609a4942e745936afd23070d8540c5daa5
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 21:01:13 +0800

    Subject : Body

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,19 +1,19 @@
 import {Container} from 'react-bootstrap' ;
 import { NavDashboard } from '../components/NavBar/NavDashboard';
+import { MainLayout } from '../layout/MainLayout';
 
 export const DashboardPage = () => {
 
     return (
-
-        <Container fluid='lg'>
-            <NavDashboard />
-
+        
+        <MainLayout>
             <Container>
                 {/* Dashboard Components */}
                 <h1>Dashboard</h1>
             </Container>
+        </MainLayout>
 
-        </Container>
+
     )
 }
 
