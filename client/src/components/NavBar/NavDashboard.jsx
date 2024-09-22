6fbbda6c8be8995088104695463d7e02e2b670da
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 16:19:10 +0800

    Subject : Body

--- client/src/components/NavBar/NavDashboard.jsx
+++ client/src/components/NavBar/NavDashboard.jsx
@@ -3,6 +3,10 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from 'react';
 import Navbars from './Navbar.module.css' ;
 
+import { MdDashboardCustomize } from "react-icons/md";
+import { AiOutlineProduct } from "react-icons/ai";
+import { TbReportSearch } from "react-icons/tb";
+import { VscAccount } from "react-icons/vsc";
 
 export const NavDashboard = () => {
 
@@ -36,10 +40,10 @@ export const NavDashboard = () => {
 
                 <Offcanvas.Body>
                     <div className={Navbars.buttonOffcanvas}>
-                        <Button variant="light">Dashboard</Button>
-                        <Button variant="light">Product</Button>
-                        <Button variant="light">Reports</Button>
-                        <Button variant="light">Accounts</Button>
+                        <Button variant="light"><MdDashboardCustomize size={25}/>Dashboard</Button>
+                        <Button variant="light"><AiOutlineProduct size={25}/>Product</Button>
+                        <Button variant="light"> <TbReportSearch size={25}/>Reports</Button>
+                        <Button variant="light"><VscAccount size={20}/>Accounts</Button>
                     </div>
                 </Offcanvas.Body>
             </Offcanvas>
