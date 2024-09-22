10903f061203da7f36605444790c655fa5ef453e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 21:10:56 +0800

    Subject : Body

--- client/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-
-//* To one-line exporting to App.jsx
-
-export { default as Product } from './Product';
-export { default as Reports } from './Reports';
-export { default as Accounts } from './Accounts';
-export { default as Dashboard } from './DashboardPage';
-export { default as Login } from './Login';
-
