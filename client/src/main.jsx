1db257cdf33b60342d3c4cb611fe04eb83b675c2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 20:31:14 +0800

    Subject : Body

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import DashboardPage from './pages/DashboardPage.jsx';
 
 import {
   createBrowserRouter,
@@ -16,7 +17,7 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "Dashboard",
+    path: "DashboardPage",
     element: <DashboardPage />,
   },
 ]);
