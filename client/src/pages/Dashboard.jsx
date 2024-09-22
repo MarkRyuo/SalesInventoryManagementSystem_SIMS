58d8226b621b4e74fed89e73737c657f7619c246
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 15:34:10 +0800

    Subject : Body

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,13 +1,14 @@
 import {Container} from 'react-bootstrap' ;
 import {Routes, Route, Outlet} from 'react-router-dom'
 import { Product, Reports, Accounts } from 'pages' ; 
+import { NavDashboard } from '../components/NavBar/NavDashboard';
 
 export const Dashboard = () => {
 
     return (
 
         <Container>
-            
+            <NavDashboard/>
             <Routes>
                 <Route path=' Product' element={<Product />}/>
                 <Route path=' Reports' element={<Reports />}/>
