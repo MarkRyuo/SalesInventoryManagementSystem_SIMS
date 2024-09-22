92c9db527a6af364ab825bd7b629815d847e96fc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 21:21:33 +0800

    Subject : Body

--- client/src/components/NavBar/Buttons.jsx
+++ client/src/components/NavBar/Buttons.jsx
@@ -11,9 +11,9 @@ export const Buttons = () => {
     return (
         <>
             <Button as={Link} to="/Dashboard" variant="light"><MdDashboardCustomize size={25}/>Dashboard</Button>
-            <Button as={Link} to=" Product" variant="light"><AiOutlineProduct size={25}/>Product</Button>
-            <Button as={Link} to=" Reports" variant="light"> <TbReportSearch size={25}/>Reports</Button>
-            <Button as={Link} to=" Accounts" variant="light"><VscAccount size={20}/>Accounts</Button>
+            <Button as={Link} to="/Product" variant="light"><AiOutlineProduct size={25}/>Product</Button>
+            <Button as={Link} to="/Reports" variant="light"> <TbReportSearch size={25}/>Reports</Button>
+            <Button as={Link} to="/Accounts" variant="light"><VscAccount size={20}/>Accounts</Button>
         </>
     )
 }
