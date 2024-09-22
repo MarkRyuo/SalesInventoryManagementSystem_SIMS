582f0c304481eaff089fdc2aa8e1dd5f267b6025
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 16:37:03 +0800

    Subject : Body

--- client/src/components/NavBar/Buttons.jsx
+++ client/src/components/NavBar/Buttons.jsx
@@ -13,10 +13,10 @@ export const Buttons = () => {
     const [dashboard, setDashboard] = useState() ;
 
     const handleDashboard = () => 
-
+            setDashboard("/")
     return (
         <>
-            <Button variant="light"><MdDashboardCustomize size={25}/>Dashboard</Button>
+            <Button variant="light"><MdDashboardCustomize size={25} onClick={handleDashboard()}/>Dashboard</Button>
             <Button variant="light"><AiOutlineProduct size={25}/>Product</Button>
             <Button variant="light"> <TbReportSearch size={25}/>Reports</Button>
             <Button variant="light"><VscAccount size={20}/>Accounts</Button>
