8ea108617b5b25b3310aa4607b4fdad868040778
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 16:41:49 +0800

    Subject : Body

--- client/src/components/NavBar/Buttons.jsx
+++ client/src/components/NavBar/Buttons.jsx
@@ -3,20 +3,16 @@ import { AiOutlineProduct } from "react-icons/ai";
 import { TbReportSearch } from "react-icons/tb";
 import { VscAccount } from "react-icons/vsc";
 
-import { useState } from 'react' ;
+import { Link } from 'react-router-dom' ;
 
 
 import { Button } from 'react-bootstrap';
 
 export const Buttons = () => {
 
-    const [dashboard, setDashboard] = useState() ;
-
-    const handleDashboard = () => 
-            setDashboard("/")
     return (
         <>
-            <Button variant="light"><MdDashboardCustomize size={25} onClick={handleDashboard()}/>Dashboard</Button>
+            <Button variant="light"><MdDashboardCustomize size={25}/>Dashboard</Button>
             <Button variant="light"><AiOutlineProduct size={25}/>Product</Button>
             <Button variant="light"> <TbReportSearch size={25}/>Reports</Button>
             <Button variant="light"><VscAccount size={20}/>Accounts</Button>
