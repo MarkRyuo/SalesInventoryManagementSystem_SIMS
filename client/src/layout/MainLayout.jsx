f219f02fd30ac65df2cfb3283be2d13534f58175
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 20:57:08 +0800

    Subject : Body

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,10 +1,15 @@
-
+import { NavDashboard } from "../components/NavBar/NavDashboard"
+import { Container } from "react-bootstrap"
 
 export const MainLayout = () => {
 
     return (
-        <>
+        <Container fluid>
             
-        </>
+            <div className="mainContainer">
+                <NavDashboard />
+            </div>
+
+        </Container>
     )
 }
