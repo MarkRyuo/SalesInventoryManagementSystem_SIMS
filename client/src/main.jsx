ee7116a159d5a21b1af0d578046cddf9fc31fa04
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 20:19:20 +0800

    Subject : Body

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,26 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
