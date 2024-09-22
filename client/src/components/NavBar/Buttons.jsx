2140df94e0148377c0cfc62517ec0ae49343ddc9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 16:27:06 +0800

    Subject : Body

---
+++ client/src/components/NavBar/Buttons.jsx
@@ -0,0 +1,13 @@
+
+
+export const Buttons = () => {
+
+    return (
+        <>
+            <Button variant="light"><MdDashboardCustomize size={25}/>Dashboard</Button>
+            <Button variant="light"><AiOutlineProduct size={25}/>Product</Button>
+            <Button variant="light"> <TbReportSearch size={25}/>Reports</Button>
+            <Button variant="light"><VscAccount size={20}/>Accounts</Button>
+        </>
+    )
+}
