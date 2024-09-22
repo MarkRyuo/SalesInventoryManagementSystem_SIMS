48c06623fb0e00033412b6baa0af59d1150a01c6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-22 21:28:17 +0800

    Subject : Body

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,13 +1,13 @@
-
+import { MainLayout }
 
 //* Accounts Page
 
 export const AccountPage = () => {
 
     return (
-        <>
+        <MainLayout>
             <h1>AccountPage Dashboard</h1>
-        </>
+        </MainLayout>
     )
 }
 
