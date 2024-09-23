6966d9459b6486d66ed02d85797d8f3f7f103a14
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-23 13:29:07 +0800

    Subject: Body

--- client/src/components/Dashboard/Charts.jsx
+++ client/src/components/Dashboard/Charts.jsx
@@ -6,17 +6,17 @@ export const Charts = () => {
     return (
         <>
             <div className="mainChart">
-                <Row>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "6px"}}>
+                <Row className="gap-2">
+                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px"}}>
                         Col-1
                     </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "6px"}}>
+                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px"}}>
                         Col-2
                     </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "6px"}}>
+                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", marginRight: "2px"}}>
                         Col-3
                     </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "6px"}}>
+                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", marginRight: "2px"}}>
                         Col-4
                     </Col>
                 </Row>
