6dfbd5608c3cbe458df325efa9501dfe534b7e6e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-23 13:23:23 +0800

    Subject: Body

--- client/src/components/Dashboard/Charts.jsx
+++ client/src/components/Dashboard/Charts.jsx
@@ -7,16 +7,16 @@ export const Charts = () => {
         <>
             <div className="mainChart">
                 <Row>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "5px"}}>
+                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "5px", width: "270px"}}>
                         Col-1
                     </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "5px"}}>
+                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "5px", width: "270px"}}>
                         Col-2
                     </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "5px"}}>
+                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "5px", width: "270px"}}>
                         Col-3
                     </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "5px"}}>
+                    <Col lg={3} md={6} style={{border: "1px solid", height: "200px", margin: "5px", width: "270px"}}>
                         Col-4
                     </Col>
                 </Row>
