17703ff2e39ab7f7ac5849b64cb21ac6777c94db
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-25 22:42:10 +0800

    Subject : Body

--- client/src/components/Dashboard/Charts.jsx
+++ client/src/components/Dashboard/Charts.jsx
@@ -4,12 +4,10 @@
 
 export const Charts = ({ChartName}) => {
 
-    const ChartName = props.ChartName
-
     return (
         <>
             <div className="mainChart">
-                <div style={{border: " 1px solid"}}>{ChartName}</div>
+                <div style={{border: ""}}>{ChartName}</div>
             </div>
         </>
     )
