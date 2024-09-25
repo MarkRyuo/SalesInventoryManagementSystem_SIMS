e1db80564005c805c7bd221fd84883e92934c1dc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-25 21:49:17 +0800

    Subject : Body

--- client/src/components/Dashboard/Charts.jsx
+++ client/src/components/Dashboard/Charts.jsx
@@ -1,4 +1,3 @@
-import { Row, Col } from "react-bootstrap"
 
 
 export const Charts = () => {
@@ -6,20 +5,7 @@ export const Charts = () => {
     return (
         <>
             <div className="mainChart">
-                <Row>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "150px"}}>
-                        Col-1
-                    </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "150px"}}>
-                        Col-2
-                    </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "150px"}}>
-                        Col-3
-                    </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "150px"}}>
-                        Col-4
-                    </Col>
-                </Row>
+                
             </div>
         </>
     )
