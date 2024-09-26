3f6c06ef7cc98c962e47faf9728f981ceba4b1bb
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-26 20:05:05 +0800

    Subject : Body

--- client/src/components/Dashboard/BigCharts.jsx
+++ client/src/components/Dashboard/BigCharts.jsx
@@ -4,7 +4,7 @@
 
 function BigCharts() {
     return (
-        <div style={{border: "1px solid red", height: 500, marginTop: 10}}>
+        <div style={{border: "1px solid red", height: 400, marginTop: 10}}>
         </div>
     )
 }
--- client/src/components/Dashboard/Charts.jsx
+++ client/src/components/Dashboard/Charts.jsx
@@ -12,20 +12,6 @@ export const Charts = ({ChartNumber}) => {
                 <div style={{border: "1px solid", height: 150}}>
                     {ChartNumber}
                 </div>
-                {/* <Row>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "150px"}}>
-                        Col-1
-                    </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "150px"}}>
-                        Col-2
-                    </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "150px"}}>
-                        Col-3
-                    </Col>
-                    <Col lg={3} md={6} style={{border: "1px solid", height: "150px"}}>
-                        Col-4
-                    </Col>
-                </Row> */}
             </div>
         </>
     )
