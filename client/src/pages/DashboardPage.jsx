799fa808aeee8a342299b3a92187c13ecaa20570
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-26 13:43:49 +0800

    Subject : Body

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -16,7 +16,7 @@ export const DashboardPage = () => {
                 {/* Dashboard Components */}
                 <h1>Dashboard</h1>
                 <div>
-                    <Charts ChartData={}/>
+                    <Charts ChartName={...CharData.ChartName}/>
                     <Row>
                         <Col lg={8} sm={12}>
                             <BigCharts />
