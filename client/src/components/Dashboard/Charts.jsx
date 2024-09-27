4148bf038d7d7f6637b060cbce176ba9eff1e3bf
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-27 18:15:40 +0800

    Subject : Body

--- client/src/components/Dashboard/Charts.jsx
+++ client/src/components/Dashboard/Charts.jsx
@@ -1,4 +1,5 @@
 import { Row, Col } from "react-bootstrap"
+import Chart1 from "./Chartcomp/Chart1"
 
 
 
@@ -10,7 +11,7 @@ export const Charts = () => {
             <div className="mainChart">
                 <Row>
                     <Col>
-                        
+                        <Chart1 />
                     </Col>
                 </Row>
             </div>
