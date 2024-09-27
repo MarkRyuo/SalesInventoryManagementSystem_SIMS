3adfd58935681c06ddfac6722e5381f9c423b3fc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-27 21:26:07 +0800

    Subject : Body

---
+++ client/src/components/Dashboard/Chartcomp/Chart4.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Chart4() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Chart4
--- client/src/components/Dashboard/Chartcomp/Chart5.jsx
+++ client/src/components/Dashboard/Chartcomp/Chart5.jsx
@@ -1,8 +1,5 @@
 
 
-
-import React from 'react'
-
 function Chart5() {
     return (
         <div>
