0d9977e6517a3fa90ef50fcd02c9e7de95b84b82
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-27 22:04:10 +0800

    Subject : Body

--- client/src/components/Dashboard/BigCharts.jsx
+++ client/src/components/Dashboard/BigCharts.jsx
@@ -10,11 +10,11 @@ import { Row, Col } from 'react-bootstrap';
 function BigCharts() {
     return (
         <>
-                <Row className='gap-4'>
+                <Row className=' d-flex justify-content-between'>
                     <Col lg={7} className={DashStyle.colChartlg}>
                         <Chart4 />
                     </Col>
-                    <Col lg={4} className={DashStyle.colChartlg}>
+                    <Col lg={5} className={DashStyle.colChartlg}>
                         <Chart5 />
                     </Col>
                 </Row>
