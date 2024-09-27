6472c6674c7ab1957bc76681c23aecc488cfdfdc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-27 18:15:19 +0800

    Subject : Body

--- client/src/components/Dashboard/Chartcomp/Chart1.jsx
+++ client/src/components/Dashboard/Chartcomp/Chart1.jsx
@@ -5,10 +5,10 @@ function Chart1() {
     return (
         <div className="MainChart1">
             <div className="contentChart1">
-                
+
             </div>
         </div>
     )
 }
 
-export default Chart1
+export default Chart1 ;
