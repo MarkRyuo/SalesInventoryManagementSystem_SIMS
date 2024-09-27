a334b0a9ae54114b612c2f73e70290db83aa9699
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-27 21:34:01 +0800

    Subject : Body

--- client/src/components/Dashboard/BigCharts.jsx
+++ client/src/components/Dashboard/BigCharts.jsx
@@ -1,4 +1,5 @@
 import Chart4 from './Chartcomp/Chart4';
+import Chart5 from './Chartcomp/Chart5';
 import DashStyle from './Dashboard.module.css' ;
 
 import { Container, Row, Col } from 'react-bootstrap';
@@ -14,7 +15,7 @@ function BigCharts() {
                     <Chart4 />
                 </Col>
                 <Col>
-
+                    <Chart5 />
                 </Col>
             </Row>
         </Container>
