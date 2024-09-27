5470b82e02f4c54e3237f3fdbbc3cbbb55893701
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-27 21:32:53 +0800

    Subject : Body

--- client/src/components/Dashboard/BigCharts.jsx
+++ client/src/components/Dashboard/BigCharts.jsx
@@ -1,5 +1,7 @@
 import DashStyle from './Dashboard.module.css' ;
 
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 
 
@@ -8,7 +10,7 @@ function BigCharts() {
         <Container>
             <Row>
                 <Col>
-                    
+
                 </Col>
             </Row>
         </Container>
