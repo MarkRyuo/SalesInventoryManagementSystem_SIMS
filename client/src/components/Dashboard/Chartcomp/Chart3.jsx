1fea14a3dabdc374d59543212f2d5728f07ff49f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-27 18:19:23 +0800

    Subject : Body

--- client/src/components/Dashboard/Chartcomp/Chart3.jsx
+++ client/src/components/Dashboard/Chartcomp/Chart3.jsx
@@ -2,11 +2,11 @@
 
 function Chart3() {
     return (
-        <div className="mainChart3">
+        <>
             <div className="contentChart3">
 
             </div>
-        </div>
+        </>
     )
 }
 
