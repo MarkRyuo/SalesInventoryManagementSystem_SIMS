f40312357b6fbe4b6928849352a61a3d5e89b720
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-27 21:32:36 +0800

    Subject : Body

--- client/src/components/Dashboard/BigCharts.jsx
+++ client/src/components/Dashboard/BigCharts.jsx
@@ -5,8 +5,13 @@ import DashStyle from './Dashboard.module.css' ;
 
 function BigCharts() {
     return (
-        <div style={{border: "1px solid red", height: 400, marginTop: 10}}>
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
