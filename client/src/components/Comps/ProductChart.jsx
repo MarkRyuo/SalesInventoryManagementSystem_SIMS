4ac1e0354c6771b2c1d374887ef7031245265c9f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-28 15:21:29 +0800

    Subject : Body

--- client/src/components/Comps/ProductChart.jsx
+++ client/src/components/Comps/ProductChart.jsx
@@ -1,11 +1,15 @@
-
+import { Row, col } from "react-bootstrap"
 
 function ProductChart() {
     return (
         <>
-            <div style={{width: "100%", height: "400px", overflow: "auto", border: "1px solid", maxWidth: "500px"}}>
-                Scrollable
-            </div>
+            <Row>
+                <Col>
+                    <div style={{ width: "100%", height: "400px", overflow: "auto", border: "1px solid", maxWidth: "500px" }}>
+                        Scrollable
+                    </div>
+                </Col>
+            </Row>
         </>
     )
 }
