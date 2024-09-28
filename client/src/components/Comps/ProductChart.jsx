f625717406a370c7845f7927f3359dc386054f69
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-28 15:30:15 +0800

    Subject : Body

--- client/src/components/Comps/ProductChart.jsx
+++ client/src/components/Comps/ProductChart.jsx
@@ -4,8 +4,9 @@ function ProductChart() {
     return (
         <>
             <Row className="mt-3">
-                <Col lg={12} style={{border: "1px solid"}}>
-                    Scrollable
+                <Col lg={12} style={{border: "1px solid", height: "300px"}}>
+                    Scrollable 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non sodales enim. Phasellus aliquam, ipsum in faucibus vehicula, justo velit tincidunt tortor, nec efficitur est felis at dui. Maecenas volutpat, turpis vel convallis fermentum, lectus eros vehicula risus, nec tincidunt nulla mauris vitae odio. Curabitur mollis sem sit amet ante ultrices, non ultricies justo laoreet. Nam ut magna tincidunt, egestas mauris id, convallis sapien. Integer vulputate dapibus neque, nec porttitor odio. Nullam non sem nec eros tincidunt laoreet id a lorem. Aenean interdum pretium mauris, sit amet mollis magna vestibulum in. Duis tempor dictum purus, non accumsan elit. Nam rhoncus lacus et sapien euismod, sed tempor magna suscipit. Vestibulum ut urna ac lorem tristique rhoncus id ut mauris. Integer eget magna nec justo ullamcorper vehicula nec eget lacus. Vestibulum sodales sapien nec lectus luctus, in fringilla ipsum viverra. Pellentesque ac varius turpis, at venenatis est.
                 </Col>
             </Row>
         </>
