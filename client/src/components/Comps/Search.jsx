bc363b4f2c1b9ddcff26e72a05c627bb93c87a13
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-28 11:32:26 +0800

    Subject : Body

--- client/src/components/Comps/Search.jsx
+++ client/src/components/Comps/Search.jsx
@@ -4,7 +4,7 @@ import { IoSearch } from "react-icons/io5";
 function Search() {
     return (
         <>
-            <Form >
+            <Form inline>
                 <Row>
                     <Col xs="auto">
                         <Form.Control
@@ -14,7 +14,7 @@ function Search() {
                         />
                     </Col>
                     <Col xs="auto">
-                        <Button type="submit"><IoSearch /></Button>
+                        <Button type="submit" variant="primary"><IoSearch /></Button>
                     </Col>
                 </Row>
             </Form>
