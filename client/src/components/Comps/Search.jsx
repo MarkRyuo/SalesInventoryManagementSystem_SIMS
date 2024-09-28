d3b842d4449bde7722e47656effec09d9f0a9803
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-28 11:28:15 +0800

    Subject : Body

--- client/src/components/Comps/Search.jsx
+++ client/src/components/Comps/Search.jsx
@@ -4,7 +4,20 @@ import { Form, Row, Col, Button } from "react-bootstrap";
 function Search() {
     return (
         <>
-
+            <Form inline>
+                <Row>
+                    <Col xs="auto">
+                        <Form.Control
+                            type="text"
+                            placeholder="Search"
+                            className=" mr-sm-2"
+                        />
+                    </Col>
+                    <Col xs="auto">
+                        <Button type="submit">Submit</Button>
+                    </Col>
+                </Row>
+            </Form>
         </>
     )
 }
