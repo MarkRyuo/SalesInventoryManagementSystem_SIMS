ee9873953cc9f722541063747cda215a53f937ff
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-28 15:16:06 +0800

    Subject : Body

--- client/src/components/Comps/ProductChart.jsx
+++ client/src/components/Comps/ProductChart.jsx
@@ -3,7 +3,7 @@
 function ProductChart() {
     return (
         <>
-            <div style={{width: "100%", height: "400px", overflow: "auto", border: "1px solid", maxWidth: "500px"}}>
+            <div style={{, height: "400px", overflow: "auto", border: "1px solid", maxWidth: "500px"}}>
                 Scrollable
             </div>
         </>
