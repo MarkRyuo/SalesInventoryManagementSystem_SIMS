2f96f1bb429e60d3639fc9be112ca0387b3443cb
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-28 15:42:53 +0800

    Subject : Body

--- client/src/components/Comps/ProductChart.jsx
+++ client/src/components/Comps/ProductChart.jsx
@@ -5,8 +5,9 @@ function ProductChart() {
     return (
         <>
             <Row className="mt-3">
-                <Col lg={12}>
-                    Scrollable........
+                <Col lg={12} style={{border: "1px solid", height: "500px", overflow: "auto", width: '100%'}}>
+                    Scrollable.......
+
                 </Col>
             </Row>
         </>
