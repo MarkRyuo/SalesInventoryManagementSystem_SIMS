9769da8995abc14c7e4d92aca5b1f2493b7a833a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-28 11:30:56 +0800

    Subject : Body

--- client/src/components/Comps/Search.jsx
+++ client/src/components/Comps/Search.jsx
@@ -1,20 +1,20 @@
 import { Form, Row, Col, Button } from "react-bootstrap";
-
+import { IoSearch } from "react-icons/io5";
 
 function Search() {
     return (
         <>
-            <Form inline>
+            <Form >
                 <Row>
                     <Col xs="auto">
                         <Form.Control
                             type="text"
-                            placeholder="Search"
+                            placeholder="Search Products"
                             className=" mr-sm-2"
                         />
                     </Col>
                     <Col xs="auto">
-                        <Button type="submit">Submit</Button>
+                        <Button type="submit"><IoSearch /></Button>
                     </Col>
                 </Row>
             </Form>
