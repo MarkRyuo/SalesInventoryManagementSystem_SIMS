60edbe719219f04d0df56eb1bc486fa678d95a1c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-28 15:26:32 +0800

    Subject : Body

--- client/src/components/Comps/ProductChart.jsx
+++ client/src/components/Comps/ProductChart.jsx
@@ -3,7 +3,7 @@ import { Row, Col } from "react-bootstrap"
 function ProductChart() {
     return (
         <>
-            <Row className="">
+            <Row className="mt-3">
                 <Col lg={12} md={12} style={{border: "1px solid"}}>
                     Scrollable
                 </Col>
