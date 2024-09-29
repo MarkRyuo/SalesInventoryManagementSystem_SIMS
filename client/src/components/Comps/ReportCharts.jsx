671b0a952af61a2e1c48545707f95ff355274315
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-29 15:51:13 +0800

    Subject : Body

--- client/src/components/Comps/ReportCharts.jsx
+++ client/src/components/Comps/ReportCharts.jsx
@@ -11,7 +11,7 @@ const ReportCharts = () => {
     return (
         <>
             <div className="contentChart">
-                <p className="fs-4">Test</p>
+                <p className="fs-4">{title}</p>
                 <p className="fs-3">1000</p>
             </div>
         </>
