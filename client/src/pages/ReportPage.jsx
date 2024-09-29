7ccffa75b17fa36e09169e1ce752f3d949db575b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-29 13:29:51 +0800

    Subject : Body

--- client/src/pages/ReportPage.jsx
+++ client/src/pages/ReportPage.jsx
@@ -10,7 +10,7 @@ export const ReportPage = () => {
     return (
         <MainLayout>
             <Container fluid='lg'>
-                <p><span></span> Report Page Dashboard</p>
+                <p><span><TbReport /></span> Report Page Dashboard</p>
             </Container>
         </MainLayout>
     )
