76093f437f22deba2bbe2d19e8502854dc0af215
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-30 16:18:19 +0800

    Subject : Body

--- client/src/components/Comps/ReportCharts.jsx
+++ client/src/components/Comps/ReportCharts.jsx
@@ -10,7 +10,7 @@ const ReportCharts = ({reports}) => {
             <div>
                 {reports.map((report) => (
                     <div key={report.id} className="contentReport">  {/*Parent */}
-                        <p>{report.title}</p> {/* Childs */}
+                        <p>{report.title}</p> {/* Child */}
                         <p>{report.total}</p>
                     </div>
                 ))}
