9d2601dd74212daf6323be7756077ba8aee04ac9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-30 14:35:06 +0800

    Subject : Body

--- client/src/pages/ReportPage.jsx
+++ client/src/pages/ReportPage.jsx
@@ -11,10 +11,7 @@ import Reportcss from './Css/Report.module.css' ;
 export const ReportPage = () => {
 
     const Reportobj = {
-        Revenue : {
-            title: "Total Revenue",
-            total: "21"
-        }
+        Revenue : {title: "Total Revenue",total: "21"},
     }
 
     return (
