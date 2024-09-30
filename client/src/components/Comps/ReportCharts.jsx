17e9645a2f65566e9f1bd304071a7687b8da0773
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-30 14:39:39 +0800

    Subject : Body

--- client/src/components/Comps/ReportCharts.jsx
+++ client/src/components/Comps/ReportCharts.jsx
@@ -3,14 +3,15 @@
 
 //* Reusable, Props 
 
-const ReportCharts = ({className}) => {
+const ReportCharts = ({title, total}) => {
     
     return (
         <>
             <div>
                 {Reportobj.map((reports) => (
                     <div key={reports.id} className="contentReport">
-
+                        <p>{title}</p>
+                        <p>{total}</p>
                     </div>
                 ))}
             </div>
