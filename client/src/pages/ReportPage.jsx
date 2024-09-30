1fcff127907fc696c30131ecd1a4aefe967e0f04
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-30 19:06:48 +0800

    Subject : Body

--- client/src/pages/ReportPage.jsx
+++ client/src/pages/ReportPage.jsx
@@ -1,9 +1,9 @@
 import ReportCharts from '../components/Comps/ReportCharts';
-import { MainLayout } from '../layout/MainLayout' ;
+import { MainLayout } from '../layout/MainLayout';
 import { TbReport } from "react-icons/tb";
 import { Row, Col } from 'react-bootstrap';
 
-import Reportcss from './Css/Report.module.css' ;
+import Reportcss from './Css/Report.module.css';
 import { useState } from 'react';
 
 
@@ -14,28 +14,31 @@ export const ReportPage = () => {
     //* need Unique id 
     const [reports, setReport] = useState([
         { title: "Total Revenue", total: "$1000", id: 1 },
-        { title: "Total Units Sold", total: "500 units", id: 2},
+        { title: "Total Units Sold", total: "500 units", id: 2 },
         { title: "No. of Transactions", total: "2000 Transaction", id: 3 },
-    ]) ;
+    ]);
 
 
     return (
         <MainLayout>
 
-                <p className='fs-4'><span><TbReport /></span> Sales Report</p>
-                
-                <div className='contentReport' style={{border: "1px solid", height: "800px"}}>
-                    <Row className='rowReport'>
-                        <Col lg={4} className={Reportcss.colReport}>
-                            <ReportCharts reports={reports.filter((report) => report.title === "Total Revenue") }/>  {/* Create module css here */}
-                        </Col>
-                        <Col lg={4}>
-                            <ReportCharts reports={reports.filter((report) => report.title === "Total Units Sold")} />
-                        </Col>
-                    </Row>
-                </div>
+            <p className='fs-4'><span><TbReport /></span> Sales Report</p>
+
+            <div className='contentReport' style={{ border: "1px solid", height: "800px" }}>
+                <Row className='rowReport'>
+                    <Col lg={4} className={Reportcss.colReport}>
+                        <ReportCharts reports={reports.filter((report) => report.title === "Total Revenue")} />  {/* Create module css here */}
+                    </Col>
+                    <Col lg={4}>
+                        <ReportCharts reports={reports.filter((report) => report.title === "Total Units Sold")} />
+                    </Col>
+                    <Col lg={4}>
+                        <ReportCharts reports={reports.filter((report) => report.title === "Total Units Sold")} />
+                    </Col>
+                </Row>
+            </div>
         </MainLayout>
     )
 }
 
-export default ReportPage ;
+export default ReportPage;
