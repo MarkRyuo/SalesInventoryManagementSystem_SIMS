2bb5ce6aedf771f5333b98cd436129a7951eb83c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-01 23:22:26 +0800

    Subject: Body

--- client/src/components/Account/AccountComp.jsx
+++ client/src/components/Account/AccountComp.jsx
@@ -6,12 +6,12 @@ import Dropdown from 'react-bootstrap/Dropdown';
 const AccountComp = () => {
 
     return ( 
-        <div className="contentAccount" style={{border: "1px solid"}}>
+        <div className="contentAccount" style={{border: "1px solid", width: "500px", height: "500px"}}>
             <p>New Staff</p>
             <p>Details</p>
 
-            <Form.Control size="lg" type="text" placeholder="FullName" />
-            <Form.Control size="lg" type="text" placeholder="Username" />
+            <Form.Control  type="text" placeholder="FullName" />
+            <Form.Control  type="text" placeholder="Username" />
             <Form.Control size="lg" type="password" placeholder="Password" />
             <Form.Control size="lg" type="text" placeholder="FullName" />
 
