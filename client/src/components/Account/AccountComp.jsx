ba9692f702c844d88a7cd3167af83b7f57333aee
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-01 23:17:05 +0800

    Subject: Body

--- client/src/components/Account/AccountComp.jsx
+++ client/src/components/Account/AccountComp.jsx
@@ -1,10 +1,14 @@
-
+import Form from 'react-bootstrap/Form';
 
 const AccountComp = () => {
 
     return ( 
         <div className="contentAccount">
-            
+            <p>New Staff</p>
+            <p>Details</p>
+
+            <Form.Control size="lg" type="text" placeholder="Fullname" />
+
         </div>
     );
 }
