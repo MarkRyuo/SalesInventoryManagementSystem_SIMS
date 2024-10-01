d1c46f7bd78e3f3ea92d80f3d21adde3c9c47795
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-01 23:18:43 +0800

    Subject: Body

--- client/src/components/Account/AccountComp.jsx
+++ client/src/components/Account/AccountComp.jsx
@@ -1,4 +1,7 @@
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const AccountComp = () => {
 
@@ -7,7 +10,22 @@ const AccountComp = () => {
             <p>New Staff</p>
             <p>Details</p>
 
-            <Form.Control size="lg" type="text" placeholder="Fullname" />
+            <Form.Control size="lg" type="text" placeholder="FullName" />
+            <Form.Control size="lg" type="text" placeholder="Username" />
+            <Form.Control size="lg" type="password" placeholder="Password" />
+            <Form.Control size="lg" type="text" placeholder="FullName" />
+
+            <Dropdown as={ButtonGroup}>
+                <Button variant="success">Split Button</Button>
+
+                <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
 
         </div>
     );
