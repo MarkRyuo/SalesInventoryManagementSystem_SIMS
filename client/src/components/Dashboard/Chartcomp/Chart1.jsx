0b97eff051a8c6d807cd69f55f6f6117d6b41d42
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-02 22:39:06 +0800

    Subject: Body

--- client/src/components/Dashboard/Chartcomp/Chart1.jsx
+++ client/src/components/Dashboard/Chartcomp/Chart1.jsx
@@ -6,7 +6,7 @@ function Chart1({charts}) {
     return (
         <div className="mainContent">
             {charts.map((chart) => (
-                <div className="contentChart" key={chart.id}>
+                <div className="contentChart" key={chart.id} style={{border: "1px solid", textAlign:"center", height: "150px", alignContent: "center"}}>
                     <p className="fs-4">{chart.ChartTitle}</p>
                     <p className="fs-5">{chart.ChartNumber}</p>
                 </div>
