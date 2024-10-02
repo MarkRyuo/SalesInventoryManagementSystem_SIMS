7e83c5ff10b2c8daa48fc080165aaa3c798f6f31
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-02 22:27:11 +0800

    Subject: Body

--- client/src/components/Dashboard/Chartcomp/Chart1.jsx
+++ client/src/components/Dashboard/Chartcomp/Chart1.jsx
@@ -13,7 +13,9 @@ function Chart1() {
     return (
         <div className="contentChart1">
             {charts.map((chart) => (
-                
+                <div>
+                    
+                </div>
             ))}
         </div>
     )
