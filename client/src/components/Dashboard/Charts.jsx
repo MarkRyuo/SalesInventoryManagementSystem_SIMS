58164037140f28d6acdbe801b9c55bc4efbffd5d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-02 22:29:47 +0800

    Subject: Body

--- client/src/components/Dashboard/Charts.jsx
+++ client/src/components/Dashboard/Charts.jsx
@@ -9,6 +9,12 @@ import DashStyle from './Dashboard.module.css'
 //* Prop Parent
 export const Charts = () => {
 
+    const [charts, SetCharts] = useState([
+        { ChartTitle: "Chart1", ChartNumber: "00000", id: "C-1" },
+        { ChartTitle: "Chart2", ChartNumber: "00000", id: "C-2" },
+        { ChartTitle: "Chart3", ChartNumber: "00000", id: "C-3" },
+
+    ])
 
     return (
         <>
