92b461db10adac778d331276826aefae514cb1cf
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-02 22:20:48 +0800

    Subject: Body

--- client/src/components/Dashboard/Chartcomp/Chart1.jsx
+++ client/src/components/Dashboard/Chartcomp/Chart1.jsx
@@ -2,11 +2,12 @@
 
 
 function Chart1() {
+    
     return (
         <>
             <div className="contentChart1">
-                Chart1
-                {/* Insert Logic */}
+                
+                
             </div>
         </>
     )
