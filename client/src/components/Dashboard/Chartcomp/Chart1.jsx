d6637c61112f6cfce28d403b7156951e361e7c98
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-02 22:24:22 +0800

    Subject: Body

--- client/src/components/Dashboard/Chartcomp/Chart1.jsx
+++ client/src/components/Dashboard/Chartcomp/Chart1.jsx
@@ -4,7 +4,10 @@ import { useState } from "react";
 function Chart1() {
 
     const [Chart, SetChart] = useState([
-        {ChartTitle: "Chart1", ChartNumber: "00000", id: "C-1"}
+        { ChartTitle: "Chart1", ChartNumber: "00000", id: "C-1" },
+        { ChartTitle: "Chart2", ChartNumber: "00000", id: "C-2" },
+        { ChartTitle: "Chart1", ChartNumber: "00000", id: "C-1" },
+
     ])
 
     return (
