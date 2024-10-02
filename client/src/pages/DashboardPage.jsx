d3655e13fde03bab66fb3e9a87d0772505001ab3
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-02 22:19:29 +0800

    Subject: Body

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import {Container, Row, Col} from 'react-bootstrap' ;
+import {Container, Row} from 'react-bootstrap' ;
 import { MainLayout } from '../layout/MainLayout';
 import { Charts } from '../components/Dashboard/Charts';
 import BigCharts from '../components/Dashboard/BigCharts';
