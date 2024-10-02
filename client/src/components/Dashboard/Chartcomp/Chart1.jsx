8d5272c9cb065771a44422c8dbf3464fac3d1506
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-02 22:24:30 +0800

    Subject: Body

--- client/src/components/Dashboard/Chartcomp/Chart1.jsx
+++ client/src/components/Dashboard/Chartcomp/Chart1.jsx
@@ -6,7 +6,7 @@ function Chart1() {
     const [Chart, SetChart] = useState([
         { ChartTitle: "Chart1", ChartNumber: "00000", id: "C-1" },
         { ChartTitle: "Chart2", ChartNumber: "00000", id: "C-2" },
-        { ChartTitle: "Chart1", ChartNumber: "00000", id: "C-1" },
+        { ChartTitle: "Chart3", ChartNumber: "00000", id: "C-3" },
 
     ])
 
