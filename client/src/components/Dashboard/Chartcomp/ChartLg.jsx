aa4755012f0d78e90c0a4e3b3ffa000d69fe252c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-03 08:34:36 +0800

    Subject: Body

--- client/src/components/Dashboard/Chartcomp/ChartLg.jsx
+++ client/src/components/Dashboard/Chartcomp/ChartLg.jsx
@@ -5,7 +5,7 @@ function ChartLg() {
 
     const [chartlg, setChartlg] = useState([
         {title: "Chartlg1", id: "lg-1"},
-        {title: "Chartlg2"}
+        {title: "Chartlg2", id: "lg-2"}
     ])
 
     return (
