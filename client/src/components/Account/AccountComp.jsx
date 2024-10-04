5a9bb5845d7ebe2d6c7c285bf50fbd27e404eaf7
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-04 22:35:36 +0800

    Subject: Body

--- client/src/components/Account/AccountComp.jsx
+++ client/src/components/Account/AccountComp.jsx
@@ -3,7 +3,7 @@ import { Form, Button, ButtonGroup, Dropdown  } from 'react-bootstrap';
 const AccountComp = () => {
 
     return ( 
-        <div className="contentAccount" style={{border: "1px solid", width: "500px", height: "500px", padding: "20px"}}>
+        <div className="contentAccount" style={{border: "1px solid", width: "500px", height: "100%", padding: "20px"}}>
             <p className='fs-3'>New Staff</p>
             <p className='fs-5'>Details</p>
 
@@ -20,7 +20,7 @@ const AccountComp = () => {
                     <Dropdown.Item href="#">Female</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
-            <Form.Control  type="text" placeholder="Gender" className='my-2' />
+            <Form.Control  type="text" placeholder="Gender" className='my-2' readOnly/>
 
             <Button type='button' variant='primary'>Create</Button>
 
