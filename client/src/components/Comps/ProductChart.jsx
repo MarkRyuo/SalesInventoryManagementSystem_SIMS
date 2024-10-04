cffb2d06a7d8bf7f20a67fa33a38e748e37e12db
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-04 18:30:21 +0800

    Subject: Body

--- client/src/components/Comps/ProductChart.jsx
+++ client/src/components/Comps/ProductChart.jsx
@@ -5,7 +5,7 @@
 function ProductChart() {
     return (
         <>
-            <div style={{border: "1px solid"}}>
+            <div style={{border: "1px solid red"}}>
                 Item 1 
                 {/* Insert Logic */}
             </div>
