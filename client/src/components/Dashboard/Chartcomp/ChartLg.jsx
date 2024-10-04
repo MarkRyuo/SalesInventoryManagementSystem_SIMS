731e989ca9476ba112ca215b74441c9c1ed54598
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-04 09:24:50 +0800

    Subject: Body

--- client/src/components/Dashboard/Chartcomp/ChartLg.jsx
+++ client/src/components/Dashboard/Chartcomp/ChartLg.jsx
@@ -8,7 +8,7 @@ function ChartLg({chartlg}) {
         
         <div className ={chartcomp.containerChartlg} >
             {chartlg.map((chartlg) => (
-                <div className="contentChartLg" key={chartlg.id} style={{border: "1px solid", width: "100%", height: "380px"}}>
+                <div className={chartcomp.contentChartLg} key={chartlg.id}>
                     <p>{chartlg.title}</p>
                 </div>
             ))}
