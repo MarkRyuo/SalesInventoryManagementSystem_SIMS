016bb8f37b2e340fedb79b4dee9fa56c5720751a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-04 22:27:15 +0800

    Subject: Body

--- client/src/components/Account/AccountComp.jsx
+++ client/src/components/Account/AccountComp.jsx
@@ -3,7 +3,7 @@ import { Form, Button, ButtonGroup, Dropdown  } from 'react-bootstrap';
 const AccountComp = () => {
 
     return ( 
-        <div className="contentAccount" style={{border: "1px solid", width: "500px", height: "500px"}}>
+        <div className="contentAccount" style={{border: "1px solid", width: "500px", height: "500px", padding: "20px"}}>
             <p className='fs-3'>New Staff</p>
             <p className='fs-5'>Details</p>
 
