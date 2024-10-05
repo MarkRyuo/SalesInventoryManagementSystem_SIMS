fdd9b6b471d04ea131fac4190f92a6d3b5936b92
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 22:02:56 +0800

    Subject: Body

--- client/src/pages/StaffAccount.jsx
+++ client/src/pages/StaffAccount.jsx
@@ -14,7 +14,7 @@ export const AccountPage = () => {
             <p className='fs-3'><span><MdAccountBox /></span> Accounts</p>
 
             <div className='contentAccount' style={{ border: "1px solid #e6e6e6", height: "700px", padding: 50, boxShadow: "5px 8px 8px #e6e6e6", background: " #ffffff" }}>
-                <p className='fs-4'>My Details</p>
+                <p className='fs-4'>Staff Details</p>
                 <p style={{ borderBottom: "1px solid gray", padding: "20px 10px" }}>Personal Information</p>
                 <ProfileComp />
 
