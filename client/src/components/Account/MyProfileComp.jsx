997c2bc2b5e04ab724f351f6588c9863a87432f6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 20:31:13 +0800

    Subject: Body

--- client/src/components/Account/MyProfileComp.jsx
+++ client/src/components/Account/MyProfileComp.jsx
@@ -2,13 +2,7 @@
 
 const MyProfileComp = () => {
     return (
-        <div 
-            style={{ border: "1px solid" }}>
-            <div style={{border: "1px solid", width: "100%", maxWidth: "500px", alignContent: 'center', minWidth: "px"}}>
-                <p className='fs-3 m-0'>Ramil Reyes</p>
-                <p>Example@email.com</p>
-            </div>
-        </div>
+        <
     );
 }
 
