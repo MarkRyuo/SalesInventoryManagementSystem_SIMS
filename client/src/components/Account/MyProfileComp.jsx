5932f4714799b957632d581fe118592d6e6fc331
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 20:26:45 +0800

    Subject: Body

--- client/src/components/Account/MyProfileComp.jsx
+++ client/src/components/Account/MyProfileComp.jsx
@@ -4,10 +4,7 @@ import {Button, Image, } from 'react-bootstrap'
 const MyProfileComp = () => {
     return (
         <div 
-            style={{
-                border: "1px solid",
-                display: 'flex',
-                }}>
+            style={{ border: "1px solid" }}>
             <div style={{border: "1px solid", width: "100%", maxWidth: "500px", alignContent: 'center', minWidth: "px"}}>
                 <p className='fs-3 m-0'>Ramil Reyes</p>
                 <p>Example@email.com</p>
