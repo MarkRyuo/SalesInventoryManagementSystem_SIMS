0b8db9f9b131606d7e794d3de50193538c842388
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 21:54:46 +0800

    Subject: Body

--- client/src/components/Account/ProfileComp.jsx
+++ client/src/components/Account/ProfileComp.jsx
@@ -29,24 +29,6 @@ const ProfileComp = () => {
                     </Form.Group>
                 </Col>
             </Row>
-            <InputGroup className="mb-3 ps-2" style={{width: "100%", maxWidth: "500px"}}>
-                <Form.Control
-                    aria-label="Text input with dropdown button"
-                    placeholder={gender || 'Select Gender'}
-                    readOnly
-                />
-
-                <DropdownButton
-                    variant="outline-secondary"
-                    title="Dropdown"
-                    id="input-group-dropdown-2"
-                    align="end"
-                    onSelect={handleGenderSelect}
-                >
-                    <Dropdown.Item eventKey="Male">Male</Dropdown.Item>
-                    <Dropdown.Item eventKey="Female">Female</Dropdown.Item>
-                </DropdownButton>
-            </InputGroup>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{ width: "100%", maxWidth: "500px", paddingLeft: 10 }}>
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="name@example.com" />
--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -4,9 +4,26 @@
 const Dropdown = () => {
     return (
         <>
-            
+            <InputGroup className="mb-3 ps-2" style={{ width: "100%", maxWidth: "500px" }}>
+                <Form.Control
+                    aria-label="Text input with dropdown button"
+                    placeholder={gender || 'Select Gender'}
+                    readOnly
+                />
+
+                <DropdownButton
+                    variant="outline-secondary"
+                    title="Dropdown"
+                    id="input-group-dropdown-2"
+                    align="end"
+                    onSelect={handleGenderSelect}
+                >
+                    <Dropdown.Item eventKey="Male">Male</Dropdown.Item>
+                    <Dropdown.Item eventKey="Female">Female</Dropdown.Item>
+                </DropdownButton>
+            </InputGroup>
         </>
     );
 }
- 
+
 export default Dropdown;
