bd2fc651ea146c60248c2ad4f2e8e15c8b757322
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 21:55:34 +0800

    Subject: Body

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -1,3 +1,4 @@
+import { InputGroup, DropdownButton, Dropdown } from 'react-bootstrap';
 
 
 
@@ -9,7 +10,7 @@ const Dropdown = () => {
     const handleGenderSelect = (eventKey) => {
         setGender(eventKey); // Updates the gender state with the selected value
     };
-    
+
     return (
         <>
             <InputGroup className="mb-3 ps-2" style={{ width: "100%", maxWidth: "500px" }}>
