9ce799e2b980858d7a375f59ef233a3d892c008b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 20:03:39 +0800

    Subject: Body

--- client/src/components/Account/MyProfileComp.jsx
+++ client/src/components/Account/MyProfileComp.jsx
@@ -1,11 +1,12 @@
-
+import {Image} from 'react-bootstrap'
 
 
 const MyProfileComp = () => {
     return (
-        <>
+        <div>
+            <Image src="https://placekitten.com/171/180" />
 
-        </>
+        </div>
     );
 }
 
