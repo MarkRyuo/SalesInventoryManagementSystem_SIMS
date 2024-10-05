ed6139a40f17fd6e5b31eb94df2320c580fbcf72
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 15:31:00 +0800

    Subject: Body

--- client/src/components/NavBar/NavDashboard.jsx
+++ client/src/components/NavBar/NavDashboard.jsx
@@ -68,6 +68,7 @@ export const NavDashboard = () => {
                         </div>
                     </div>
                 </Offcanvas.Body>
+                
             </Offcanvas>
 
         </Container>
