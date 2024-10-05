34b6497fd00fff3ff8447dcb5aa0f95b192378d2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 20:50:47 +0800

    Subject: Body

--- client/src/components/Account/ProfileComp.jsx
+++ client/src/components/Account/ProfileComp.jsx
@@ -2,10 +2,10 @@ import { Row, Form, Col } from 'react-bootstrap';
 
 const ProfileComp = () => {
     return (
-        <Form>
+        <Form style={{border: "1px solid"}}>
 
-            <Row>
-                <Col>
+            <Row style={{border: "1px solid red", width: "100%", maxWidth: "700px", minWidth: "380px"}}>
+                <Col sm={12}>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                         <Form.Label>FIRST NAME</Form.Label>
                         <Form.Control type="text" />
