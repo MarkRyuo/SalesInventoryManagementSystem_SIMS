f88a062d8d500f4029855d5362eddee2de1d52f0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 18:36:07 +0800

    Subject: Body

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -1,11 +1,23 @@
+import AccountComp from '../components/Account/AccountComp';
+import { MainLayout } from '../layout/MainLayout';
+import { MdAccountBox } from "react-icons/md";
 
+//* Accounts Page
 
-const MyProfile = () => {
+export const MyProfile = () => {
+
     return (
-        <>
-            
-        </>
-    );
+        <MainLayout>
+
+            <p className='fs-3'><span><MdAccountBox /></span> Accounts</p>
+
+            <div className='contentAccount' style={{ border: "1px solid red", height: "700px" }}>
+
+                <AccountComp />
+
+            </div>
+        </MainLayout>
+    )
 }
- 
+
 export default MyProfile;
