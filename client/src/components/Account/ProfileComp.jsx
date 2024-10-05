8bd8f0c15a2b9b7e5975e69fd0221457c7ed2940
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 20:44:38 +0800

    Subject: Body

--- client/src/components/Account/ProfileComp.jsx
+++ client/src/components/Account/ProfileComp.jsx
@@ -7,8 +7,8 @@ const ProfileComp = () => {
             <Row>
                 <Col>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" />
+                        <Form.Label>FIRST NAME</Form.Label>
+                        <Form.Control type="text" placeholder="n" />
                     </Form.Group>
                 </Col>
             </Row>
