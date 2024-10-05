32df476c119ee1c5ef85becded44edb05197da03
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 14:52:02 +0800

    Subject: Body

--- client/src/components/NavBar/Buttons.jsx
+++ client/src/components/NavBar/Buttons.jsx
@@ -1,13 +1,14 @@
 import { MdSpaceDashboard } from "react-icons/md";
 
 //* Props
-export const Buttons = ({buttons, Icon}) => {
+export const Buttons = ({buttons}) => {
 
     return (
-        <div>
+        <div style={{border: "1px solid", display: "flex", alignItems: "center"}}>
             <span>{Icon}</span>
             {buttons.map((button) => (
                 <a href="#" key={button.id}>
+                    <span></span>
                     <p>{button.btnName}</p>
                 </a>
             ))}
