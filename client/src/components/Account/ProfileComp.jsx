b98f0d828aaefdc5a6c161c48f07c5560b9609f8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 21:44:28 +0800

    Subject: Body

--- client/src/components/Account/ProfileComp.jsx
+++ client/src/components/Account/ProfileComp.jsx
@@ -1,6 +1,17 @@
-import { Row, Form, Col } from 'react-bootstrap';
+import { Row, Form, Col, InputGroup } from 'react-bootstrap';
+import { useState } from 'react';
 
 const ProfileComp = () => {
+
+    const [gender, setGender] = useState('');
+
+    // Function to handle gender selection
+    const handleGenderSelect = (eventKey) => {
+        setGender(eventKey); // Updates the gender state with the selected value
+    };
+
+
+
     return (
         <Form>
 
@@ -22,6 +33,24 @@ const ProfileComp = () => {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="name@example.com" />
             </Form.Group>
+            <InputGroup className="mb-3">
+                <Form.Control
+                    aria-label="Text input with dropdown button"
+                    placeholder={gender || 'Select Gender'}
+                    readOnly
+                />
+
+                <DropdownButton
+                    variant="outline-secondary"
+                    title="Dropdown"
+                    id="input-group-dropdown-2"
+                    align="end"
+                    onSelect={handleGenderSelect}
+                >
+                    <Dropdown.Item eventKey="Male">Male</Dropdown.Item>
+                    <Dropdown.Item eventKey="Female">Female</Dropdown.Item>
+                </DropdownButton>
+            </InputGroup>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{width: "100%", maxWidth: "500px", paddingLeft: 10}}>
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="name@example.com" />
