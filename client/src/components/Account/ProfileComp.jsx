6ae5a3e42e4bed75388f254c91ee3fdc159658d4
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 20:41:01 +0800

    Subject: Body

--- client/src/components/Account/ProfileComp.jsx
+++ client/src/components/Account/ProfileComp.jsx
@@ -2,9 +2,12 @@ import Form from 'react-bootstrap/Form';
 
 const ProfileComp = () => {
     return (
-        <>
-
-        </>
+        <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+        </Form>
     );
 }
 
