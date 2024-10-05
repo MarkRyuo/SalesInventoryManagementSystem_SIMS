5f9445d3e3412e0e30514e9a464f0fce87f5e4bc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 15:26:29 +0800

    Subject: Body

--- client/src/components/NavBar/Buttons.jsx
+++ client/src/components/NavBar/Buttons.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 
 //* Props
 export const Buttons = ({ buttons }) => {
@@ -5,7 +6,7 @@ export const Buttons = ({ buttons }) => {
     return (
         <div>
             {buttons.map((button) => (
-                <a href="#" key={button.id} style={{ border: "1px solid", display: "flex", alignItems: "center", height: "50px", textDecoration: "none" }}>
+                <a href="#" key={button.id} style={{ border: "1px solid", display: "flex", alignItems: "center", height: "50px", textDecoration: "none", width: "100%", minWidth: "200px" }}>
                     <span>{button.icon}</span>
                     {button.btnName}
                 </a>
