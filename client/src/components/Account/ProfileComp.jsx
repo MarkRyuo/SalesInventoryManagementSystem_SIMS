df72600594d3fc4506bf796399510be2d0337aa9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 20:39:36 +0800

    Subject: Body

--- client/src/components/Account/ProfileComp.jsx
+++ client/src/components/Account/ProfileComp.jsx
@@ -1,4 +1,4 @@
-
+import Form from 'react-bootstrap/Form';
 
 const ProfileComp = () => {
     return (
--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -10,9 +10,9 @@ export const MyProfile = () => {
 
             <p className='fs-3'><span><MdAccountBox /></span> Accounts</p>
 
-            <div className='contentAccount' style={{ border: "1px solid red", height: "700px" }}>
-                <p>My Details</p>
-                <p style={{borderBottom:"1px solid gray", paddingBottom: "50px" }}>Personal Information</p>
+            <div className='contentAccount' style={{ border: "1px solid red", height: "700px", padding: 20 }}>
+                <p className='fs-4'>My Details</p>
+                <p style={{borderBottom:"1px solid gray", padding: "20px 10px" }}>Personal Information</p>
             </div>
         </MainLayout>
     )
