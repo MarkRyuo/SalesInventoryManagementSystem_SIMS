0540f0d1a7af1ce2a1bda80078b1fa590b1c22ca
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 20:55:05 +0800

    Subject: Body

--- client/src/components/Account/ProfileComp.jsx
+++ client/src/components/Account/ProfileComp.jsx
@@ -4,7 +4,7 @@ const ProfileComp = () => {
     return (
         <Form>
 
-            <Row style={{border: "1px solid red", width: "100%", margin: 0, padding: 0}}>
+            <Row style={{width: "100%", margin: 0, padding: 0}}>
                 <Col lg={6}>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                         <Form.Label>FIRST NAME</Form.Label>
@@ -18,7 +18,7 @@ const ProfileComp = () => {
                     </Form.Group>
                 </Col>
             </Row>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{width: "100%"}}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{width: "100%", maxWidth: "400px"}}>
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="name@example.com" />
             </Form.Group>
