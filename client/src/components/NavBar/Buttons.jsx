981953698e81c5e0ae9ad1dfec47d4d6e28d3ae2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 14:14:06 +0800

    Subject: Body

--- client/src/components/NavBar/Buttons.jsx
+++ client/src/components/NavBar/Buttons.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
+import { MdSpaceDashboard } from "react-icons/md";
 
 //* Props
 export const Buttons = () => {
 
     const [bottons, setBottons] = useState([
-        
+        { icon: "<MdSpaceDashboard />", btnName: "Dashboard", }
     ])
 
     return (
