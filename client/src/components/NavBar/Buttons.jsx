eade201cd31d4872eecd16fd144dd8ae040cc846
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 14:59:08 +0800

    Subject: Body

--- client/src/components/NavBar/Buttons.jsx
+++ client/src/components/NavBar/Buttons.jsx
@@ -1,13 +1,13 @@
 
 //* Props
-export const Buttons = ({buttons}) => {
+export const Buttons = ({ buttons }) => {
 
     return (
         <div>
             {buttons.map((button) => (
                 <a href="#" key={button.id} style={{ border: "1px solid", display: "flex", alignItems: "center", height: "50px" }}>
                     <span>{button.icon}</span>
-                        {button.btnName}
+                    {button.btnName}
 
                 </a>
             ))}
