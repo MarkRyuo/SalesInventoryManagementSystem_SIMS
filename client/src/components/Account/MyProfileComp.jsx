d4ec29b3a644f6fbf99f12b08295a44cc81e9a8e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 20:21:00 +0800

    Subject: Body

--- client/src/components/Account/MyProfileComp.jsx
+++ client/src/components/Account/MyProfileComp.jsx
@@ -8,10 +8,10 @@ const MyProfileComp = () => {
                 border: "1px solid",
                 display: 'flex',
                 }}>
-            <div style={{width: "200px"}}> 
+            <div style={{width: "150px", marginRight: "15px" }}> 
                 <Image src="https://www.pngkey.com/png/full/22-223848_businessman-vector-person-logo-png.png" thumbnail style={{width: "auto"}}/>
             </div>
-            <div>
+            <div style={{border: "1px solid", width: "100%", maxWidth: "250px", alignContent: 'center'}}>
                 <p className='fs-3 m-0'>Ramil Reyes</p>
                 <p>Example@email.com</p>
                 <Button>Upload Image</Button>
