55e349c205cef581e7b6654a9cb896f8f8695027
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 22:01:59 +0800

    Subject: Body

--- client/src/pages/StaffAccount.jsx
+++ client/src/pages/StaffAccount.jsx
@@ -1,4 +1,4 @@
-import AccountComp from '../components/Account/AccountComp';
+
 import { MainLayout } from '../layout/MainLayout' ;
 import { MdAccountBox } from "react-icons/md";
 
@@ -8,14 +8,18 @@ export const AccountPage = () => {
 
     return (
         <MainLayout>
-            
+
             <p className='fs-3'><span><MdAccountBox /></span> Accounts</p>
 
-            <div className='contentAccount' style={{border: "1px solid red", height: "700px"}}> 
+            <div className='contentAccount' style={{ border: "1px solid #e6e6e6", height: "700px", padding: 50, boxShadow: "5px 8px 8px #e6e6e6", background: " #ffffff" }}>
+                <p className='fs-4'>My Details</p>
+                <p style={{ borderBottom: "1px solid gray", padding: "20px 10px" }}>Personal Information</p>
+                <ProfileComp />
 
-            
-
+                <Button variant='primary' className='ms-2'>Save</Button>
+                <Button variant='primary' className='ms-2'>Edit</Button>
             </div>
+
         </MainLayout>
     )
 }
