cb9fb2f5b31cd87ad06cc7045c2d3090a006243c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 21:04:25 +0800

    Subject: Body

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { NavDashboard } from "../components/NavBar/NavDashboard"
 import { Container } from "react-bootstrap"
+import MainLayoutCss from './MainLayout.module.css'
 
 export const MainLayout = ({children}) => {
 
