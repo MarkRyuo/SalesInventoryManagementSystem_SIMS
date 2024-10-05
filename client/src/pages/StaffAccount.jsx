9be848da2572b60a696a43998f73d65667262602
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-05 18:32:04 +0800

    Subject: Body

--- client/src/pages/StaffAccount.jsx
+++ client/src/pages/StaffAccount.jsx
@@ -11,7 +11,7 @@ export const AccountPage = () => {
             
             <p className='fs-3'><span><MdAccountBox /></span> Accounts</p>
 
-            <div className='contentAccount' style={{border: "1px solid red", height: "700px"}}> 
+            <div className='contentAccount' style={{border: "1px solid red", height: "700px", padding: "300px"}}> 
 
                 <AccountComp />
 
