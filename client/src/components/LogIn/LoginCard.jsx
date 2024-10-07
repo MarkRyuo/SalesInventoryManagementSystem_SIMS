709a4a03ee821a9816496cf7af6500819b5666ff
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-07 21:10:39 +0800

    Setting up: Login

--- client/src/components/LogIn/LoginCard.jsx
+++ client/src/components/LogIn/LoginCard.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 export const LoginCard = () => {
 
-    const [navigate, setNavigate] = useNavigate
+    const [navigate, setNavigate] = useNavigate()
 
     return (
 
