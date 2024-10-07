a80e305433deb8980074343cd03d4f236add1cf7
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-07 21:41:56 +0800

    Setting up: Login

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -13,18 +13,20 @@ export const MyProfile = () => {
 
             <p className='fs-3'><span><MdAccountBox /></span> Accounts</p>
 
-            <div className='contentAccount' style={{ border: "1px solid #e6e6e6", height: "700px", padding: 50, boxShadow: "5px 8px 8px #e6e6e6", background: " #ffffff", borderRadius: "15px" }}>
+            <div className='contentAccount' style={{ border: "1px solid #e6e6e6", height: "750px", padding: 50, boxShadow: "5px 8px 8px #e6e6e6", background: " #ffffff", borderRadius: "15px" }}>
                 <p className='fs-4'>My Details</p>
                 <p style={{borderBottom:"1px solid gray", padding: "20px 10px" }}>Personal Information</p>
                 <ProfileComp />
 
-                <Button variant="light" size='sm'>
+                <Button variant="light" size='sm' className='ms-2'>
                     <FcGoogle size={35} className='me-2' />
                     Login with Google
                 </Button>
-
-                <Button variant='primary' className='ms-2'>Save</Button>
-                <Button variant='primary' className='ms-2'>Edit</Button>
+            
+                <div className='mt-3'>
+                    <Button variant='primary' className='ms-2'>Save</Button>
+                    <Button variant='primary' className='ms-2'>Edit</Button>
+                </div>
             </div>
 
         </MainLayout>
