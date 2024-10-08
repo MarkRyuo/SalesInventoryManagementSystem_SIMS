d7337dfeaa7909222bbe7e54955589ad8704e1b8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-08 08:05:48 +0800

    Re-edit: MyProfilepage

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -15,7 +15,6 @@ export const MyProfile = () => {
             <div className={MyProfilecss.contentAccount}>
                 <p className='fs-4'>My Details</p>
                 <p style={{borderBottom:"1px solid gray", padding: "20px 10px" }}>Personal Information</p>
-
                 <ProfileComp />
                 
             </div>
--- client/src/pages/StaffAccount.jsx
+++ client/src/pages/StaffAccount.jsx
@@ -3,6 +3,7 @@ import { MdAccountBox } from "react-icons/md";
 
 import ProfileComp from '../components/Account/ProfileComp';
 import { Button } from 'react-bootstrap';
+import StaffComp from '../components/Account/StaffComp';
 
 //* Accounts Page
 
@@ -16,7 +17,7 @@ export const AccountPage = () => {
             <div className='contentAccount' style={{ border: "1px solid #e6e6e6", height: "700px", padding: 50, boxShadow: "5px 8px 8px #e6e6e6", background: " #ffffff", borderRadius: "15px" }}>
                 <p className='fs-4'>Staff Details</p>
                 <p style={{ borderBottom: "1px solid gray", padding: "20px 10px" }}>Personal Information</p>
-                <ProfileComp />
+                <StaffComp />
 
                 <Button variant='primary' className='ms-2'>Add</Button>
             </div>
