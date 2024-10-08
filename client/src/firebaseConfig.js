d861e03c418e74cb4192fcc199803ef6d4078cf2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-08 09:17:17 +0800

    Backend: Login of Admin

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBuv3oJDJmx9LPa_ZammT7HksOLpsc23_o",
     authDomain: "salesinventoryms-74917.firebaseapp.com",
@@ -19,4 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export default app;
