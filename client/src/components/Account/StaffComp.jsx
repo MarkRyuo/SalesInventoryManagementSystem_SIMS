b4b15ffd1a088a723c7ca8348b20398be465a63d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-08 08:10:28 +0800

    Re-edit: MyProfilepage

--- client/src/components/Account/StaffComp.jsx
+++ client/src/components/Account/StaffComp.jsx
@@ -1,5 +1,4 @@
-import { Row, Form, Col, Button } from 'react-bootstrap';
-import DropDown from '../DropDown';
+import { Row, Form, Col, Button, InputGroup, DropdownButton, Dropdown } from 'react-bootstrap';
 import { useState } from 'react';
 
 const StaffComp = () => {
@@ -20,6 +19,13 @@ const StaffComp = () => {
         });
     };
 
+    const [gender, setGender] = useState('');
+
+    // Function to handle gender selection
+    const handleGenderSelect = (eventKey) => {
+        setGender(eventKey); // Updates the gender state with the selected value
+    };
+
     return (
         <Form>
 
@@ -37,7 +43,25 @@ const StaffComp = () => {
                     </Form.Group>
                 </Col>
             </Row>
-            <DropDown />
+            {/* Dropdown(Male or Female) */}
+            <InputGroup className="mb-3 ps-2" style={{ width: "100%", maxWidth: "500px" }}>
+                <Form.Control
+                    aria-label="Text input with dropdown button"
+                    placeholder={gender || 'Select Gender'}
+                    readOnly
+                />
+
+                <DropdownButton
+                    variant="outline-secondary"
+                    title="Dropdown"
+                    id="input-group-dropdown-2"
+                    align="end"
+                    onSelect={handleGenderSelect}
+                >
+                    <Dropdown.Item eventKey="Male">Male</Dropdown.Item>
+                    <Dropdown.Item eventKey="Female">Female</Dropdown.Item>
+                </DropdownButton>
+            </InputGroup>
 
             <Form.Group
                 className="mb-3"
