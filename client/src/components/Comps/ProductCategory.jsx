705fbd7b94eeaf13739d713935fcb3c092093249
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-09 15:08:06 +0800

    Update: Login

--- client/src/components/Comps/ProductCategory.jsx
+++ client/src/components/Comps/ProductCategory.jsx
@@ -4,7 +4,7 @@ import { Dropdown, DropdownButton } from "react-bootstrap";
 const ProductCategory = () => {
     return (
         <DropdownButton id="dropdown-basic-button" title="Category">
-            <Dropdown.Item href="#">Item 1</Dropdown.Item>
+            <Dropdown.Item href="#">Product 1</Dropdown.Item>
             <Dropdown.Item href="#">Item 2</Dropdown.Item>
             <Dropdown.Item href="#">Item 3</Dropdown.Item>
         </DropdownButton>
