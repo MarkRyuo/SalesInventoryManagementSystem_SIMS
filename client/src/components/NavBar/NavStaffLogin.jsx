ed20ce20850973bc3754deca7bf586f6890767fe
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-09 16:26:00 +0800

    Update: Adding Staff Pages

--- client/src/components/NavBar/NavStaffLogin.jsx
+++ client/src/components/NavBar/NavStaffLogin.jsx
@@ -1,11 +1,40 @@
+import { BsBox } from "react-icons/bs";
+import { Row, Col, Container, Navbar, Dropdown } from 'react-bootstrap';
+import Navbars from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 
 
 function NavStaffLogin() {
     return (
-        <div>
+        <div>   
+            <Container fluid className={Navbars.navLogin}> {/* fix sm-Screen 412px */}
+                <Navbar>
+                    <Container>
+                        <Row>
+                            <Col> <BsBox size={40} /> </Col>
+                            <Col style={{ paddingTop: 4 }}>
+                                <Navbar.Brand className="Nav-brand">SIMS</Navbar.Brand>
+                            </Col>
+                        </Row>
 
+                        <Dropdown>
+                            <Dropdown.Toggle variant="light" id="dropdown-split-basic" className="me-3">
+                                Option
+                            </Dropdown.Toggle>
+
+                            {/* Dropdown function */}
+                            <Dropdown.Menu style={{ width: 100 }}>
+                                <Dropdown.Item as={Link} to='/SLogin' >Sales Inventory</Dropdown.Item> {/* Staff Account*/}
+                                <Dropdown.Item as={Link} to='DeveloperPage' >Developer</Dropdown.Item>
+                                <Dropdown.Item as={Link} to='/AboutPage' >About</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+                    </Container>
+                </Navbar>
+            </Container>
         </div>
     )
 }
 
-export default NavStaffLogin
+export default NavStaffLogin ;
