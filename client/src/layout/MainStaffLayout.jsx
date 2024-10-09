969497d14d3fe7b7dff34b62f0cad75b2b068fd9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-09 18:05:12 +0800

    Update: Adding Staff Navbar

--- client/src/layout/MainStaffLayout.jsx
+++ client/src/layout/MainStaffLayout.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 
 import { Container } from "react-bootstrap"
+import NavbarStaffDashboard from "../components/NavBar/NavbarStaffDashboard"
 
 
 function MainStaffLayout({children}) {
     return (
         <div className="container">
+            <NavbarStaffDashboard />
             <Container fluid className="content"> {/**Content */}
                 {children}
             </Container>
