3bc1dea4efe1c1ed470fb566a568d180fa525c5d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-09 15:28:46 +0800

    Update: Adding Staff Layout

--- client/src/components/LogIn/SLoginCard.jsx
+++ client/src/components/LogIn/SLoginCard.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 function SLoginCard() {
 
     const navigate = useNavigate();
-    const handleLogin = () => navigate("/DashboardPage")
+    const handleLogin = () => navigate("/SDashboard)
 
     return (
         <>
