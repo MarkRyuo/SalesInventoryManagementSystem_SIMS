f166f72f77007899962d9a483fdbbd78f4cf83e9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-09 15:27:35 +0800

    Update: Adding Staff Layout

--- client/src/components/LogIn/SLoginCard.jsx
+++ client/src/components/LogIn/SLoginCard.jsx
@@ -1,10 +1,35 @@
 
 
-//*
+//* Child
 function SLoginCard() {
     return (
         <>
+            <Form>
+                {/* Username */}
+                <FloatingLabel controlId="floatingInput" label="Email or Username" className="mb-4">
+                    <Form.Control
+                        type="text"
+                        placeholder='Email or Username'
+                    />
+                </FloatingLabel>
 
+                {/* Password */}
+                <FloatingLabel controlId="floatingPassword" label="Password">
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                    />
+                </FloatingLabel>
+
+                {/* Button of Login */}
+                <Button
+                    variant="primary"
+                    style={{ width: "70%", marginTop: "20px" }}
+                    onClick={handleLogin}
+                    size='lg'>
+                    Login
+                </Button>
+            </Form>
         </>
     )
 }
