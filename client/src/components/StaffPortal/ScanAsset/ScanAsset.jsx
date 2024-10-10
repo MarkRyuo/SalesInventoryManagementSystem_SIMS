eb2350fae3fbbd6665800d4b78a41757de019b4d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-10 21:53:07 +0800

    Update: SDashboard

--- client/src/components/StaffPortal/ScanAsset/ScanAsset.jsx
+++ client/src/components/StaffPortal/ScanAsset/ScanAsset.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
-
+import { BiScan } from "react-icons/bi";
 
 function ScanAsset() {
 
@@ -10,8 +10,8 @@ function ScanAsset() {
 
     return (
         <div style={{ width: "100%", height: 100, alignContent: 'center' }}>
-            <Button size='lg' variant="outline-success" onClick={handleAddNewAssets}>
-                ScanAsset
+            <Button size='lg' variant="outline-success" onClick={handleAddNewAssets} style={{marginLeft: 50}}>
+                <BiScan size={30}/>ScanAsset
             </Button>
         </div>
     )
