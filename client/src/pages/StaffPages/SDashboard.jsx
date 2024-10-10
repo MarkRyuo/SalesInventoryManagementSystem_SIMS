c405bfb552edcf45b325d9795da4945c86314eb9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-10 10:08:02 +0800

    Update: Adding Btns

--- client/src/pages/StaffPages/SDashboard.jsx
+++ client/src/pages/StaffPages/SDashboard.jsx
@@ -4,9 +4,7 @@ import { Row, Col } from "react-bootstrap";
 function SDashboard() {
     return (
         <MainStaffLayout>
-            <div>
-                <h1>Staff DashboardPage</h1>
-            </div>
+            
         </MainStaffLayout>
     )
 }
