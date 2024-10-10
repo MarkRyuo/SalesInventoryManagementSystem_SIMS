4439af7334039e868ca33a1198e558d52e10f6e1
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-10 11:04:56 +0800

    Update: Adding Btns

--- client/src/components/AddNewAssets/AddNewAssets.jsx
+++ client/src/components/AddNewAssets/AddNewAssets.jsx
@@ -9,8 +9,8 @@ function AddNewAssets() {
     const handleAddNewAssets = () => navigate("/") //* Add a page here 
 
     return (
-        <div style={{ border: "1px solid", width: "100%", height: 100, alignContent: 'center',  }}>
-            <Button size='lg' onClick={handleAddNewAssets} style={{border: "1px solid"}}>
+        <div style={{ border: "1px solid", width: "100%", height: 100, alignContent: 'center' }}>
+            <Button size='lg' variant='' onClick={handleAddNewAssets}>
                 Add New Assets
             </Button>
         </div>
