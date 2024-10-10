755dc2e1df60a3c8f6ec520ce8e895f00889b18f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-10 22:17:54 +0800

    Update: SDashboard

--- client/src/components/Charts/DashboardChart/Chart1.jsx
+++ client/src/components/Charts/DashboardChart/Chart1.jsx
@@ -1,9 +1,9 @@
-import Chartcss from './Charts.module.css'
+import Chartcss from './Charts.module.css' ;
 
 //* Chart1 Small
 function Chart1() {
     return (
-        <div className="containerContent">
+        <div className={Chartcss.containerContent}>
                 <div className="contentChart" style={{ border: "1px solid", textAlign: "center", height: "150px", alignContent: "center", borderRadius: "10px" }}>
                     <p className="fs-4">Chart1</p>
                     <p className="fs-5">$0000</p>
