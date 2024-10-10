e727693ae2f9a6116504759031ccfb0feacb21f9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-10 10:18:27 +0800

    Update: Adding Btns

--- client/src/pages/StaffPages/SDashboard.jsx
+++ client/src/pages/StaffPages/SDashboard.jsx
@@ -6,13 +6,13 @@ function SDashboard() {
     return (
         <MainStaffLayout>
             <Row style={{border: "1px solid", width: "100%", height: "700px", margin: 0, justifyContent: "center"}}>
-                <Col sm={12} style={{border: "1px solid red", borderRadius: "10px", width: "100%", maxWidth: 300}}>
+                <Col sm={12} style={{border: "1px solid red", borderRadius: "10px", width: "100%", maxWidth: 300, height: 200}}>
                     <AddNewAssets />
                 </Col>
-                <Col sm={12} style={{ border: "1px solid red", borderRadius: "10px", width: "100%", maxWidth: 300 }}>
+                <Col sm={12} style={{ border: "1px solid red", borderRadius: "10px", width: "100%", maxWidth: 300, height: 200 }}>
                     <AddNewAssets />
                 </Col>
-                <Col sm={12} style={{ border: "1px solid red", borderRadius: "10px", width: "100%", maxWidth: 300 }}>
+                <Col sm={12} style={{ border: "1px solid red", borderRadius: "10px", width: "100%", maxWidth: 300, height: 200}}>
                     <AddNewAssets />
                 </Col>
             </Row>
