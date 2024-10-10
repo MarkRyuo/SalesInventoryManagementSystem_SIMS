2e8f00881f0b1dfa05b76ae0959c3c34063ef0fa
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-10 22:25:06 +0800

    Update: SDashboard

--- client/src/components/Charts/DashboardChart/Chart1.jsx
+++ client/src/components/Charts/DashboardChart/Chart1.jsx
@@ -4,6 +4,9 @@ import Chartcss from './Charts.module.css' ;
 function Chart1() {
     return (
         <div className={Chartcss.containerContent}>
+                <div>
+                    
+                </div>
                 <div className={Chartcss.contentChart}>
                     <p className="fs-4">Chart1</p>
                     <p className="fs-5">$0000</p>
