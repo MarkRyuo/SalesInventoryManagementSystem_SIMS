e5b647b9ecc17ecd133e1e1f11314dc0814cd40c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-10 11:22:28 +0800

    Update: Adding Btns

---
+++ client/src/components/ScanAsset/ScanAsset.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
+
+function ScanAsset() {
+
+    const navigate = useNavigate();
+
+    const handleAddNewAssets = () => navigate("") //* Add a page here 
+
+    return (
+        <div style={{ width: "100%", height: 100, alignContent: 'center' }}>
+            <Button size='lg' variant="outline-success" onClick={handleAddNewAssets}>
+                Add New Assets
+            </Button>
+        </div>
+    )
+}
+
+export default ScanAsset;
