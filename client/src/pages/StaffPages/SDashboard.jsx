7838d8171f629fbc680d680acee56c7e58a887ba
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-10 21:33:52 +0800

    Update: SDashboard

--- client/src/pages/StaffPages/SDashboard.jsx
+++ client/src/pages/StaffPages/SDashboard.jsx
@@ -3,19 +3,12 @@ import ScanAsset from "../../components/StaffPortal/ScanAsset/ScanAsset";
 import SearchAssets from "../../components/StaffPortal/SearchAsset/SearchAsset";
 import MainStaffLayout from "../../layout/MainStaffLayout";
 import { Row, Col } from "react-bootstrap";
+import SDashboardCss from './SDashboard.module.css' ;
 
 function SDashboard() {
     return (
         <MainStaffLayout>
-            <Row style={{
-                
-                width: "100%", 
-                height: "700px", 
-                margin: 0, 
-                justifyContent: "space-around", 
-                flexDirection: "column", 
-                alignContent: "center",
-                }}>
+            <Row className="">
                 <Col lg={12} style={{
                     borderRadius: "10px", 
                     width: "100%", 
