c53663801f7e4d7b6037dbae8744582254e4dbac
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-10 22:14:45 +0800

    Update: SDashboard

--- client/src/components/Charts/DashboardChart/Chart1.jsx
+++ client/src/components/Charts/DashboardChart/Chart1.jsx
@@ -5,8 +5,8 @@ function Chart1() {
     return (
         <div className="containerContent">
                 <div className="contentChart" style={{ border: "1px solid", textAlign: "center", height: "150px", alignContent: "center", borderRadius: "10px" }}>
-                    <p className="fs-4"></p>
-                    <p className="fs-5"></p>
+                    <p className="fs-4">Chart1</p>
+                    <p className="fs-5">Chart2</p>
                 </div>
         </div>
     )
