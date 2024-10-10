fdb8746eca2da04e00137444393b12c83b376d48
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-10 10:17:39 +0800

    Update: Adding Btns

--- client/src/pages/StaffPages/SDashboard.jsx
+++ client/src/pages/StaffPages/SDashboard.jsx
@@ -5,15 +5,15 @@ import { Row, Col } from "react-bootstrap";
 function SDashboard() {
     return (
         <MainStaffLayout>
-            <Row style={{border: "1px solid", width: "100%", height: "700px", margin: 0}}>
-                <Col sm={12} style={{border: "1px solid red", borderRadius: "10px", width: "100"}}>
+            <Row style={{border: "1px solid", width: "100%", height: "700px", margin: 0, justifyContent: "center"}}>
+                <Col sm={12} style={{border: "1px solid red", borderRadius: "10px", width: "100%", maxWidth: 300}}>
                     <AddNewAssets />
                 </Col>
-                <Col>
-
+                <Col sm={12} style={{ border: "1px solid red", borderRadius: "10px", width: "100%", maxWidth: 300 }}>
+                    <AddNewAssets />
                 </Col>
-                <Col>
-
+                <Col sm={12} style={{ border: "1px solid red", borderRadius: "10px", width: "100%", maxWidth: 300 }}>
+                    <AddNewAssets />
                 </Col>
             </Row>
         </MainStaffLayout>
