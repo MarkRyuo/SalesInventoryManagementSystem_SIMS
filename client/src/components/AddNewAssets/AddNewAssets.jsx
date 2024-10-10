de3601bc41076f71b4f941b49816b137b097796b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-10 11:14:58 +0800

    Update: Adding Btns

--- client/src/components/AddNewAssets/AddNewAssets.jsx
+++ client/src/components/AddNewAssets/AddNewAssets.jsx
@@ -6,11 +6,11 @@ function AddNewAssets() {
 
     const navigate = useNavigate() ;
     
-    const handleAddNewAssets = () => navigate("/") //* Add a page here 
+    const handleAddNewAssets = () => navigate("") //* Add a page here 
 
     return (
         <div style={{ border: "1px solid", width: "100%", height: 100, alignContent: 'center' }}>
-            <Button size='lg' variant="outline-success" onClick={handleAddNewAssets}>
+            <Button size='lg' className=' btn btn-outline-success' onClick={handleAddNewAssets}>
                 Add New Assets
             </Button>
         </div>
