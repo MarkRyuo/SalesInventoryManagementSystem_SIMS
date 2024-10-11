7e22053921728a4f5a28e98bf6ca616c46bbb5e1
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 18:11:59 +0800

    Update: System Updating

--- client/src/pages/StaffPages/SStaffAccount.jsx
+++ client/src/pages/StaffPages/SStaffAccount.jsx
@@ -5,7 +5,14 @@ import MainStaffLayout from "../../layout/MainStaffLayout";
 function SStaffAccount() {
     return (
         <MainStaffLayout>
-            <StaffAccountComp />
+            <p className='fs-3'><span><MdAccountBox /></span> Accounts</p>
+
+            <div className='contentAccount' style={{ border: "1px solid #e6e6e6", height: "700px", padding: 50, boxShadow: "5px 8px 8px #e6e6e6", background: " #ffffff", borderRadius: "15px" }}>
+                <p className='fs-4'>Staff Details</p>
+                <p style={{ borderBottom: "1px solid gray", padding: "20px 10px" }}>Personal Information</p>
+                <StaffAccountComp />
+
+            </div>
         </MainStaffLayout>
     )
 }
