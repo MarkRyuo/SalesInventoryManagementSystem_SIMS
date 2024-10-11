2a69d597e14c00b5bbaf217b888a1efb427bfa30
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 09:19:35 +0800

    Update: Admin Dashboard

--- client/src/components/Charts/DashboardChart/ChartLg2.jsx
+++ client/src/components/Charts/DashboardChart/ChartLg2.jsx
@@ -3,7 +3,7 @@ import Chartcss from './Charts.module.css';
 
 function ChartLg2() {
     return (
-        <div className="containerChartLg">
+        <div className={Chartcss.containerChartLg}>
             {/* Code for Chart Here */}
         </div>
     )
