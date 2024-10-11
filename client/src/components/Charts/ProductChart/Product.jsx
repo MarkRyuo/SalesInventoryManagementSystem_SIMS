69300b517b38c5b15db7b4f96a0e4eb8ce72a27c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 10:01:06 +0800

    Update: Admin Dashboard

--- client/src/components/Charts/ProductChart/Product.jsx
+++ client/src/components/Charts/ProductChart/Product.jsx
@@ -1,23 +1,33 @@
 import Search from "../components/Comps/Search";
 import ProductCategory from "../components/Comps/ProductCategory";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form, Button } from "react-bootstrap";
 
 
 //* Props 
 function ProductChart() {
     return (
         <>
-            <div>
-                <Search />
-                <ProductCategory />
-            </div>
 
             <Row>
                 <Col>
+                    <Form inline>
+                        <Row className="d-flex justify-content-end g-1 my-4">
+                            <Col lg={3} xs={8} md={5}>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Search Products"
+                                    className=" mr-sm-2"
+                                />
+                            </Col>
+                            <Col lg={1} xs={2} md={2}>
+                                <Button type="submit" variant="primary"><IoSearch /></Button>
+                            </Col>
+                        </Row>
+                    </Form>
 
                 </Col>
                 <Col>
-                    
+
                 </Col>
             </Row>
 
