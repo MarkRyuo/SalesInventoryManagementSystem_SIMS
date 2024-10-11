770365ca01704aa45decc64530d5f6546d02cb66
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 19:40:41 +0800

    Update: System Updating

--- client/src/pages/StaffPages/AddNewAssets.jsx
+++ client/src/pages/StaffPages/AddNewAssets.jsx
@@ -1,12 +1,11 @@
 
-import React from 'react'
 
 function AddNewAssets() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default AddNewAssets
