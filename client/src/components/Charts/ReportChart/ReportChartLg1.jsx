46af839af0475e5b4f314e1b1e0e68f3fffd462a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 11:34:01 +0800

    Update: Admin Dashboard

---
+++ client/src/components/Charts/ReportChart/ReportChartLg1.jsx
@@ -0,0 +1,11 @@
+import ReportChartcss from './ReportChart.module.css'
+
+function ChartLg1() {
+    return (
+        <div className={ReportChartcss.containerChartLg}>
+            {/* Code for Chart Here */}
+        </div>
+    )
+}
+
+export default ChartLg1;
