25314f4f3b0a18988b42bea15d3489d7078d176b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 18:09:05 +0800

    Update: System Updating

--- client/src/pages/StaffPages/SStaffAccount.jsx
+++ client/src/pages/StaffPages/SStaffAccount.jsx
@@ -1,12 +1,11 @@
 
-import React from 'react'
 
 function SStaffAccount() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default SStaffAccount
