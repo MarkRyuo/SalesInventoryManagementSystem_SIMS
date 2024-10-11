c19a7600cd33c9e1683497ee56dfdb372ea605ec
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 11:28:39 +0800

    Update: Admin Dashboard

--- client/src/pages/ReportPage.jsx
+++ client/src/pages/ReportPage.jsx
@@ -27,7 +27,7 @@ export const ReportPage = () => {
                 <Col lg={6} md={12}>
                     <ReportChart1 />
                 </Col>
-                <Col lg={5 } md={12}>
+                <Col lg={5} md={12}>
                     <ReportChart2 />
                 </Col>
                 <Col lg={6} md={12}>
