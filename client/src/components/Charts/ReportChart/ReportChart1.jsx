3fe6e4db9b06e577e16bac5bc43ee8bb0a05d338
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 11:01:14 +0800

    Update: Admin Dashboard

--- client/src/components/Charts/ReportChart/ReportChart1.jsx
+++ client/src/components/Charts/ReportChart/ReportChart1.jsx
@@ -1,10 +1,19 @@
-
-function ReportChart1() {
+import Chartcss from './Charts.module.css';
+import { FaReact } from "react-icons/fa";
+//* Chart1 Small
+function Chart1() {
     return (
-        <div>
-            
+        <div className={Chartcss.containerChart1}>
+            <div className={Chartcss.containerText}>
+                <FaReact size={25} />
+                <p className='fs-5 m-0'>Text Here</p>
+            </div>
+            <div className={Chartcss.contentChart}>
+                <p className="fs-4">00000</p>
+                <p>From the running month</p>
+            </div>
         </div>
     )
 }
 
-export default ReportChart1
+export default Chart1;
