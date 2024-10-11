46046bfa53a801d1704cdca726fbf22521286d39
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 08:15:53 +0800

    Update: Admin Dashboard

---
+++ client/src/components/Charts/DashboardChart/Chart2.jsx
@@ -0,0 +1,19 @@
+import Chartcss from './Charts.module.css';
+
+//* Chart1 Small
+function Chart2() {
+    return (
+        <div className={Chartcss.containerContent}>
+            <div>
+                {/* Icon Here */}
+                <p className='fs-4'>Text Here</p>
+            </div>
+            <div className={Chartcss.contentChart}>
+                <p className="fs-4">Chart1</p>
+                <p className="fs-5">From the running month</p>
+            </div>
+        </div>
+    )
+}
+
+export default Chart2;
