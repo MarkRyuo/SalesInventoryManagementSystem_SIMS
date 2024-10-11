765ab5ede6e228ee71f0fa5be0c71ec23bb75456
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 18:01:26 +0800

    Update: System Updating

--- client/src/components/NavBar/NavDashboard.jsx
+++ client/src/components/NavBar/NavDashboard.jsx
@@ -2,7 +2,7 @@ import { Container, Navbar, Button, Offcanvas, Image } from 'react-bootstrap';
 import { useState } from 'react';
 import Navbars from './Navbar.module.css';
 import { Buttons } from './Buttons';
-import AccountDropdown from './AccountDropdown'
+import AccountDropdown from './AccountDropdown' ;
 
 //* Icons
 import { GiHamburgerMenu } from "react-icons/gi";
