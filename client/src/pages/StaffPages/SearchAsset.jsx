4bd1c898daeff78841aebe5131c9532cc6bf6f5a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 19:41:43 +0800

    Update: System Updating

--- client/src/pages/StaffPages/SearchAsset.jsx
+++ client/src/pages/StaffPages/SearchAsset.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 function SearchAsset() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default SearchAsset
