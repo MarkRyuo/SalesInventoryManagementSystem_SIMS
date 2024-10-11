4dfbdb32163c3277d22ef81143c6f62294d6fbd6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 18:19:07 +0800

    Update: System Updating

--- client/src/pages/StaffPages/SStaffAccount.jsx
+++ client/src/pages/StaffPages/SStaffAccount.jsx
@@ -10,7 +10,7 @@ function SStaffAccount() {
             <p className='fs-3'><span><MdAccountBox /></span> Accounts</p>
 
             <div className='contentAccount' style={{ border: "1px solid #e6e6e6", height: "700px", padding: 50, boxShadow: "5px 8px 8px #e6e6e6", background: " #ffffff", borderRadius: "15px" }}>
-                <p className='fs-4'>Staff Details</p>
+                <p className='fs-4'>My Profile</p>
                 <p style={{ borderBottom: "1px solid gray", padding: "20px 10px" }}>Personal Information</p>
                 <StaffAccountComp />
 
