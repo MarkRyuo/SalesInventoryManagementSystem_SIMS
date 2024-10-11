d2281aafb57418e790a0680a2053028108c40d4f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-11 08:23:30 +0800

    Update: Admin Dashboard

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -8,15 +8,14 @@ export const DashboardPage = () => {
     return (
         
         <MainLayout>
-            <Container>
+
                 {/* Dashboard Components */}
                 <p className='fs-3 text-center d-inline'>
                 <span style={{marginRight: "8px"}}><RxDashboard /></span>
                 Dashboard
                 </p>
-
                 
-            </Container>
+
         </MainLayout>
 
 
