280af3614a14269a63cafaa31f8de4fcfe39c60e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-12 21:29:45 +0800

    Update: System Updating

--- client/src/components/LogIn/LoginCard.jsx
+++ client/src/components/LogIn/LoginCard.jsx
@@ -26,7 +26,7 @@ export const LoginCard = () => {
     };
 
     return (
-        
+
         <Form onSubmit={handleLogin}>
             <FloatingLabel controlId="floatingInput" label="Username" className="mb-4">
                 <Form.Control
@@ -58,5 +58,6 @@ export const LoginCard = () => {
                 {loading ? "Loading..." : "Login"}
             </Button>
         </Form>
+        
     );
 };
