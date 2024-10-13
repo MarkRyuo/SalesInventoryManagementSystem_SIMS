0662e887747df89a1990147cdfac35d0790842f0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-13 10:18:10 +0800

    Update: System Updating

--- client/src/components/Account/StaffComp.jsx
+++ client/src/components/Account/StaffComp.jsx
@@ -2,7 +2,7 @@ import {Form} from 'react-bootstrap';
 
 const StaffComp = () => {
     //? Logic 
-
+    //* firstname, lastname, gender, username, password
     return (
         <Form>
             {/* Design Here */}
