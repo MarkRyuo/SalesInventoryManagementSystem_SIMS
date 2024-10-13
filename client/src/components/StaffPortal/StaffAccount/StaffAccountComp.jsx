6654a57274fda762b706a2fa95fd9218e61f32dc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-13 10:19:21 +0800

    Update: System Updating

--- client/src/components/StaffPortal/StaffAccount/StaffAccountComp.jsx
+++ client/src/components/StaffPortal/StaffAccount/StaffAccountComp.jsx
@@ -8,48 +8,7 @@ const StaffAccountComp = () => {
 
     return (
         <Form>
-
-            <Row style={{ width: "100%", margin: 0, padding: 0 }}>
-                <Col lg={6}>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>FIRST NAME</Form.Label>
-                        <Form.Control type="text" />
-                    </Form.Group>
-                </Col>
-                <Col lg={6}>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>LAST NAME</Form.Label>
-                        <Form.Control type="text" />
-                    </Form.Group>
-                </Col>
-            </Row>
-            {/* (Male or Female) */}
-            <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-                style={{ width: "100%", maxWidth: "500px", paddingLeft: 10 }}>
-                <Form.Label>Gender</Form.Label>
-                <Form.Control type="text" />
-            </Form.Group>
-
-            {/* Username */}
-            <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-                style={{ width: "100%", maxWidth: "500px", paddingLeft: 10 }}>
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" />
-            </Form.Group>
-
-            {/* Password */}
-            <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-                style={{ width: "100%", maxWidth: "500px", paddingLeft: 10 }}>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" />
-            </Form.Group>
-
+            {/* Code Here */}
         </Form>
     );
 }
