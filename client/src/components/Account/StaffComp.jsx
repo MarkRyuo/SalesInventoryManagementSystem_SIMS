b367ee83689aa042ce2668d858b5a6894c71c1bc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-13 09:52:27 +0800

    Update: System Updating

--- client/src/components/Account/StaffComp.jsx
+++ client/src/components/Account/StaffComp.jsx
@@ -1,72 +1,11 @@
-import { Row, Form, Col, Button, InputGroup, DropdownButton, Dropdown } from 'react-bootstrap';
-import { useState } from 'react';
+import {Form} from 'react-bootstrap';
 
 const StaffComp = () => {
 
-    const [gender, setGender] = useState('');
-
-    //* Function to handle gender selection
-    const handleGenderSelect = (eventKey) => {
-        setGender(eventKey); // * Update the gender state with the selected value
-    };
 
     return (
         <Form>
-            {/* Row of firstName and lastName */}
-            <Row style={{ width: "100%", margin: 0, padding: 0 }}>
-                <Col lg={6}>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>FIRST NAME</Form.Label>
-                        <Form.Control type="text" />
-                    </Form.Group>
-                </Col>
-                <Col lg={6}>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>LAST NAME</Form.Label>
-                        <Form.Control type="text"/>
-                    </Form.Group>
-                </Col>
-            </Row>
-
-            {/* Dropdown(Male or Female) */}
-            <InputGroup className="mb-3" style={{ width: "100%", maxWidth: "500px", paddingLeft: "11px" }}>
-                <Form.Control
-                    aria-label="Text input with dropdown button"
-                    placeholder={gender || 'Select Gender'}
-                    readOnly
-                />
-                <DropdownButton
-                    variant="outline-secondary"
-                    title="Dropdown"
-                    id="input-group-dropdown-2"
-                    align="end"
-                    onSelect={handleGenderSelect}
-                >
-                    <Dropdown.Item eventKey="Male">Male</Dropdown.Item>
-                    <Dropdown.Item eventKey="Female">Female</Dropdown.Item>
-                </DropdownButton>
-            </InputGroup>
-
-            {/* Username */}
-            <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-                style={{ width: "100%", maxWidth: "500px", paddingLeft: 10 }}>
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" />
-            </Form.Group>
-
-            {/* Password  */}
-            <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-                style={{ width: "100%", maxWidth: "500px", paddingLeft: 10 }}>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password"/>
-            </Form.Group>
             
-            {/* Button Add */}
-            <Button variant='primary' className='ms-2'>Add</Button>
         </Form>
     );
 }
