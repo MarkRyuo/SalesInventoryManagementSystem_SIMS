e1c50737afc00733c5e90646197e98c547d602db
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-13 23:51:42 -0700

    Update: Staff Dashboard

--- client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
+++ client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
@@ -3,7 +3,9 @@ import { TiDocumentAdd } from "react-icons/ti";
 import { useState } from 'react';
 
 function StaffButtons() {
-    const [buttons, setButtons] = 
+    const [buttons, setButtons] = useState([
+        
+    ])
     return (
         <div style={{ width: "100%", height: 100, alignContent: 'center' }}>
             <Button size='lg' variant="outline-success" style={{ margin: 20 }}>
