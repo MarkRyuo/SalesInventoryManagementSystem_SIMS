c8776c8a27e7da018adfdd985734573ddc786df0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-14 06:50:08 -0700

    Update: System Design Staff

--- client/src/components/StaffPortal/AddNewAssets/NewAssetsScanner.jsx
+++ client/src/components/StaffPortal/AddNewAssets/NewAssetsScanner.jsx
@@ -5,12 +5,9 @@ import StaffNavBar from "../StaffNavbar/StaffNavBar";
 function NewAssetsScanner() {
 
     return (
-        <Container fluid className="p-0">
-            <StaffNavBar backBtn={backBtn.filter(Backbtn => Backbtn.id === 1)}/>
-            <Container fluid='lg' className="mt-4" style={{border: "1px solid", width: "100%", height: "auto"}}>
-                <h2>New Assets Scanner</h2>
-            </Container>
-        </Container>
+        <>
+            
+        </>
     )
 }
 
