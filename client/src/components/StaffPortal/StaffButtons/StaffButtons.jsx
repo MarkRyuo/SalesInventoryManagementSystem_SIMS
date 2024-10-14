702be53aba1105ca3030d120a58c95f4a572f10c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-13 23:51:24 -0700

    Update: Staff Dashboard

--- client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
+++ client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
@@ -1,11 +1,12 @@
 import { Button } from 'react-bootstrap';
 import { TiDocumentAdd } from "react-icons/ti";
+import { useState } from 'react';
 
 function StaffButtons() {
-
+    const [buttons, setButtons] = 
     return (
         <div style={{ width: "100%", height: 100, alignContent: 'center' }}>
-            <Button size='lg' variant="outline-success" onClick={handleAddNewAssets} style={{ margin: 20 }}>
+            <Button size='lg' variant="outline-success" style={{ margin: 20 }}>
                 <TiDocumentAdd size={30} /> Add New Assets
             </Button>
         </div>
