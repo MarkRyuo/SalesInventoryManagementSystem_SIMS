6490b9929cb6da6a4107beff2ae113242adb2f10
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-14 02:18:46 -0700

    Update: Staff Dashboard

--- client/src/pages/StaffPages/SDashboard.jsx
+++ client/src/pages/StaffPages/SDashboard.jsx
@@ -10,9 +10,9 @@ import { MdOutlineManageSearch } from "react-icons/md";
 function SDashboard() {
 
     const [buttons, setButtons] = useState([
-        { btnName: "AddNewAssets", btnIcon: <TiDocumentAdd size={30} />, id: 1 },
-        { btnName: "ScanAssets", btnIcon: <BiScan size={30} />, id: 2 },
-        { btnName: "SearchAssets", btnIcon: <MdOutlineManageSearch size={30} />, id: 3 },
+        { btnName: "AddNewAssets", btnIcon: <TiDocumentAdd size={30} />, id: 1, path: "#" },
+        { btnName: "ScanAssets", btnIcon: <BiScan size={30} />, id: 2, path: "#"},
+        { btnName: "SearchAssets", btnIcon: <MdOutlineManageSearch size={30} />, id: 3, path: "#" },
     ])
 
     return (
