f51e2c14bb6ee23d92c8e98a07db87c26054219d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-13 23:58:27 -0700

    Update: Staff Dashboard

--- client/src/components/StaffPortal/AddNewAssets/AddNewAssets.jsx
+++ client/src/components/StaffPortal/AddNewAssets/AddNewAssets.jsx
@@ -1,21 +1,21 @@
-import { useNavigate }  from 'react-router-dom' ;
+import { useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { TiDocumentAdd } from "react-icons/ti";
 
 
 function AddNewAssets() {
 
-    const navigate = useNavigate() ;
-    
+    const navigate = useNavigate();
+
     const handleAddNewAssets = () => navigate("") //* Add a page here 
 
     return (
-        <div style={{width: "100%", height: 100, alignContent: 'center' }}>
-            <Button size='lg' variant="outline-success" onClick={handleAddNewAssets} style={{margin: 20}}>
-                <TiDocumentAdd size={30}/> Add New Assets
+        <div style={{ width: "100%", height: 100, alignContent: 'center' }}>
+            <Button size='lg' variant="outline-success" onClick={handleAddNewAssets} style={{ margin: 20 }}>
+                <TiDocumentAdd size={30} /> Add New Assets
             </Button>
         </div>
     )
 }
 
-export default AddNewAssets ;
+export default AddNewAssets;
--- client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
+++ client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
@@ -5,10 +5,16 @@ import { useState } from 'react';
 function StaffButtons() {
     const [buttons, setButtons] = useState([
         { btnName: "AddNewAssets", btnIcon: <TiDocumentAdd size={30} />, id: 1 },
+        { btnName: "AddNewAssets", btnIcon: <TiDocumentAdd size={30} />, id: 2 },
+        { btnName: "AddNewAssets", btnIcon: <TiDocumentAdd size={30} />, id: 3 },
     ])
     return (
         {buttons.map((button) => (
-            
+            <div key={button.id}>
+                <Button size='lg' variant="outline-success" onClick={handleAddNewAssets} style={{ margin: 20 }}>
+                    <TiDocumentAdd size={30} /> Add New Assets
+                </Button>
+            </div>
         ))}
     )
 }
