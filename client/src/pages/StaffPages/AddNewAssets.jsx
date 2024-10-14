53bfe7cda1555c61fd9a9b1266d6cbb98059e2a3
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-14 06:45:14 -0700

    Update: System Design Staff

--- client/src/pages/StaffPages/AddNewAssets.jsx
+++ client/src/pages/StaffPages/AddNewAssets.jsx
@@ -1,10 +1,25 @@
-
+import { Container } from "react-bootstrap"
+import { useState } from "react"
+import { IoMdArrowBack } from "react-icons/io";
+import StaffNavBar from "../StaffNavbar/StaffNavBar";
 
 function AddNewAssets() {
-    return (
-        <div>
 
-        </div>
+    const [backBtn] = useState([
+        {
+            btnIcon: <IoMdArrowBack size={20} />,
+            path: "/SDashboard",
+            id: 1
+        }
+    ])
+    
+    return (
+        <Container fluid className="p-0">
+            <StaffNavBar backBtn={backBtn.filter(Backbtn => Backbtn.id === 1)} />
+            <Container fluid='lg' className="mt-4" style={{ border: "1px solid", width: "100%", height: "auto" }}>
+                <h2>New Assets Scanner</h2>
+            </Container>
+        </Container>
     )
 }
 
