0f70f2b34fd6f98dea300187107d1be5ed669d5f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-14 06:12:52 -0700

    Update: System Design Staff

--- client/src/components/StaffPortal/AddNewAssets/NewAssetsScanner.jsx
+++ client/src/components/StaffPortal/AddNewAssets/NewAssetsScanner.jsx
@@ -1,10 +1,14 @@
-
+import { Container } from "react-bootstrap"
 //? 1st See
 function NewAssetsScanner() {
     return (
-        <>
-            <h2>New Assets Scanner</h2>
-        </>
+        <Container Fluid>
+            
+            <Container fluid='lg'>
+                <h2>New Assets Scanner</h2>
+
+            </Container>
+        </Container>
     )
 }
 
