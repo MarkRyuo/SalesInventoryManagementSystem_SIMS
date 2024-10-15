fa36e2680548ff80a623cbf0da5e2ff605d31f67
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-15 23:11:43 +0800

    Update: System Design

--- client/src/components/LogIn/ForgotPasswordMode.jsx
+++ client/src/components/LogIn/ForgotPasswordMode.jsx
@@ -55,10 +55,10 @@ function ForgotPasswordMode() {
     return (
         <Container fluid='lg' style={{ width: "100%", height: "auto", marginTop: "200px", justifyContent: 'center', display: "flex" }}>
             <div className="Container-" style={{ boxShadow: '2px 8px 5px #EFF3F4', borderRadius: "20px", padding: "20px", height: "auto", width: "500px", display: "flex", flexDirection: 'column', gap: '10px' }}>
-                <div> {/* Flexbox for alignment */}
-                    <Button variant="link" onClick={() => navigate('/')} style={{ marginRight: '10px' }}>
-                        <FaArrowLeft size={20} /> {/* Back icon */}
-                    </Button>
+                <Button variant="link" onClick={() => navigate('/')} style={{ marginRight: '10px' }}>
+                    <FaArrowLeft size={20} /> {/* Back icon */}
+                </Button>
+                <div>
                     <span><GoShieldLock size={35} /></span>
                     <p className="fs-4 ms-2">Forgot Password</p> {/* Added margin to separate the icon */}
                 </div>
