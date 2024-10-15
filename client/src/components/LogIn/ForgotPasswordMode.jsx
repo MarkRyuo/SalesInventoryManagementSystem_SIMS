2ddfd11e1ab3d62ab8fbbcfd20aca5d72a99c0f5
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-15 21:38:05 +0800

    Update: System Design

--- client/src/components/LogIn/ForgotPasswordMode.jsx
+++ client/src/components/LogIn/ForgotPasswordMode.jsx
@@ -4,30 +4,29 @@ import { GoShieldLock } from "react-icons/go";
 function ForgotPasswordMode() {
     return (
         <Container fluid='lg'>
+            <div className="Container-" style={{border: "1px solid", borderRadius: "15px", padding: "20px"}}>
+                <div>
+                    <span><GoShieldLock size={20}/></span>
+                    <p className="fs-4">ForgotPassword</p>
+                    <p>Text Here</p>
+                </div>
 
+                <div>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Username"
+                        className="mb-3"
+                    >
+                        <Form.Control type="text" placeholder="name@example.com" />
+                            </FloatingLabel>
+                            <FloatingLabel controlId="floatingPassword" label="Username">
+                        <Form.Control type="text" placeholder="Username" />
+                    </FloatingLabel>
+
+                    <Button variant="primary">Verify Username</Button>
+                </div>
+            </div>
         </Container>
-        <div className="Container-" style={{border: "1px solid", borderRadius: "15px", padding: "20px"}}>
-            <div>
-                <span><GoShieldLock size={20}/></span>
-                <p className="fs-4">ForgotPassword</p>
-                <p>Text Here</p>
-            </div>
-
-            <div>
-                <FloatingLabel
-                    controlId="floatingInput"
-                    label="Username"
-                    className="mb-3"
-                >
-                    <Form.Control type="text" placeholder="name@example.com" />
-                        </FloatingLabel>
-                        <FloatingLabel controlId="floatingPassword" label="Username">
-                    <Form.Control type="text" placeholder="Username" />
-                </FloatingLabel>
-
-                <Button variant="primary">Verify Username</Button>
-            </div>
-        </div>
     )
 }
 
