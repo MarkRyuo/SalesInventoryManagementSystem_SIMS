7c9f84d37b108cae29d1fb3236b1e1c8422e0035
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 07:43:41 +0800

    Update: System Design

--- client/src/components/LogIn/ResetRendering.jsx
+++ client/src/components/LogIn/ResetRendering.jsx
@@ -4,7 +4,23 @@ function ResetRendering() {
     return (
         <Container fluid='lg'>
             <div>
-                
+                <h1>Cube Flipping Loader</h1>
+                <div className="cube-wrapper">
+                    <div className="cube-folding">
+                        <span className="leaf1"></span>
+                        <span className="leaf2"></span>
+                        <span className="leaf3"></span>
+                        <span className="leaf4"></span>
+                    </div>
+                    <span className="loading" data-name="Loading">Loading</span>
+                </div>
+
+
+                <div className="made-with-love">
+                    Made with
+                    <i>♥</i> by
+                    <a target="_blank" href="https://codepen.io/nikhil8krishnan">Nikhil Krishnan</a>
+                </div>
             </div>
         </Container>
     )
