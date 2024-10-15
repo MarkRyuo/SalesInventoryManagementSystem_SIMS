ce933f70c6a1f5737e6d6ba5171dbb43f8bad560
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-15 21:24:35 +0800

    Update: System Design

---
+++ client/src/components/LogIn/EnableRecoveryMode.jsx
@@ -0,0 +1,29 @@
+import { Form, FloatingLabel } from "react-bootstrap"
+import { FaBoxOpen } from "react-icons/fa6";
+
+function EnableRecoveryMode() {
+    return (
+        <div className="Container-" style={{ border: "1px solid", borderRadius: "15px", padding: "20px" }}>
+            <div>
+                <span><FaBoxOpen size={20} /></span>
+                <p className="fs-4">ForgotPassword</p>
+                <p>Text Here</p>
+            </div>
+
+            <div>
+                <FloatingLabel
+                    controlId="floatingInput"
+                    label="Username"
+                    className="mb-3"
+                >
+                    <Form.Control type="text" placeholder="name@example.com" />
+                </FloatingLabel>
+                <FloatingLabel controlId="floatingPassword" label="Username">
+                    <Form.Control type="text" placeholder="Username" />
+                </FloatingLabel>
+            </div>
+        </div>
+    )
+}
+
+export default EnableRecoveryMode;
