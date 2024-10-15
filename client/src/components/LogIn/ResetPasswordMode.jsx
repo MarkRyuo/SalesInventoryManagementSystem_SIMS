854fb2a83237d9f5d98270b6abc390186821a634
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-15 21:39:50 +0800

    Update: System Design

--- client/src/components/LogIn/ResetPasswordMode.jsx
+++ client/src/components/LogIn/ResetPasswordMode.jsx
@@ -4,30 +4,29 @@ import { FaUnlock } from "react-icons/fa";
 function ResetPasswordMode() {
     return (
         <Container fluid='lg'>
+            <div className="Container-" style={{ border: "1px solid", borderRadius: "15px", padding: "20px" }}>
+                <div>
+                    <span><FaUnlock size={20}/></span>
+                    <p className="fs-4">Enable Recovery Mode</p>
+                    <p>Text Here</p>
+                </div>
 
+                <div>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Username"
+                        className="mb-3"
+                    >
+                        <Form.Control type="text" placeholder="name@example.com" />
+                    </FloatingLabel>
+                    <FloatingLabel controlId="floatingPassword" label="Username">
+                        <Form.Control type="text" placeholder="Username" />
+                    </FloatingLabel>
+
+                    <Button variant="primary">Reset Password</Button>
+                </div>
+            </div>
         </Container>
-        <div className="Container-" style={{ border: "1px solid", borderRadius: "15px", padding: "20px" }}>
-            <div>
-                <span><FaUnlock size={20}/></span>
-                <p className="fs-4">Enable Recovery Mode</p>
-                <p>Text Here</p>
-            </div>
-
-            <div>
-                <FloatingLabel
-                    controlId="floatingInput"
-                    label="Username"
-                    className="mb-3"
-                >
-                    <Form.Control type="text" placeholder="name@example.com" />
-                </FloatingLabel>
-                <FloatingLabel controlId="floatingPassword" label="Username">
-                    <Form.Control type="text" placeholder="Username" />
-                </FloatingLabel>
-
-                <Button variant="primary">Reset Password</Button>
-            </div>
-        </div>
     )
 }
 
