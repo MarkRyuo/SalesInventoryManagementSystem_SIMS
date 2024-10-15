de8f124aef60d529dbd68a4cbebd03f5337e4684
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 06:57:38 +0800

    Update: System Design

--- client/src/components/LogIn/ForgotPasswordMode.jsx
+++ client/src/components/LogIn/ForgotPasswordMode.jsx
@@ -78,7 +78,7 @@ function ForgotPasswordMode() {
             <div className={ResetModecss.containerContent}>
                 <div>
                     <Button variant="link" onClick={() => navigate('/')} size='lg' className='p-0'>
-                        <FaArrowLeft size={25} /> {/* Back icon */}
+                        <FaArrowLeft size={25} /> 
                     </Button>
                 </div>
                 <div>
