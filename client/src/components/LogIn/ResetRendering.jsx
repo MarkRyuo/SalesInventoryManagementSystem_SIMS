4428cdd36a727ecfe9a691ff9dbec18d120241b9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 07:38:26 +0800

    Update: System Design

--- client/src/components/LogIn/ResetRendering.jsx
+++ client/src/components/LogIn/ResetRendering.jsx
@@ -3,7 +3,9 @@ import { Container } from "react-bootstrap";
 function ResetRendering() {
     return (
         <Container fluid='lg'>
-        
+            <div>
+                
+            </div>
         </Container>
     )
 }
