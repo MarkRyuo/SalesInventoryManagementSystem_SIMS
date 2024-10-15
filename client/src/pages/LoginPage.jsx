4f9c39b7907d5b7d64aeb23d8632e762930e7110
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-15 21:06:59 +0800

    Update: System Design Staff

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -19,13 +19,6 @@ export const LoginPage = () => {
                     <Col lg={12}> <RiBox3Fill size={80} /> </Col>
                     <Col lg={12}> <p className='lead fs-5'>Sales Inventory Management System</p> </Col>
                     <Col lg={12}> <LoginCard /> </Col>
-                    {/* <Col lg={12} className='my-3'>
-                        <p>or</p>
-                        <Button variant="light" size='sm' className={LoginStyle.btnGoogle}>
-                            <FcGoogle size={35} className='me-2'/>
-                            Login with Google
-                        </Button>
-                    </Col> */}
 
                 </Row>
 
