3de31904c464bff4ae292247ca8c1fa477b53d55
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-14 22:16:25 -0700

    Update: System Design Staff

--- client/src/pages/StaffPages/SearchAsset.jsx
+++ client/src/pages/StaffPages/SearchAsset.jsx
@@ -1,5 +1,13 @@
 
 function SearchAsset() {
+    
+    const [backBtn] = useState([
+        {
+            btnIcon: <IoMdArrowBack size={20} />,
+            path: "/SDashboard",
+            id: 1
+        }
+    ]);
     return (
         <>
             <h1>SearchAsset</h1>
