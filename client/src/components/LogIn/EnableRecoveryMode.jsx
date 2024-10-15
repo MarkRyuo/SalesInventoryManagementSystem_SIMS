0cfc838a0b1cdfc356b32e7560c163d65e59de90
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-15 21:39:05 +0800

    Update: System Design

--- client/src/components/LogIn/EnableRecoveryMode.jsx
+++ client/src/components/LogIn/EnableRecoveryMode.jsx
@@ -4,43 +4,42 @@ import { FaLock } from "react-icons/fa";
 function EnableRecoveryMode() {
     return (
         <Container fluid='lg'>
+            <div className="Container-" style={{ border: "1px solid", borderRadius: "15px", padding: "20px" }}>
+                <div>
+                    <span><FaLock  size={20}/></span>
+                    <p className="fs-4">Enable Recovery Mode</p>
+                    <p>Text Here</p>
+                </div>
 
+                <div>
+                    {/* First Question */}
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label=""
+                        className="mb-3"
+                    >
+                        <Form.Control type="text" placeholder="" />
+                            </FloatingLabel>
+                            <FloatingLabel controlId="floatingPassword" label="">
+                        <Form.Control type="text" placeholder="" />
+                    </FloatingLabel>
+
+                    {/* 2nd Question */}
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label=""
+                        className="mb-3"
+                    >
+                        <Form.Control type="text" placeholder="" />
+                            </FloatingLabel>
+                            <FloatingLabel controlId="floatingPassword" label="">
+                        <Form.Control type="text" placeholder="" />
+                    </FloatingLabel>
+
+                    <Button variant="primary">Verify Answers</Button>
+                </div>
+            </div>
         </Container>
-        <div className="Container-" style={{ border: "1px solid", borderRadius: "15px", padding: "20px" }}>
-            <div>
-                <span><FaLock  size={20}/></span>
-                <p className="fs-4">Enable Recovery Mode</p>
-                <p>Text Here</p>
-            </div>
-
-            <div>
-                {/* First Question */}
-                <FloatingLabel
-                    controlId="floatingInput"
-                    label=""
-                    className="mb-3"
-                >
-                    <Form.Control type="text" placeholder="" />
-                        </FloatingLabel>
-                        <FloatingLabel controlId="floatingPassword" label="">
-                    <Form.Control type="text" placeholder="" />
-                </FloatingLabel>
-
-                {/* 2nd Question */}
-                <FloatingLabel
-                    controlId="floatingInput"
-                    label=""
-                    className="mb-3"
-                >
-                    <Form.Control type="text" placeholder="" />
-                        </FloatingLabel>
-                        <FloatingLabel controlId="floatingPassword" label="">
-                    <Form.Control type="text" placeholder="" />
-                </FloatingLabel>
-
-                <Button variant="primary">Verify Answers</Button>
-            </div>
-        </div>
     )
 }
 
