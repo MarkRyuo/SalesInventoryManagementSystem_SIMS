311791f27ba6e5fdf821afa703f5c917b1b797aa
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 08:13:33 +0800

    Update: System Design

--- client/src/components/LogIn/ResetRendering.jsx
+++ client/src/components/LogIn/ResetRendering.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
