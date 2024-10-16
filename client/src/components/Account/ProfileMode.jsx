cb80a6654d524b304de296a8f184b6cb0f3bc44b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 17:43:42 +0800

    Update: System Design

--- client/src/components/Account/ProfileMode.jsx
+++ client/src/components/Account/ProfileMode.jsx
@@ -6,8 +6,8 @@ function ProfileMode() {
 
             <div className="content" style={{border: '1px solid'}}>
                 <div>
-                    <div style={{width: '100%', maxWidth: 200}}>
-                        <Image src="https://i.pinimg.com/564x/01/19/1f/01191fd3ece2dcd44122ff6d88149abc.jpg" roundedCircle />
+                    <div style={}
+                        <Image style={{width: "100%", maxWidth: '100px'}} src="https://i.pinimg.com/564x/01/19/1f/01191fd3ece2dcd44122ff6d88149abc.jpg" roundedCircle />
                     </div>
                     <p>Admin firstname</p>
                     <p>Administrator</p>
