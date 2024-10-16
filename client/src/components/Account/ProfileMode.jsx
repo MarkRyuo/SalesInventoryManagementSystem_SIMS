e7166a6292d7fd1db246dbfe2b27bfc1afc3719b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 17:23:52 +0800

    Update: System Design

--- client/src/components/Account/ProfileMode.jsx
+++ client/src/components/Account/ProfileMode.jsx
@@ -2,7 +2,8 @@ import { Container } from "react-bootstrap"
 
 function ProfileMode() {
     return (
-        <Container fluid='lg'>
+        <Container fluid='lg' style={{border: '1px solid'}}>
+
 
         </Container>
     )
