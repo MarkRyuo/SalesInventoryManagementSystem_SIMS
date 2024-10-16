1a9098dd3773353b551aaf570ef19876d5f59963
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 09:32:33 +0800

    Update: System Design

--- client/src/components/LogIn/SLoginCard.jsx
+++ client/src/components/LogIn/SLoginCard.jsx
@@ -28,7 +28,7 @@ function SLoginCard() {
 
             // Automatically hide the success message and navigate after 2.5 seconds
             setTimeout(() => {
-                setShowSuccess(false);
+                setShowSuccess(true);
                 navigate('/SDashboard'); 
             }, 2500); // Delay for success message display
         } catch (error) {
