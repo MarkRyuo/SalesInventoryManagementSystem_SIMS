14c87c473420126c93354427e82af8334299c3b3
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 17:47:54 +0800

    Update: System Design

--- client/src/components/Account/ProfileMode.jsx
+++ client/src/components/Account/ProfileMode.jsx
@@ -5,15 +5,15 @@ function ProfileMode() {
         <Container fluid='lg' style={{border: '1px solid'}}>
 
             <div className="content" style={{border: '1px solid'}}>
-                <div style={{display: "flex"}}>
+                <div style={{display: "flex", border: '1px solid'}}>
                     <Image style={{width: "100%", maxWidth: '100px'}} src="https://i.pinimg.com/564x/01/19/1f/01191fd3ece2dcd44122ff6d88149abc.jpg" roundedCircle />
                     <div>
                         <p>Admin firstname</p>
                         <p>Administrator</p>
                     </div>
+                    <Button variant="outline-primary" style={{justifySelf: "self-end"}}>Edit Profile</Button>
                 </div>
 
-                <Button variant="outline-primary">Edit Profile</Button>
 
                 <div>
                     <p className="fs-4">Personal Information</p>
