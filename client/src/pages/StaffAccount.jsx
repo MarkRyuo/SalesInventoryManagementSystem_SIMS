1e30d554680f69bdd50f58a66032aeb802c92532
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 10:43:00 +0800

    Update: System Design

--- client/src/pages/StaffAccount.jsx
+++ client/src/pages/StaffAccount.jsx
@@ -14,13 +14,13 @@ export const StaffAccount = () => {
             <div className='contentAccount' style={{ 
                 border: "1px solid #e6e6e6",
                 height: "auto", 
-                padding: 50, 
+                padding: 35, 
                 boxShadow: "5px 8px 8px #e6e6e6", 
                 background: " #ffffff", 
                 borderRadius: "50px" 
             }}>
                 <p className='fs-4'>Staff Management</p>
-                <p style={{ borderBottom: "1px solid gray", padding: "20px 10px" }}>Personal Information</p>
+                <p style={{ borderBottom: "1px solid gray", padding: "15px 10px" }}>Personal Information</p>
                 <StaffComp />
 
             </div>
