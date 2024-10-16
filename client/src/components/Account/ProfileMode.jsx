db17f166481dc823b01f8151759f3a7f4e5a5b67
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 17:26:58 +0800

    Update: System Design

--- client/src/components/Account/ProfileMode.jsx
+++ client/src/components/Account/ProfileMode.jsx
@@ -4,13 +4,16 @@ function ProfileMode() {
     return (
         <Container fluid='lg' style={{border: '1px solid'}}>
 
-            <div className="content">
+            <div className="content" style={{border: '1px solid'}}>
                 <div>
+                    <div>
+                        <Image src="holder.js/171x180" roundedCircle />
+                    </div>
 
                 </div>
 
                 <div>
-                    
+
                 </div>
             </div>
 
