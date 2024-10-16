bc0ee74ab62dbde30af9874d7d3f44a3c310a1ac
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 19:06:23 +0800

    Update: System Design

--- client/src/components/StaffPortal/StaffAccount/StaffAccountMode.jsx
+++ client/src/components/StaffPortal/StaffAccount/StaffAccountMode.jsx
@@ -1,14 +1,14 @@
 import MainStaffLayout from "../../../layout/MainStaffLayout";
 import { Image} from "react-bootstrap";
 
-function ProfileMode() {
+function StaffAccountMode() {
 
     return (
         <MainStaffLayout>
             <div style={{ marginTop: 100, boxShadow: '2px 5px 5px #E1E4E4 ', borderRadius: 15, width: '100%', minWidth: 400 }}>
                 <div className="content">
                             <div style={{ display: "flex", padding: 15 }}>
-                                <Image style={{ width: "100px", marginRight: 10 }} src="https://i.pinimg.com/564x/01/19/1f/01191fd3ece2dcd44122ff6d88149abc.jpg" roundedCircle />
+                        <Image style={{ width: "100px", marginRight: 10 }} src="https://i.pinimg.com/564x/1c/ea/a9/1ceaa9f13fcff14ffcb5236be236972c.jpg" roundedCircle />
                                 <div style={{ display: 'flex', width: '100%', justifyContent: "space-between", alignItems: "center" }}>
                                     <div style={{ display: "flex", flexDirection: 'column' }}>
                                         <p className="fs-4 m-0">{/** Fetch firstname and lastname */}</p>
@@ -33,4 +33,4 @@ function ProfileMode() {
     );
 }
 
-export default ProfileMode;
+export default StaffAccountMode;
