398c87d5e181082ed997696d2522dea0a79c1746
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 19:01:04 +0800

    Update: System Design

--- client/src/components/StaffPortal/StaffAccount/StaffAccountMode.jsx
+++ client/src/components/StaffPortal/StaffAccount/StaffAccountMode.jsx
@@ -1,4 +1,5 @@
 import MainStaffLayout from "../../../layout/MainStaffLayout";
+import { Image,  } from "react-bootstrap";
 
 function ProfileMode() {
 
@@ -6,12 +7,6 @@ function ProfileMode() {
         <MainStaffLayout>
             <div style={{ marginTop: 100, boxShadow: '2px 5px 5px #E1E4E4 ', borderRadius: 15, width: '100%', minWidth: 400 }}>
                 <div className="content">
-                    {loading ? (
-                        <div style={{ textAlign: 'center', padding: '50px' }}>
-                            <p className="fs-4">Loading...</p>
-                        </div>
-                    ) : (
-                        <>
                             <div style={{ display: "flex", padding: 15 }}>
                                 <Image style={{ width: "100px", marginRight: 10 }} src="https://i.pinimg.com/564x/01/19/1f/01191fd3ece2dcd44122ff6d88149abc.jpg" roundedCircle />
                                 <div style={{ display: 'flex', width: '100%', justifyContent: "space-between", alignItems: "center" }}>
@@ -27,14 +22,12 @@ function ProfileMode() {
                                 <p className="fs-4">Personal Information</p>
                                 <hr />
                                 <div style={{ borderRadius: 20, padding: 20, boxShadow: '1px 4px 5px #E1E4E4' }}>
-                                    <p>FirstName: <span>{adminData ? adminData.firstname : 'N/A'}</span></p>
-                                    <p>LastName: <span>{adminData ? adminData.lastname : 'N/A'}</span></p>
-                                    <p>Gender: <span>{adminData ? adminData.gender : 'N/A'}</span></p>
-                                    <p>Username: <span>{adminData ? adminData.username : 'N/A'}</span></p>
+                                    <p>FirstName: <span>{/** */}</span></p>
+                                    <p>LastName: <span>{/** */}</span></p>
+                                    <p>Gender: <span>{/** */}</span></p>
+                                    <p>Username: <span>{/** */}</span></p>
                                 </div>
                             </div>
-                        </>
-                    )}
                 </div>
             </div>
         </MainStaffLayout>
