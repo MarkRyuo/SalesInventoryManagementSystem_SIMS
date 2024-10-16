3c90554a4704f8532449d8ae5110b4cd14638a80
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 18:17:00 +0800

    Update: System Design

--- client/src/components/Account/ProfileMode.jsx
+++ client/src/components/Account/ProfileMode.jsx
@@ -7,7 +7,7 @@ function ProfileMode() {
             <div style={{ border: '1px solid', padding: '20px' }}>
 
                 <div className="content" style={{ border: '1px solid' }}>
-                    <div style={{ display: "flex", border: '1px solid red', padding: 20}}>
+                    <div style={{ display: "flex", padding: 20}}>
                         <Image style={{ width: "100%", maxWidth: '100px' }} src="https://i.pinimg.com/564x/01/19/1f/01191fd3ece2dcd44122ff6d88149abc.jpg" roundedCircle />
                         <div style={{display: 'flex', border: '1px solid', width: '100%', justifyContent: "space-between", alignItems: "center"}}>
                             <div style={{ border: '1px solid', display: "flex", flexDirection: 'column' }}>
@@ -19,7 +19,7 @@ function ProfileMode() {
                     </div>
 
 
-                    <div>
+                    <div style={{padding: 20}}>
                         <p className="fs-4">Personal Information</p>
                         <hr />
                         <p>FirstName: <span></span></p>
