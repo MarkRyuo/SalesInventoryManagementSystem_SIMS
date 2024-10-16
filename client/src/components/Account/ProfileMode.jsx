d12f97ca6842a8e28202b184fa9a1df3a846307e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 18:01:23 +0800

    Update: System Design

--- client/src/components/Account/ProfileMode.jsx
+++ client/src/components/Account/ProfileMode.jsx
@@ -3,30 +3,32 @@ import { MainLayout } from "../../layout/MainLayout"
 
 function ProfileMode() {
     return (
-        <Container fluid='lg' style={{border: '1px solid'}}>
+        <MainLayout>
+            <Container fluid='lg' style={{ border: '1px solid' }}>
 
-            <div className="content" style={{border: '1px solid'}}>
-                <div style={{display: "flex", border: '1px solid'}}>
-                    <Image style={{width: "100%", maxWidth: '100px'}} src="https://i.pinimg.com/564x/01/19/1f/01191fd3ece2dcd44122ff6d88149abc.jpg" roundedCircle />
-                    <div>
-                        <p>Admin firstname</p>
-                        <p>Administrator</p>
+                <div className="content" style={{ border: '1px solid' }}>
+                    <div style={{ display: "flex", border: '1px solid' }}>
+                        <Image style={{ width: "100%", maxWidth: '100px' }} src="https://i.pinimg.com/564x/01/19/1f/01191fd3ece2dcd44122ff6d88149abc.jpg" roundedCircle />
+                        <div>
+                            <p>Admin firstname</p>
+                            <p>Administrator</p>
+                        </div>
+                        <Button variant="outline-primary" style={{ justifySelf: "flex-end" }}>Edit Profile</Button>
                     </div>
-                    <Button variant="outline-primary" style={{justifySelf: "flex-end"}}>Edit Profile</Button>
+
+
+                    <div>
+                        <p className="fs-4">Personal Information</p>
+                        <hr />
+                        <p>FirstName: <span></span></p>
+                        <p>LastName: <span></span></p>
+                        <p>Gender: <span></span></p>
+                        <p>Username: <span></span></p>
+                    </div>
                 </div>
 
-
-                <div>
-                    <p className="fs-4">Personal Information</p>
-                    <hr/>
-                    <p>FirstName: <span></span></p>
-                    <p>LastName: <span></span></p>
-                    <p>Gender: <span></span></p>
-                    <p>Username: <span></span></p>
-                </div>
-            </div>
-
-        </Container>
+            </Container>
+        </MainLayout>
     )
 }
 
