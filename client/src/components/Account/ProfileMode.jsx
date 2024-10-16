02ce7c86dcbba1b7f66f148b807d9d362b2a166e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-16 18:12:33 +0800

    Update: System Design

--- client/src/components/Account/ProfileMode.jsx
+++ client/src/components/Account/ProfileMode.jsx
@@ -7,15 +7,15 @@ function ProfileMode() {
             <div style={{ border: '1px solid', padding: '20px' }}>
 
                 <div className="content" style={{ border: '1px solid' }}>
-                    <div style={{ display: "flex", border: '1px solid red', padding: 20, alignContent: 'center', justifyContent: "space-between" }}>
-                        <div style={{display: 'flex'}}>
-                            <Image style={{ width: "100%", maxWidth: '100px' }} src="https://i.pinimg.com/564x/01/19/1f/01191fd3ece2dcd44122ff6d88149abc.jpg" roundedCircle />
+                    <div style={{ display: "flex", border: '1px solid red', padding: 20}}>
+                        <Image style={{ width: "100%", maxWidth: '100px' }} src="https://i.pinimg.com/564x/01/19/1f/01191fd3ece2dcd44122ff6d88149abc.jpg" roundedCircle />
+                        <div style={{display: 'flex', border: '1px solid', width: '100%', justifyContent: "space-between", alignItems: "center"}}>
                             <div style={{ border: '1px solid', display: "flex", height: '70px', flexDirection: 'column' }}>
                                 <p>Admin firstname</p>
                                 <p>Administrator</p>
                             </div>
+                            <Button variant="outline-primary" style={{height: 50}}>Edit Profile</Button>
                         </div>
-                        <Button variant="outline-primary" style={{height: 50}}>Edit Profile</Button>
                     </div>
 
 
