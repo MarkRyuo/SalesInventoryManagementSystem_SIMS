09706ffae28610743e1d156cf0d3eb65c798810b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-18 10:25:31 +0800

    Update: System

--- client/src/components/StaffPortal/Cards/CardProduct.jsx
+++ client/src/components/StaffPortal/Cards/CardProduct.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react"
+import { LiaProductHunt } from "react-icons/lia";
 
 function CardProduct() {
 
     const [cardProduct] = useState([
-        
+        {
+            ProductIcon: <LiaProductHunt /> ,
+
+        }
     ])
 
     return (
