a4a3739ca40347decffab1ff1b01fd7a6876eefd
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-18 09:55:11 +0800

    Update: System

--- client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
+++ client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
@@ -6,9 +6,8 @@ function StaffButtons({buttons}) {
     return (
         <>
             {buttons.map((button) => (
-                <div key={button.id}>
+                <div key={button.id} style={{ display: "flex", flexDirection: "column", padding: 20, boxShadow: '2px 2px 5px #e2dfdf', borderRadius: 20, width: 250, height: 150 }}>
                     <Link to={button.path}>
-                        <span>{button.btnIcon}</span> 
                         {button.btnName}
                     </Link>
                 </div>
