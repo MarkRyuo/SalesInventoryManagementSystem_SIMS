6b92629efb87cc2e2fa9a9ce52420a1d638ca82a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-18 08:00:04 +0800

    Update: System

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -16,32 +16,34 @@ export const DashboardPage = () => {
 
         <MainLayout>
 
-            {/* Dashboard Components */}
-            <p className='fs-3 text-center d-inline'>
-                <span style={{ marginRight: "8px" }}><RxDashboard /></span>
-                Dashboard
-            </p>
+            <div>
+                {/* Dashboard Components */}
+                <p className='fs-3 text-center d-inline'>
+                    <span style={{ marginRight: "8px" }}><RxDashboard /></span>
+                    Dashboard
+                </p>
 
-            <Row className={DashboardCss.rowContainer} > {/* Fix width(sm-screen) 400px */}
-                <Col className='colContainer' sm={12} md={12} lg={3}>
-                    <Chart1 />
-                </Col>
-                <Col className='colContainer' sm={12} md={12} lg={5}>
-                    <Chart2 />
-                </Col>
-                <Col className='colContainer' sm={12} md={12} lg={3}>
-                    <Chart3 />
-                </Col>
-            </Row>
+                <Row className={DashboardCss.rowContainer} > {/* Fix width(sm-screen) 400px */}
+                    <Col className='colContainer' sm={12} md={12} lg={3}>
+                        <Chart1 />
+                    </Col>
+                    <Col className='colContainer' sm={12} md={12} lg={5}>
+                        <Chart2 />
+                    </Col>
+                    <Col className='colContainer' sm={12} md={12} lg={3}>
+                        <Chart3 />
+                    </Col>
+                </Row>
 
-            <Row className={DashboardCss.rowContainerLg}>
-                <Col className={DashboardCss.colContainerLg} sm={12} lg={6}>
-                    <ChartLg1 />
-                </Col>
-                <Col className={DashboardCss.colContainerLg} lg={5} md={9} sm={10} xs={10}>
-                    <ChartLg2 />
-                </Col>  
-            </Row>
+                <Row className={DashboardCss.rowContainerLg}>
+                    <Col className={DashboardCss.colContainerLg} sm={12} lg={6}>
+                        <ChartLg1 />
+                    </Col>
+                    <Col className={DashboardCss.colContainerLg} lg={5} md={9} sm={10} xs={10}>
+                        <ChartLg2 />
+                    </Col>
+                </Row>
+            </div>
 
         </MainLayout>
 
