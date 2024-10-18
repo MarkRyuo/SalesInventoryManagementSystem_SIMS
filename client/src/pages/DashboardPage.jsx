1a701e23416d1da270b96cf8e8dd20cc1a9d1f67
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-18 08:15:26 +0800

    Update: System

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -17,7 +17,7 @@ export const DashboardPage = () => {
 
             <div className={DashboardCss.mainComponent}>
                 {/* Dashboard Components */}
-                <div>
+                <div className={DashboardCss.componentHeroCard}>
                     <Image src="https://i.pinimg.com/control/564x/6a/61/32/6a6132119767a37330924720a5733a96.jpg" roundedCircle style={{width: 100}} />
                     <p className='fs-4'>Hello, <span>Admin Name{/* Fetch admin firstname & lastname */}</span></p>
                     <p>Date: <span>{/* Realtime Date */}</span></p>
