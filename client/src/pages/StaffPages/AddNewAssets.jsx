4ab0ad8726ceef248cb7b1f0beca2a4d390b3a33
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-18 22:15:57 +0800

    Update: System

--- client/src/pages/StaffPages/AddNewAssets.jsx
+++ client/src/pages/StaffPages/AddNewAssets.jsx
@@ -1,44 +1,65 @@
-import { Button, Container } from "react-bootstrap"
-import { useState } from "react"
+import { Button, Container } from "react-bootstrap";
+import { useState } from "react";
 import { IoMdArrowBack } from "react-icons/io";
 import StaffNavBar from "../../components/StaffPortal/StaffNavbar/StaffNavBar";
 import { MdQrCodeScanner } from "react-icons/md";
 import { Link } from "react-router-dom";
 
-
 function AddNewAssets() {
-
     const [backBtn] = useState([
         {
             btnIcon: <IoMdArrowBack size={20} />,
             path: "/SDashboard",
             id: 1
         }
-    ]) ;
+    ]);
 
     return (
         <Container fluid className="p-0">
             <StaffNavBar backBtn={backBtn.filter(Backbtn => Backbtn.id === 1)} />
-            <Container fluid='md' className="mt-4 p-0">
-                <div style={{display: "flex", justifyContent: "center", flexDirection: 'column', alignItems: "center", minWidth: 380}}>
-                    <div style={{width: '100%', maxWidth: "400px", textAlign: "center", padding: 30, borderRadius: 20, boxShadow: '2px 2px 5px #E1E4E4',}}>
-                        <p className="fs-4 m-0">Scan Product</p>
-                        <p className="">Scan new products to add them to your inventory. Click the Scan button below to start scanning using the QR code scanner.</p>
+            <Container fluid="md" className="mt-4 p-0">
+                <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", minWidth: 380 }}>
+                    <div
+                        style={{
+                            width: '100%',
+                            maxWidth: "400px",
+                            textAlign: "center",
+                            padding: "20px 30px",
+                            borderRadius: 20,
+                            boxShadow: "2px 2px 5px #E1E4E4",
+                            marginBottom: 20
+                        }}
+                    >
+                        <p className="fs-4 m-0" style={{ fontWeight: 600 }}>Scan QR Code & Barcode</p>
+                        <p className="m-0" style={{ marginTop: 10 }}>
+                            Scan new products to add them to your inventory. Click the Scan button below to start scanning using the QR code scanner.
+                        </p>
                     </div>
-                    <div style={{ height: "auto", width: "100%", padding: 20, textAlign: 'center',display: 'flex', justifyContent: 'center'}}>
-                        <div style={{ width: 400, boxShadow: '2px 2px 5px #E1E4E4', borderRadius: 20, padding: 30}}>
-                            <div style={{textAlign: 'center'}}>
-                                <MdQrCodeScanner size={300} style={{ color: '#688DCE', width: 300 }} />
-                            </div>
-                            <Button as={Link} to={'/NewAssetsScanner'} size="lg" className="my-5" style={{ width: '100%' }}>
-                                Scan
-                            </Button>
-                        </div>
+                    <div
+                        style={{
+                            width: "100%",
+                            maxWidth: "400px",
+                            padding: 20,
+                            boxShadow: "2px 2px 5px #E1E4E4",
+                            borderRadius: 20,
+                            textAlign: "center"
+                        }}
+                    >
+                        <MdQrCodeScanner size={300} style={{ color: "#688DCE" }} />
+                        <Button
+                            as={Link}
+                            to={'/NewAssetsScanner'}
+                            size="lg"
+                            className="my-4"
+                            style={{ width: '100%' }}
+                        >
+                            Scan
+                        </Button>
                     </div>
                 </div>
             </Container>
         </Container>
-    )
+    );
 }
 
 export default AddNewAssets;
