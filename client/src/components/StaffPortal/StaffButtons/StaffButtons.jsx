022fe61d298832c5d7c5f277c517703690c2938a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-18 10:18:16 +0800

    Update: System

--- client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
+++ client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
@@ -9,9 +9,9 @@ function StaffButtons({buttons}) {
                     <Link 
                         key={button.id} 
                         to={button.path} 
-                        style={{ display: "flex", flexDirection: "column", padding: 20, boxShadow: '2px 2px 5px #e2dfdf', borderRadius: 20, width: 250, height: 150, justifyContent: 'center', color: 'black', textDecoration: 'none', borderLeft: '1px solid green' }}>
-                        <span className='text-center'>{button.btnIcon}</span>
-                        <span className='text-center'>{button.btnName}</span>
+                    style={{ display: "flex", flexDirection: "column", padding: 20, boxShadow: '2px 2px 5px #e2dfdf', borderRadius: 20, width: 250, height: 150, justifyContent: 'center', color: 'black', textDecoration: 'none', borderLeft: '1px solid #92E3B8' }}>
+                    <span className='text-center' style={{ color: '#92E3B8' }}>{button.btnIcon}</span>
+                        <span className='text-center' >{button.btnName}</span>
                     </Link>
             ))}
         </>
