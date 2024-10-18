ec456525f0fc845efdb259045f12c5fb8bbf0253
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-18 10:50:38 +0800

    Update: System

--- client/src/components/StaffPortal/Cards/CardProduct.jsx
+++ client/src/components/StaffPortal/Cards/CardProduct.jsx
@@ -8,7 +8,7 @@ function CardProduct({cardProduct}) {
             {cardProduct.map((cardProducts) => (
                 <div 
                     key={cardProducts.id} 
-                    style={{border: '1px solid', width: 300, padding: 20, boxShadow: '1px 1px 5px ', borderRadius: 15, flexShrink: 0}}>
+                    style={{border: '1px solid', width: 250, padding: 20, boxShadow: '1px 1px 5px ', borderRadius: 15, flexShrink: 0}}>
                     <span>{cardProducts.productIcon}</span>
                     <p className="fs-5 m-0">{cardProducts.productName}</p>
                     <p>{cardProducts.productValue}</p>
