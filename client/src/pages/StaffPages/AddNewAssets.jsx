0c01f17805bd93a3f94d9d9f911e5cc53694c31c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-18 21:48:31 +0800

    Update: System

--- client/src/pages/StaffPages/AddNewAssets.jsx
+++ client/src/pages/StaffPages/AddNewAssets.jsx
@@ -20,6 +20,8 @@ function AddNewAssets() {
         <Container fluid className="p-0">
             <StaffNavBar backBtn={backBtn.filter(Backbtn => Backbtn.id === 1)} />
             <Container fluid='md' className="mt-4" style={{width: "100%", height: "auto", padding: 0, display: 'flex', justifyContent: 'center', marginTop: 100}}>
+                <div></div>
+                <p className="fs-5">Scan new products to add them to your inventory. Click the Scan button below to start scanning using the QR code scanner.</p>                    
                 <div style={{height: "600px", width: "auto", padding: 20 ,textAlign: 'center'}}>
                     <div style={{width: 'auto', textAlign: 'center', padding: 30, boxShadow: '2px 2px 5px #E1E4E4', borderRadius: 20}}>
                         <MdQrCodeScanner size={300} style={{ color: '#688DCE', width: 300}} />
