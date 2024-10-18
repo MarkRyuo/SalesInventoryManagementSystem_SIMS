3bfc442246665ad96bbda943bb42db145f9c3d66
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-18 21:19:37 +0800

    Update: System

--- client/src/pages/StaffPages/AddNewAssets.jsx
+++ client/src/pages/StaffPages/AddNewAssets.jsx
@@ -22,8 +22,8 @@ function AddNewAssets() {
             <Container fluid='md' className="mt-4" style={{width: "100%", height: "auto", padding: 0, display: 'flex', justifyContent: 'center', marginTop: 100}}>
                 <div style={{height: "600px", width: "auto", padding: 20 ,textAlign: 'center', border: '1px solid'}}>
                     <p className="fs-5">Text Here</p>
-                    <div style={{width: '400px', textAlign: 'center', padding: 30, boxShadow: '2px 2px 5px #E1E4E4', borderRadius: 20}}>
-                        <MdQrCodeScanner size={300} style={{ color: '#688DCE'}} />
+                    <div style={{width: 'auto', textAlign: 'center', padding: 30, boxShadow: '2px 2px 5px #E1E4E4', borderRadius: 20}}>
+                        <MdQrCodeScanner size={300} style={{ color: '#688DCE', maxwidth: 300}} />
                     </div>
                     <Button as={Link} to={'/'} size="lg">
                         Scan
