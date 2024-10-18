ac0259c8c3255e52b10ea7eb26be7c28dc15d796
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-18 08:07:36 +0800

    Update: System

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -18,7 +18,7 @@ export const DashboardPage = () => {
             <div className='main-component'>
                 {/* Dashboard Components */}
                 <div>
-                    <Image src="holder.js/171x180" roundedCircle style={{width: 150}} />
+                    <Image src="https://i.pinimg.com/control/564x/6a/61/32/6a6132119767a37330924720a5733a96.jpg" roundedCircle style={{width: 150}} />
                 </div>
 
                 <Row className={DashboardCss.rowContainer} > {/* Fix width(sm-screen) 400px */}
