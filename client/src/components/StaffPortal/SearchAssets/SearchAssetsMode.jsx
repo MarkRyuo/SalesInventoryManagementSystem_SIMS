9fb895030110200f3a8d07a12a7a0464652a3804
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-20 15:13:14 +0800

    Update: System

--- client/src/components/StaffPortal/SearchAssets/SearchAssetsMode.jsx
+++ client/src/components/StaffPortal/SearchAssets/SearchAssetsMode.jsx
@@ -4,7 +4,18 @@ import { Form, Button } from "react-bootstrap"
 function SearchAssetsMode() {
     return (
         <div style={{ border: "1px solid", width: "100%", height: "auto" }}>
-            <div>
+            <div style={{display: 'inline-flex'}}>
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Dropdown Button
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
                 <Form className="d-flex">
                     <Form.Control
                         type="search"
