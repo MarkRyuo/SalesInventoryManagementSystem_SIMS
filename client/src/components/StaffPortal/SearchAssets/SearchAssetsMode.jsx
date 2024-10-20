ea2ffe43545afad33e8113c3899b524bd2839046
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-20 15:11:07 +0800

    Update: System

--- client/src/components/StaffPortal/SearchAssets/SearchAssetsMode.jsx
+++ client/src/components/StaffPortal/SearchAssets/SearchAssetsMode.jsx
@@ -1,10 +1,19 @@
+import { Form, Button } from "react-bootstrap"
 
 //* Search Product
 function SearchAssetsMode() {
     return (
         <div style={{ border: "1px solid", width: "100%", height: "auto" }}>
             <div>
-                
+                <Form className="d-flex">
+                    <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
             </div>
             <div>
 
