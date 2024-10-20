dc0b4cb882c4a65d130c9d4561d1e473cd107303
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-20 15:16:58 +0800

    Update: System

---
+++ client/src/components/StaffPortal/SearchAssets/SearchAssetsMode.jsx
@@ -0,0 +1,36 @@
+import { Form, Button, Dropdown } from "react-bootstrap"
+
+//* Search Product
+function SearchAssetsMode() {
+    return (
+        <div style={{ border: "1px solid", width: "100%", height: "auto" }}>
+            <div style={{display: 'flex', gap: '30px', flexDirection: 'column-reverse'}}>
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Choose
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#">Action</Dropdown.Item>
+                        <Dropdown.Item href="#">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#">Something else</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Form className="d-flex" style={{width: 400}}>
+                    <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default SearchAssetsMode
