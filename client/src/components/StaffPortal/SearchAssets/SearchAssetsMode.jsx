b77c0218aa56cfe986fb73e098dfb89e04dfdc18
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-20 15:03:30 +0800

    Update: System

--- client/src/components/StaffPortal/SearchAssets/SearchAssetsMode.jsx
+++ client/src/components/StaffPortal/SearchAssets/SearchAssetsMode.jsx
@@ -3,7 +3,7 @@
 function SearchAssetsMode() {
     return (
         <div style={{ border: "1px solid", width: "100%", height: "auto" }}>
-            
+            <div></div>
         </div>
     )
 }
