a1448cb0affea878ad75f7b444f6883a4f250feb
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-21 21:05:36 +0800

    Update: System

--- client/src/components/StaffPortal/AddNewAssets/NewAssetsScanner.jsx
+++ client/src/components/StaffPortal/AddNewAssets/NewAssetsScanner.jsx
@@ -5,10 +5,10 @@ function NewAssetsScanner() {
 
     return (
         <Container fluid className="p-0">
-            <Container fluid='md' className='' style={{ border: '1px solid green', minWidth: 380}}>
+            <Container fluid='sm' className='' style={{ border: '1px solid green', minWidth: 400, padding: 2}}>
                 <div style={{border: '1px solid red', padding: 0}}>
                     <div style={{border: '1px solid', width: 'auto'}}>
-                        <video style={{ width: '600px', height: '80vh', border: '1px solid red'}} />
+                        <video style={{ width: '100%', height: '80vh', border: '1px solid red'}} />
                         {/* Camera Here */}
                         <p className="fs-6">Place a Barcode/QRcode inside the rectangle to scan it</p>
                     </div>
