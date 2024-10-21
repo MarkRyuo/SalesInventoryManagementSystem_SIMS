25856e469748b55ee084ac6b66b8109f71f01056
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-21 20:01:15 +0800

    Update: System

--- client/src/components/StaffPortal/AddNewAssets/NewAssets.jsx
+++ client/src/components/StaffPortal/AddNewAssets/NewAssets.jsx
@@ -1,10 +1,11 @@
+import { Container } from "react-bootstrap";
 
 //? 2nd See Add Product Details of the product 
 function NewAssets() {
     return (
-        <>
-            <h1>Add New Assets</h1>
-        </>
+        <Container>
+            
+        </Container>
     )
 }
 
