e230a359105ced03560d518146b89a6592a889e7
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-25 22:12:16 +0800

    Update: System

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -81,7 +81,7 @@ export const DashboardPage = () => {
                     ) : (
                         <>
                             <Image
-                                src="https://i.pinimg.com/control/564x/6a/61/32/6a6132119767a37330924720a5733a96.jpg"
+                                src="/ReyesElectronicsLogo.png"
                                 roundedCircle
                                 style={{ width: '100%', maxWidth: '100px', height: '100px' }}
                             />
