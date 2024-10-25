0e297b253b514a8c276f8b3f68591fb747b3a9e0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-25 18:23:18 +0800

    Update: System

--- client/src/components/StaffPortal/SearchAssets/SearchAssetsMode.jsx
+++ client/src/components/StaffPortal/SearchAssets/SearchAssetsMode.jsx
@@ -145,7 +145,7 @@ function SearchAssetsMode() {
                                     <Card.Title>
                                         <div className="d-inline-flex gap-2" style={{border: '1px solid'}}>
                                             <p className="m-0 p-0">{product.productName}</p>
-                                            <p style={{ color: statusColor, marginTop: '10px' }}>
+                                            <p style={{ color: statusColor}} className="p-0 m-0">
                                                 <span style={{ color: statusColor }}>•</span>{stockStatus} {/* Display stock status */}
                                             </p>
                                         </div>
