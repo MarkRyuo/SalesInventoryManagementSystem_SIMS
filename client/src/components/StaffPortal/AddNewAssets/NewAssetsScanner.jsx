3794ec7559db98ef444e58fc8652e2bfd69384ff
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-26 16:43:00 +0800

    Update: System

--- client/src/components/StaffPortal/AddNewAssets/NewAssetsScanner.jsx
+++ client/src/components/StaffPortal/AddNewAssets/NewAssetsScanner.jsx
@@ -114,7 +114,7 @@ function NewAssetsScanner() {
             <StaffNavBar backBtn={backBtn.filter(Backbtn => Backbtn.id === 1)} />
             <Container fluid='lg' style={{ width: '100%', height: '80vh', boxSizing: 'border-box' }}>  {/* Parent */}
                 <Row className="justify-content-center" style={{ height: '100%', boxSizing: 'border-box' }}> {/* Sub parent */}
-                    <Col md={8} className='p-0' style={{ border: '1px solid red', display: 'flex', justifyContent: 'center' }}> {/* Child */}
+                    <Col md={8} className='p-0' style={{ display: 'flex', justifyContent: 'center' }}> {/* Child */}
                         <Card style={{ border: '1px solid green', height: '100%', display: 'flex', justifyContent: 'center' }}>
                             <div className="text-center position-relative">
                                 {error && (
