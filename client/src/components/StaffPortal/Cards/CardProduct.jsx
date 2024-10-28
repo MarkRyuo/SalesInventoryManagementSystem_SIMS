35cf902516e921458ae06afc2638b931d67f82ca
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-28 09:33:31 +0800

    Update: System

--- client/src/components/StaffPortal/Cards/CardProduct.jsx
+++ client/src/components/StaffPortal/Cards/CardProduct.jsx
@@ -13,7 +13,8 @@ function CardProduct({ cardProduct }) {
                         padding: 20, 
                         boxShadow: '1px 1px 5px #e2dfdf', 
                         borderRadius: 15, 
-                        flexShrink: 0, borderLeft: '2px solid #92E3B8' }}>
+                        flexShrink: 0, 
+                        borderLeft: '2px solid #92E3B8' }}>
                     <span>{cardProducts.productIcon}</span>
                     <p className="fs-5 m-0">{cardProducts.productName}</p>
                     <p>{cardProducts.productValue}</p>
