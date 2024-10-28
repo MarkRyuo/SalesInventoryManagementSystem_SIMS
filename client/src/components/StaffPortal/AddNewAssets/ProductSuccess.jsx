7c7eeb25e8419caa33006c25b537674946e3e964
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-28 15:34:57 +0800

    Update: SystemA

--- client/src/components/StaffPortal/AddNewAssets/ProductSuccess.jsx
+++ client/src/components/StaffPortal/AddNewAssets/ProductSuccess.jsx
@@ -1,10 +1,14 @@
+import { Container } from 'react-bootstrap';
+import { FaCheckCircle } from 'react-icons/fa'; // Import the check icon
 
 function ProductSuccess() {
     return (
-        <div>
-            <h2 className="text-center mt-4">Product successfully added!</h2>
-        </div>
-    )
+        <Container style={{ textAlign: 'center', marginTop: '5rem' }}>
+            <FaCheckCircle style={{ fontSize: '100px', color: 'green' }} />
+            <h2 style={{ marginTop: '1.5rem' }}>Product successfully added!</h2>
+            <p>Your product has been added to the inventory.</p>
+        </Container>
+    );
 }
 
 export default ProductSuccess;
