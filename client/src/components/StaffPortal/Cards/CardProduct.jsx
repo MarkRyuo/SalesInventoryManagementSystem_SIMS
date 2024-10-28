3aef6899433e791c543ddf72a5705285c54c1a12
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-28 09:39:31 +0800

    Update: System

--- client/src/components/StaffPortal/Cards/CardProduct.jsx
+++ client/src/components/StaffPortal/Cards/CardProduct.jsx
@@ -19,6 +19,7 @@ function CardProduct({ cardProduct }) {
                     <span>{cardProducts.productIcon}</span>
                     <p className="fs-6 m-0">{cardProducts.productName}</p>
                     <p className="fs-6 m-0">SKU: {cardProducts.productValue}</p>
+                    <p>barcode:{cardProducts.barcode}</p>
                 </div>
             ))}
         </>
