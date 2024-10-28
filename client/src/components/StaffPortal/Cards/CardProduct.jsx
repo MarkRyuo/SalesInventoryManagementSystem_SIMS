bcdf36bc9e5c5b10397494d17cef5a2f7658020f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-28 10:07:04 +0800

    Update: SystemA

--- client/src/components/StaffPortal/Cards/CardProduct.jsx
+++ client/src/components/StaffPortal/Cards/CardProduct.jsx
@@ -8,13 +8,12 @@ function CardProduct({ cardProduct }) {
                     key={cardProducts.id}
                     style={{
                         width: 'auto',
-                        padding: 25,
+                        padding: 20,
                         boxShadow: '1px 1px 5px #e2dfdf',
                         borderRadius: 15,
                         flexShrink: 0,
                         borderLeft: '2px solid #92E3B8',
-                        boxSizing: "border-box"
-                    }}
+                        boxSizing: "border-box",                    }}
                 >
                     <span>{cardProducts.productIcon}</span>
                     <p className="fs-5 m-0">{cardProducts.productName}</p>
