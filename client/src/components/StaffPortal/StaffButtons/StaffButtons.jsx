b9abb8b6d29e040eb7ad76d86e6ca3ef6d0feac6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-28 09:55:57 +0800

    Update: System

--- client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
+++ client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
@@ -15,7 +15,9 @@ function StaffButtons({buttons}) {
                             padding: 20, boxShadow: '2px 2px 5px #e2dfdf', 
                             borderRadius: 20, 
                             width: 250, 
-                            height: 150, justifyContent: 'center', color: 'black', textDecoration: 'none', borderLeft: '1px solid #92E3B8' }}>
+                            height: 150, 
+                            justifyContent: 'center', 
+                            color: 'black', textDecoration: 'none', borderLeft: '1px solid #92E3B8' }}>
                     <span className='text-center' style={{ color: '#92E3B8' }}>{button.btnIcon}</span>
                         <span className='text-center'>{button.btnName}</span>
                     </Link>
