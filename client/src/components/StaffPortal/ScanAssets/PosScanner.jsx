63dafafed397e54da608cfd16e1dd9e43871e567
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-29 10:54:16 +0800

    Subject: Update

--- client/src/components/StaffPortal/ScanAssets/PosScanner.jsx
+++ client/src/components/StaffPortal/ScanAssets/PosScanner.jsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap"
 //* Scanner Code 
 function PosScanner() {
     return (
-        <Container>
+        <Container fluid>
             
         </Container>
     )
