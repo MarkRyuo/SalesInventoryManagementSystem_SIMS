5195055d400b67e815234492f2af2c49d28bace8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-29 16:23:43 +0800

    Subject: Update

--- client/src/components/StaffPortal/ScanAssets/NavbarCurrentOrder.jsx
+++ client/src/components/StaffPortal/ScanAssets/NavbarCurrentOrder.jsx
@@ -1,21 +1,13 @@
 import { Container } from "react-bootstrap";
-import { useState } from "react";
+
 function NavbarCurrentOrder() {
-
-    const [Order] = useState([
-        
-    ])
-
     return (
-        <>
-
-        </>
-        // <Container fluid>
-        //     <div>
-        //         <p className="fs-4">Current Order</p>
-        //         {/* Button direct scan if customer add another product to cashier */}
-        //     </div>
-        // </Container>
+        <Container fluid>
+            <div>
+                <p className="fs-4">Current Order</p>
+                {/* Button direct scan if customer add another product to cashier */}
+            </div>
+        </Container>
     )
 }
 
