681bcf5f1dd06aa6e3ee48f610200c9d7676ff83
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-29 16:36:58 +0800

    Subject: Update

--- client/src/components/StaffPortal/ScanAssets/ScanAssetsMode.jsx
+++ client/src/components/StaffPortal/ScanAssets/ScanAssetsMode.jsx
@@ -1,11 +1,16 @@
-import { Container } from "react-bootstrap";
+import { Container, Navbar } from "react-bootstrap";
 
 //* Inputs
 function ScanAssetsMode() {
     return (
         <Container fluid className="m-0 p-0">
+            <Navbar className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand>Brand text</Navbar.Brand>
+                </Container>
+            </Navbar>
             <Container fluid='md'>
-                
+
             </Container>
             
         </Container>
