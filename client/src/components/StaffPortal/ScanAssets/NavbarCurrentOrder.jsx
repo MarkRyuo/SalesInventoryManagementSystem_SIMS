01f1b7befcba3ff2e6c4c19fb60493445f1c9ddf
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-29 16:11:42 +0800

    Subject: Update

--- client/src/components/StaffPortal/ScanAssets/NavbarCurrentOrder.jsx
+++ client/src/components/StaffPortal/ScanAssets/NavbarCurrentOrder.jsx
@@ -1,9 +1,12 @@
+import { Container } from "react-bootstrap";
 
 function NavbarCurrentOrder() {
     return (
-        <>
-
-        </>
+        <Container fluid>
+            <div>
+                <p className="fs-4">Current Order</p>
+            </div>
+        </Container>
     )
 }
 
