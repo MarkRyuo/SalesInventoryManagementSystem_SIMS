67ba6409a80e9786fc3b63211921c8250dd9f5cc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-29 16:36:09 +0800

    Subject: Update

--- client/src/components/StaffPortal/ScanAssets/ScanAssetsMode.jsx
+++ client/src/components/StaffPortal/ScanAssets/ScanAssetsMode.jsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 //* Inputs
 function ScanAssetsMode() {
     return (
-        <Container fluid className="">
+        <Container fluid className="m-0 p-0">
             <Container fluid='md'>
 
             </Container>
