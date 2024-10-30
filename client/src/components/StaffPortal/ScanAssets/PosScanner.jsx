24ed98cd5bbadbce5e9475bd31a1584d1aba2c47
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-30 09:09:52 +0800

    Subject: Update

--- client/src/components/StaffPortal/ScanAssets/PosScanner.jsx
+++ client/src/components/StaffPortal/ScanAssets/PosScanner.jsx
@@ -48,8 +48,15 @@ function PosScanner() {
             <StaffNavBar backBtn={backBtn.filter((Backbtn) => Backbtn.id === 1)} />
             <Container fluid="lg" style={{ boxSizing: "border-box", border: '1px solid blue', height: '90vh'}}>
                 <Row style={{ boxSizing: "border-box", border: '1px solid green', height: '100%'}}>
-                    <Col lg={12} style={{ boxSizing: "border-box", border: '1px solid red', height: '65vh', width: '100%', display: "flex", flexDirection: "column", alignItems: "center"}}>
-                        <video ref={videoRef} style={{ width: "100%", height: "100%", maxWidth: 800}} className="p-0 m-0" />
+                    <Col lg={12} style={{ 
+                        boxSizing: "border-box", 
+                        border: '1px solid red', 
+                        height: '65vh', 
+                        width: '100%', 
+                        display: "flex", 
+                        flexDirection: "column", 
+                        alignItems: "center"}}>
+                        <video ref={videoRef} style={{ width: "100%", height: "100%", maxWidth: 800}}/>
                         {scannedResult && (
                             <p>Scanned Result: {scannedResult}</p>
                         )}
