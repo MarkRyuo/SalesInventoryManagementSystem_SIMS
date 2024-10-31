be65a85becc3af6958dee881d46a3410b64d0ddf
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-31 17:31:14 +0800

    Subject: Update

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 function LandingPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <>
+
+        </>
+    )
 }
 
 export default LandingPage
