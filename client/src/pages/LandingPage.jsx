17b0609cb963358ab3e8c7775dd01a38a0339433
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-31 17:34:41 +0800

    Subject: Update

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -5,7 +5,7 @@ function LandingPage() {
         <Container fluid>
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#home">SIMS</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#features">Features</Nav.Link>
