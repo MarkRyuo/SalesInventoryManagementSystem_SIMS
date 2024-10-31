a6c32de2a2354ff8d2d52e8f3c427746df1bab6f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-31 17:34:24 +0800

    Subject: Update

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,9 +1,19 @@
+import { Container, Nav, Navbar } from "react-bootstrap"
 
 function LandingPage() {
     return (
-        <>
-
-        </>
+        <Container fluid>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </Container>
     )
 }
 
