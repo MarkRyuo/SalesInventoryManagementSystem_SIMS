448e559f9277717e4e580e6a2705458157b29da0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-31 18:11:52 +0800

    Subject: Update

---
+++ client/src/components/NavBar/NavBars.jsx
@@ -0,0 +1,20 @@
+import { Container, Nav, Navbar } from "react-bootstrap"
+
+function NavBars() {
+    return (
+        <>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default NavBars
