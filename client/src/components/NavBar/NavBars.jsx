fa1babb8a3cf2473280ca31178ca7684b989afbd
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-31 18:12:51 +0800

    Subject: Update

--- client/src/components/NavBar/NavBars.jsx
+++ client/src/components/NavBar/NavBars.jsx
@@ -7,9 +7,9 @@ function NavBars() {
                 <Container>
                     <Navbar.Brand href="#home">SIMS</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">About us</Nav.Link>
-                        <Nav.Link href="#pricing">Contacts</Nav.Link>
+                        <Nav.Link href="#">Home</Nav.Link>
+                        <Nav.Link href="#">About us</Nav.Link>
+                        <Nav.Link href="#">Contacts</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
