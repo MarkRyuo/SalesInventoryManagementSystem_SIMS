c9c470125df154f9533db7175c99d0e49a42815d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-10-31 20:23:14 +0800

    Subject: Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import './App.css' ;
 
 // * Imported Pages
-import Login from '../src/pages/LoginPage'
+import Login from '../src/pages/LoginPage' ;
 
 
 
