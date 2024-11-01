905d1b1901299ae3571573676a68e1e87723b1d2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-01 20:57:45 +0800

    Subject: Update

--- client/src/components/NavBar/NavLogin.jsx
+++ client/src/components/NavBar/NavLogin.jsx
@@ -1,7 +1,6 @@
 import { BsBox } from "react-icons/bs";
 import { Row, Col, Container, Navbar} from 'react-bootstrap';
 import Navbars from './Navbar.module.css';
-import { Link } from 'react-router-dom';
 
 export const NavLogin = () => {
 
