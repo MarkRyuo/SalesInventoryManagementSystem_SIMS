da38e0fd38946361620dd822d9b707db6bf105ee
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-01 22:41:53 +0800

    Subject: Update

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -10,6 +10,7 @@ function LandingPage() {
                     <div style={{display: "flex"}}> {/* Main Component */}
                         <div className=""> {/* Child */}
                             <p className="fs-4">REYES ELECTRONICS</p>
+                            <p>lorem</p>
                         </div>
                         <div className="">
 
