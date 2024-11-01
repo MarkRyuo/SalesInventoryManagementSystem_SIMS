39c60ab2d31ab48202a93217ad4bfa8cd078b1a8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-01 22:10:04 +0800

    Subject: Update

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -7,7 +7,7 @@ function LandingPage() {
             <NavBars />
             <Container fluid style={{ border: '1px solid', height: '90vh'}}>
                 <main>
-
+                    <div></div>
                 </main>
             </Container>
         </Container>
