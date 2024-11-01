44f31feaf165a641f7a4f712016c77c06c8b1167
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-01 22:04:41 +0800

    Subject: Update

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -6,7 +6,7 @@ function LandingPage() {
         <Container fluid className="m-0 p-0">
             <NavBars />
             <Container fluid='lg'>
-
+                <div></div>
             </Container>
         </Container>
     )   
