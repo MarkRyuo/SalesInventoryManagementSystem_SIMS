1a16b1b2fa4ddd707b0c1b18746c94bac9ebd1c0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-01 20:47:46 +0800

    Subject: Update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -29,6 +29,7 @@ import PosScanner from './components/StaffPortal/ScanAssets/PosScanner.jsx';
 import ReceiptMode from './components/StaffPortal/ScanAssets/ReceiptMode.jsx';
 import ScanAssetsMode from './components/StaffPortal/ScanAssets/ScanAssetsMode.jsx';
 import PosSuccess from './components/StaffPortal/ScanAssets/PosSuccess.jsx';
+import LoginPage from './pages/LoginPage.jsx'
 
 const router = createBrowserRouter([
   {
