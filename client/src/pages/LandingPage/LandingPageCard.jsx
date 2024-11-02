92bf14e64aadd13752bf06d1409e611419eeae60
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-02 15:14:49 +0800

    Subject: Update

--- client/src/pages/LandingPage/LandingPageCard.jsx
+++ client/src/pages/LandingPage/LandingPageCard.jsx
@@ -4,22 +4,24 @@ import { useState } from "react"
 function LandingPageCard() {
 
     const [card] = useState([
-        
+        {icon: '', cardTitle: '', cardText: '', cardButton: '', cardId: 1 }
     ])
 
 
     return (
         <>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        {cardText}
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
+            {card.map((cards) => (
+                <Card key={cards.cardId} style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            {cardText}
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card> 
+            ))}
         </>
     )
 }
