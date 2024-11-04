b5ba708c9ea1c41fdd06ac6d644e257b12ca3bda
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-04 21:57:58 +0800

    Subject: Body

--- client/src/components/NavBar/NavLogin.jsx
+++ client/src/components/NavBar/NavLogin.jsx
@@ -11,13 +11,15 @@ export const NavLogin = () => {
             <Navbar>
                 <Container>
                     <Row>
-                        <Col className="d-none d-block"> 
+                        <Col className="d-none d-md-block"> 
                             <Link to={"/"} style={{ color: "rgb(19, 14, 1)" }}>
                                 <BsBox size={40} className="d-none d-md-block" />
                             </Link> 
                         </Col>
                         <Col className="pt-1 m-0 p-0">
-                            <Link to={"/"} style={{ color: "rgb(19, 14, 1)", textDecoration: "none" }}><Navbar.Brand className="Nav-brand">SIMS</Navbar.Brand></Link>
+                            <Link to={"/"} style={{ color: "rgb(19, 14, 1)", textDecoration: "none" }}>
+                                <Navbar.Brand className="Nav-brand">SIMS</Navbar.Brand>
+                            </Link>
                         </Col>
                     </Row>
 
