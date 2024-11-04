bb5d5f64070e697dd7fa30c42f127e39af778089
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-04 21:51:38 +0800

    Subject: Body

--- client/src/components/NavBar/NavStaffLogin.jsx
+++ client/src/components/NavBar/NavStaffLogin.jsx
@@ -11,7 +11,7 @@ function NavStaffLogin() {
                 <Navbar>
                     <Container>
                         <Row>
-                            <Col> <Link to={"/"}><BsBox size={40} /></Link> </Col>
+                            <Col> <Link to={"/"} style={{ color: "rgb(19, 14, 1)" }}><BsBox size={40} /></Link> </Col>
                             <Col style={{ paddingTop: 4 }}>
                                 <Link to={"/"}><Navbar.Brand className="">Staff Portal</Navbar.Brand></Link>
                             </Col>
