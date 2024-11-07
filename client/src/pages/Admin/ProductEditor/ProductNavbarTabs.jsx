168f11aebc7872596838f13184a337a8a7b78b15
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-07 15:11:30 +0800

    Subject: Update

--- client/src/pages/Admin/ProductEditor/ProductNavbarTabs.jsx
+++ client/src/pages/Admin/ProductEditor/ProductNavbarTabs.jsx
@@ -4,10 +4,10 @@ function ProductNavbarTabs() {
     return (
         <Nav justify variant="tabs" defaultActiveKey="/ProductEditor" className='mt-4'>
             <Nav.Item>
-                <Nav.Link href="#">Edit Product</Nav.Link>
+                <Nav.Link href="#">Product</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="#">Loooonger NavLink</Nav.Link>
+                <Nav.Link eventKey="#">Product Edit</Nav.Link>
             </Nav.Item>
             <Nav.Item>
                 <Nav.Link eventKey="#">Link</Nav.Link>
