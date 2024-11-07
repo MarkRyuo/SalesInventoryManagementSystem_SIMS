f91ef86597728b43b18b41fcfb8075ac35723d37
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-08 07:42:12 +0800

    Subject: Update

--- client/src/components/NavBar/NavDashboard.jsx
+++ client/src/components/NavBar/NavDashboard.jsx
@@ -1,6 +1,6 @@
 import { Container, Navbar, Button, Offcanvas, Image } from 'react-bootstrap';
 import { useState } from 'react';
-import Navbars from './Navbar.module.css';
+import Navbars from './Navbar.module.scss';
 import { Buttons } from './Buttons';
 import AccountDropdown from './AccountDropdown';
 
