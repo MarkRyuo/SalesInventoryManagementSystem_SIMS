86554e3a3e9aef4a35ec3317d4fe28be402a24e5
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-07 14:55:08 +0800

    Subject: Update

--- client/src/pages/Admin/ProductEditor/ProductMain.jsx
+++ client/src/pages/Admin/ProductEditor/ProductMain.jsx
@@ -1,9 +1,13 @@
-import { Container } from "react-bootstrap";
+import { Container, Navbar } from "react-bootstrap";
 
 function ProductMain() {
     return (
         <Container fluid className="m-0 p-0">
-
+            <Navbar className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+                </Container>
+            </Navbar>
         </Container>
     )
 }
