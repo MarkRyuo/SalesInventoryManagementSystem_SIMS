f59fb51db0baf95bd1d1908944b3f3b5e75c6091
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-08 14:36:59 +0800

    Subject: Update

--- client/src/pages/LandingPage/NavBars.jsx
+++ client/src/pages/LandingPage/NavBars.jsx
@@ -5,13 +5,13 @@ function NavBars() {
     return (
         <Navbar expand="lg" className={LandingPagecss.Navbar}>
                 <Container>
-                    <Navbar.Brand href="#home">REYES ELECTRONICS</Navbar.Brand>
+                <Navbar.Brand href="#home" style={{ color: '#ffffff' }}>REYES ELECTRONICS</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#">About us</Nav.Link>
-                            <NavDropdown title="User Type" id="basic-nav-dropdown">
+                        <Nav.Link href="#home" style={{ color: '#ffffff' }}>Home</Nav.Link>
+                        <Nav.Link href="#" style={{ color: '#ffffff' }}>About us</Nav.Link>
+                        <NavDropdown title="User Type" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="LoginPage">Admin Portal</NavDropdown.Item>
                                 <NavDropdown.Item href="SLogin">Staff Portal</NavDropdown.Item>
                                 <NavDropdown.Divider />
