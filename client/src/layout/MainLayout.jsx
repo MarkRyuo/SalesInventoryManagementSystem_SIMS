b1e73ce8a7cf62c14a3c063f29affb9f06bf1a32
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-08 08:50:35 +0800

    Subject: Update

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -6,7 +6,7 @@ import MainLayoutscss from './MainLayout.module.scss'
 export const MainLayout = ({children}) => {
 
     return (
-        <Container fluid style={{ margin: "0px", padding: "0px" }}>
+        <Container fluid className={MainLayoutscss.mainLayoutContainer}>
             <NavDashboard />
             <Container fluid='lg' className='p-0'>
                 {children}
