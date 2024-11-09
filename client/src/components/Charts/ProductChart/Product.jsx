19271c2b87c0b6ec56130444b323e94beb0fc79c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-09 15:33:18 +0800

    Subject: Update

--- client/src/components/Charts/ProductChart/Product.jsx
+++ client/src/components/Charts/ProductChart/Product.jsx
@@ -69,6 +69,9 @@ function ProductChart() {
                         <NewCategory />
                     </div>
                     <div>
+                        {/* Create Barcode */}
+                    </div>
+                    <div>
                         
                     </div>
                 </div>
