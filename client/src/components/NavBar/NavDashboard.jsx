372be7ce2b3f00362f6f1bbd51a1d1461bbbb234
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-09 09:12:14 +0800

    Subject: Update

--- client/src/components/NavBar/NavDashboard.jsx
+++ client/src/components/NavBar/NavDashboard.jsx
@@ -7,11 +7,11 @@ import AccountDropdown from './AccountDropdown';
 //* Icons
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdSpaceDashboard } from "react-icons/md";
-import { AiOutlineProduct } from "react-icons/ai";
 import { TbReportAnalytics } from "react-icons/tb";
 import { VscAccount } from "react-icons/vsc";
 import { RiLogoutCircleLine } from "react-icons/ri";
 import { FaHistory } from "react-icons/fa";
+import { MdOutlineInventory2 } from "react-icons/md";
 
 export const NavDashboard = () => {
     const [show, setShow] = useState(false);
@@ -23,7 +23,7 @@ export const NavDashboard = () => {
     //* Buttons
     const [buttons] = useState([
         { icon: <MdSpaceDashboard />, btnName: "Dashboard", id: "b-1", path: "/DashboardPage" },
-        { icon: <AiOutlineProduct />, btnName: "Product", id: "b-2", path: "/ProductPage" },
+        { icon: <MdOutlineInventory2 />, btnName: "Inventory", id: "b-2", path: "/ProductPage" },
         { icon: <TbReportAnalytics />, btnName: "Report", id: "b-3", path: "/ReportPage" },
         { icon: <VscAccount />, btnName: "Account", id: "b-4", path: "/AccountPage" },
         { icon: <FaHistory />, btnName: "Transaction History", id: "b-5", path: "/AdminTransactionHistory" },
