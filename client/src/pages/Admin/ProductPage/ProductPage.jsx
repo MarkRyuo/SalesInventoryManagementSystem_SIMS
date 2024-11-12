67d01e208563cd9d04da27bed2f488add20797e1
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-12 08:29:48 +0800

    Subject: Update

--- client/src/pages/Admin/ProductPage/ProductPage.jsx
+++ client/src/pages/Admin/ProductPage/ProductPage.jsx
@@ -27,7 +27,7 @@ function ProductNavbarTabs({ setActiveTab }) {
             <Nav.Item className={ProductPagescss.mainTabs}>
                 <Nav.Link eventKey="/ProductEditor">
                     <FaRegEdit size={20} />
-                    <p className='m-0 p-0'>Set Tax</p>
+                    <p className='m-0 p-0'>Product Configuration</p>
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item className={ProductPagescss.mainTabs}>
