da96ca2290a0346bcecfcfb010f44811ca4949ee
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-12 10:37:57 +0800

    Subject: Update

--- client/src/pages/Admin/ProductPage/ReOrdering.jsx
+++ client/src/pages/Admin/ProductPage/ReOrdering.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { fetchReorderingProducts, saveReorderList } from "../../../services/ProductService";
+import { fetchReorderingProducts} from "../../../services/ProductService";
 import { Table, Spinner, Button, Badge, Container, Modal } from "react-bootstrap";
 import { jsPDF } from "jspdf";
 
@@ -35,8 +35,7 @@ function ReOrdering() {
     };
 
     // Add product to the reorder list and track reordered products
-    const handleReorderProduct = async (product) => {
-        // Add product to the reorder list
+    const handleReorderProduct = (product) => {
         setReorderList((prevList) => {
             if (!prevList.find((item) => item.barcode === product.barcode)) {
                 return [...prevList, product];
@@ -51,14 +50,6 @@ function ReOrdering() {
         setReorderingProducts((prevProducts) =>
             prevProducts.filter((item) => item.barcode !== product.barcode)
         );
-
-        // Try to save the updated reorder list to the database
-        try {
-            await saveReorderList(reorderList);
-            console.log("Reorder list saved successfully!");
-        } catch (error) {
-            console.error("Error saving reorder list:", error);
-        }
 
         setShowProductModal(false);
     };
