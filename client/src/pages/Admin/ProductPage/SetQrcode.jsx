44ff536d40eeab08ff7089f5fef430e0e3116e65
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-14 07:59:18 +0800

    Subject: Update

--- client/src/pages/Admin/ProductPage/SetQrcode.jsx
+++ client/src/pages/Admin/ProductPage/SetQrcode.jsx
@@ -38,45 +38,11 @@ function ProductListWithQRCodes() {
 
     return (
         <Container fluid="lg">
-            <h3>Product List with QR Codes</h3>
+            <h3>List of QR Codes</h3>
 
             <Button variant="primary" onClick={handleShowModal} className="mb-4">
                 Set QR Code for Product
             </Button>
-
-            <AddQrcode
-                showModal={showModal}
-                handleCloseModal={handleCloseModal}
-            />
-
-            {isLoading && <Spinner animation="border" className="d-block mx-auto" />}
-
-            {error && <Alert variant="danger">{error}</Alert>}
-
-            <Row>
-                {!isLoading && qrCodes.length === 0 && (
-                    <Col className="text-center">
-                        <p>No QR Codes available.</p>
-                    </Col>
-                )}
-
-                {qrCodes.map((qrCode) => (
-                    <Col key={qrCode.barcode} md={4} sm={6} className="mb-4">
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Barcode: {qrCode.barcode}</Card.Title>
-                                <div className="text-center">
-                                    <img
-                                        src={qrCode.qrcodeBase64}
-                                        alt={`QR Code for ${qrCode.barcode}`}
-                                        style={{ width: '200px', height: '200px' }}
-                                    />
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
         </Container>
     );
 }
