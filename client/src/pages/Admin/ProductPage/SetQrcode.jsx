b3612d27b81c8add66baaa9e3545412475cc3c2c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-13 14:30:42 +0800

    Subject: Update

--- client/src/pages/Admin/ProductPage/SetQrcode.jsx
+++ client/src/pages/Admin/ProductPage/SetQrcode.jsx
@@ -1,10 +1,27 @@
-import AddQrcode from './AddQrcode'
+import { useState } from 'react';
+import AddQrcode from "./AddQrcode";
+import { Button } from 'react-bootstrap';
 
 function SetQrcode() {
+    const [showModal, setShowModal] = useState(false);
+
+    // Handle opening the modal
+    const handleShowModal = () => setShowModal(true);
+
+    // Handle closing the modal
+    const handleCloseModal = () => setShowModal(false);
 
     return (
         <>
-            <AddQrcode />
+            <div>
+                <Button variant="primary" onClick={handleShowModal}>
+                    Set QR Code for Product
+                </Button>
+                <AddQrcode
+                    showModal={showModal}
+                    handleCloseModal={handleCloseModal}
+                />
+            </div>
         </>
     );
 }
