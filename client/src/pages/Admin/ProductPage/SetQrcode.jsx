d40bc1b587215276883cfbb929e16fe784b738fc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-13 10:02:50 +0800

    Subject: Update

--- client/src/pages/Admin/ProductPage/SetQrcode.jsx
+++ client/src/pages/Admin/ProductPage/SetQrcode.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 function SetQrcode() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default SetQrcode
