18930e928d06cb4a17011f033dde2388debd5491
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-15 07:49:33 +0800

    Subject: Update

--- client/functions/firebaseAdmin.js
+++
@@ -1,8 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://salesinventorymanagement-1bb27-default-rtdb.firebaseio.com"
-});
