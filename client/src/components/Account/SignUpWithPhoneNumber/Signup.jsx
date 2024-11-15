7831473766ce936f20ec3b8416dd189217cd590f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-15 08:06:14 +0800

    Subject: Update

--- client/src/components/Account/SignUpWithPhoneNumber/Signup.jsx
+++ client/src/components/Account/SignUpWithPhoneNumber/Signup.jsx
@@ -2,41 +2,30 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Form, Alert } from "react-bootstrap";
 import { Button } from "react-bootstrap";
-import GoogleButton from "react-google-button";
 import { useUserAuth } from "../context/UserAuthContext";
 
-const Login = () => {
+const Signup = () => {
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const [error, setError] = useState("");
-    const { logIn, googleSignIn } = useUserAuth();
-    const navigate = useNavigate();
+    const [password, setPassword] = useState("");
+    const { signUp } = useUserAuth();
+    let navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError("");
         try {
-            await logIn(email, password);
-            navigate("/home");
+            await signUp(email, password);
+            navigate("/");
         } catch (err) {
             setError(err.message);
-        }
-    };
-
-    const handleGoogleSignIn = async (e) => {
-        e.preventDefault();
-        try {
-            await googleSignIn();
-            navigate("/home");
-        } catch (error) {
-            console.log(error.message);
         }
     };
 
     return (
         <>
             <div className="p-4 box">
-                <h2 className="mb-3">Firebase Auth Login</h2>
+                <h2 className="mb-3">Firebase Auth Signup</h2>
                 {error && <Alert variant="danger">{error}</Alert>}
                 <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -57,31 +46,16 @@ const Login = () => {
 
                     <div className="d-grid gap-2">
                         <Button variant="primary" type="Submit">
-                            Log In
+                            Sign up
                         </Button>
                     </div>
                 </Form>
-                <hr />
-                <div>
-                    <GoogleButton
-                        className="g-btn"
-                        type="dark"
-                        onClick={handleGoogleSignIn}
-                    />
-                </div>
-                <Link to="/phonesignup">
-                    <div className="d-grid gap-2 mt-3">
-                        <Button variant="success" type="Submit">
-                            Sign in with Phone
-                        </Button>
-                    </div>
-                </Link>
             </div>
             <div className="p-4 box mt-3 text-center">
-                Don have an account? <Link to="/signup">Sign up</Link>
+                Already have an account? <Link to="/">Log In</Link>
             </div>
         </>
     );
 };
 
-export default Login;
+export default Signup;
