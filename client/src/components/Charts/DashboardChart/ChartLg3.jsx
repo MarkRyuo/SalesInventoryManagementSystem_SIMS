27387c0b977e9b9dfd51926f2973e04756d5d33f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-19 00:05:32 +0800

    Subjetc:Update

---
+++ client/src/components/Charts/DashboardChart/ChartLg3.jsx
@@ -0,0 +1,12 @@
+import Chartcss from './Charts.module.scss';
+
+
+function ChartLg3() {
+    return (
+        <div className={Chartcss.containerChartLg3}>
+            {/* Code for Chart Here */}
+        </div>
+    )
+}
+
+export default ChartLg3 ;
