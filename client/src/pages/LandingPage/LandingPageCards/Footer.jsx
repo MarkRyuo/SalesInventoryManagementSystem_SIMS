4f7a3ba76a46e6e9f73feefd1d64eb3d5c2a7935
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 22:10:57 +0800

    Subjetc:Update

--- client/src/pages/LandingPage/LandingPageCards/Footer.jsx
+++ client/src/pages/LandingPage/LandingPageCards/Footer.jsx
@@ -60,7 +60,7 @@ function Footer() {
                     <Col md={4} lg={3} xl={3} className="mb-4">
                         <h6 className="text-uppercase fw-bold mb-2">Contact</h6>
                         <p><BsShop className='me-2'/>Brgy 4, Rizal St. Lipa City Batangas</p>
-                        <p><FaShoppingCart className='me-2'/><a className='m-0 p-0' href='https://shopee.ph/reyeselectronics?fbclid=IwY2xjawGoJaBleHRuA2FlbQIxMAABHZeteTkZfvcjrqobcCIOnhtALf9tnpL5EavTI-F-2Sj_GJkM7gmpJOgyeg_aem_6fnRuc25GZqBjftFPAuF5Q' style={{textDecoration: 'none'}}>Our Online Shop</a></p>
+                        <p><FaShoppingCart className='me-2'/><a className='m-0 p-0 text-reset' href='https://shopee.ph/reyeselectronics?fbclid=IwY2xjawGoJaBleHRuA2FlbQIxMAABHZeteTkZfvcjrqobcCIOnhtALf9tnpL5EavTI-F-2Sj_GJkM7gmpJOgyeg_aem_6fnRuc25GZqBjftFPAuF5Q' style={{textDecoration: 'none'}}>Our Online Shop</a></p>
                         <p><FaEnvelope className="me-2" /> rramilelectronics@gmail.com</p>
                         <p><FaPhone className="me-2" /> + 63 921 238 8866</p>
                         <p><BsFillTelephoneFill className='me-2'/>(043) 741-0922</p>
