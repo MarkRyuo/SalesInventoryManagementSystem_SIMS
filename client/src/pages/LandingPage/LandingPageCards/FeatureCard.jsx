af4c6e02d446681543403f71a687260ec98a4932
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 15:55:31 +0800

    Subjetc:Update

--- client/src/pages/LandingPage/LandingPageCards/FeatureCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/FeatureCard.jsx
@@ -1,33 +1,6 @@
-import { useState } from "react";
 import { MdOutlineInventory } from "react-icons/md";
 
 function LandingPageCard() {
-
-    const [card] = useState([
-        {
-            cardTitle: 'Inventory Tracking',
-            cardText: 'Keep real-time tabs on stock levels and prevent shortages.',
-            cardId: 1
-        },
-        {
-            icon: <MdOutlineInventory size={50} />,
-            cardTitle: 'Sales Analytics',
-            cardText: 'Gain insights into top-performing products and trends.',
-            cardId: 2
-        },
-        {
-            icon: <MdOutlineInventory size={50} />,
-            cardTitle: 'POS Integration',
-            cardText: 'Easily integrate with your POS for seamless sales tracking.',
-            cardId: 3
-        },
-        {
-            icon: <MdOutlineInventory size={50} />,
-            cardTitle: 'Low Stock Alerts',
-            cardText: 'Get instant alerts when stock levels drop below your set threshold.',
-            cardId: 4
-        },
-    ])
 
     return (
         <div>
@@ -39,13 +12,19 @@ function LandingPageCard() {
                     <p>Keep real-time tabs on stock levels and prevent shortages.</p>
                 </div>
                 <div>
-                    
+                    <span><MdOutlineInventory size={50} /></span>
+                    <h4>Sales Analytics</h4>
+                    <p>Gain insights into top-performing products and trends.</p>
                 </div>
                 <div>
-                    
+                    <span><MdOutlineInventory size={50} /></span>
+                    <h4>POS Integration</h4>
+                    <p>Easily integrate with your POS for seamless sales tracking.</p>
                 </div>
                 <div>
-                    
+                    <span><MdOutlineInventory size={50} /></span>
+                    <h4>Low Stock Alerts</h4>
+                    <p>Get instant alerts when stock levels drop below your set threshold.</p>
                 </div>
             </div>
         </div>
