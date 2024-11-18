cbeab5b17513db35087426d76fc1508b0c83ca78
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 15:49:40 +0800

    Subjetc:Update

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,41 +1,10 @@
 import { Container, Button} from "react-bootstrap"
 import NavBars from "./NavBars"
-import LandingPageCard from './LandingPageCards/LandingPageCard'
 import LandingPagecss from './LandingPage.module.scss'
 import { CiGlobe } from "react-icons/ci";
-import { useState } from "react";
-
-import { MdOutlineInventory } from "react-icons/md";
 import AboutCard from "./LandingPageCards/AboutCard";
 
 function LandingPage() {
-
-    const [card] = useState([
-        {
-            icon: <MdOutlineInventory size={50} />,
-            cardTitle: 'Inventory Tracking',
-            cardText: 'Keep real-time tabs on stock levels and prevent shortages.',
-            cardId: 1
-        },
-        {
-            icon: <MdOutlineInventory size={50} />,
-            cardTitle: 'Sales Analytics',
-            cardText: 'Gain insights into top-performing products and trends.',
-            cardId: 2
-        },
-        {
-            icon: <MdOutlineInventory size={50} />,
-            cardTitle: 'POS Integration',
-            cardText: 'Easily integrate with your POS for seamless sales tracking.',
-            cardId: 3
-        },
-        {
-            icon: <MdOutlineInventory size={50} />,
-            cardTitle: 'Low Stock Alerts',
-            cardText: 'Get instant alerts when stock levels drop below your set threshold.',
-            cardId: 4
-        },
-    ])
 
     return (
         <Container fluid className="m-0 p-0">
--- client/src/pages/LandingPage/LandingPageCards/FeatureCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/FeatureCard.jsx
@@ -2,6 +2,33 @@
 
 function LandingPageCard({card}) {
 
+    const [card] = useState([
+        {
+            icon: <MdOutlineInventory size={50} />,
+            cardTitle: 'Inventory Tracking',
+            cardText: 'Keep real-time tabs on stock levels and prevent shortages.',
+            cardId: 1
+        },
+        {
+            icon: <MdOutlineInventory size={50} />,
+            cardTitle: 'Sales Analytics',
+            cardText: 'Gain insights into top-performing products and trends.',
+            cardId: 2
+        },
+        {
+            icon: <MdOutlineInventory size={50} />,
+            cardTitle: 'POS Integration',
+            cardText: 'Easily integrate with your POS for seamless sales tracking.',
+            cardId: 3
+        },
+        {
+            icon: <MdOutlineInventory size={50} />,
+            cardTitle: 'Low Stock Alerts',
+            cardText: 'Get instant alerts when stock levels drop below your set threshold.',
+            cardId: 4
+        },
+    ])
+
     return (
         <>
             <h1 className="mb-5" id="features">Features</h1>
