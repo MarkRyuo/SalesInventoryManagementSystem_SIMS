020f3e9ce9bc753410588d39353b8ce294f97b66
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 11:28:00 +0800

    Subjetc:Update

--- client/src/pages/LandingPage/LandingPageCards/AboutCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/AboutCard.jsx
@@ -3,7 +3,9 @@
 function AboutCard() {
     return (
         <div>
-            <h1>About Us</h1>
+            <div>
+                <h1>About Us</h1>
+            </div>
         </div>
     )
 }
