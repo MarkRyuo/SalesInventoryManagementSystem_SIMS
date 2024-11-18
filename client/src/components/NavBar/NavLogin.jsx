32dea3bc511b2f478ee0e9ed3203a2cf813b57e0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 17:25:45 +0800

    Subjetc:Update

--- client/src/components/NavBar/NavLogin.jsx
+++ client/src/components/NavBar/NavLogin.jsx
@@ -11,11 +11,6 @@ export const NavLogin = () => {
             <Navbar>
                 <Container>
                     <Row>
-                        <Col className="d-none d-md-block"> 
-                            <Link to={"/"} style={{ color: "rgb(19, 14, 1)" }}>
-                                <BsBox size={40} className="d-none d-md-block" />
-                            </Link> 
-                        </Col>
                         <Col className="pt-1 m-0 p-0">
                             <Link to={"/"} style={{ color: "rgb(19, 14, 1)", textDecoration: "none" }}>
                                 <Navbar.Brand className="Nav-brand">Reyes Electronics</Navbar.Brand>
