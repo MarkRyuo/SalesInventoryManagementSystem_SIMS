f1883a0b05ee073a5312dca2bb04519a92b8b3ec
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 15:51:46 +0800

    Subjetc:Update

--- client/src/pages/LandingPage/LandingPageCards/FeatureCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/FeatureCard.jsx
@@ -43,25 +43,10 @@ function LandingPageCard() {
                 <div>
                     
                 </div>
-                <div className={LandingPagecss.colFeature}>
+                <div>
                     
                 </div>
             </div>
-            {card.map((cards) => (
-                <div 
-                    key={cards.cardId} 
-                    style={{ 
-                        background: 'rgb(6, 249, 119, 15%)', 
-                        borderRadius: 15,
-                        height: 'auto'
-                        }}>
-                    <span className='text-center'>{cards.icon}</span>
-                    <div>
-                        <h4>{cards.cardTitle}</h4>
-                        <p>{cards.cardText}</p>
-                    </div>
-                </div> 
-            ))}
         </div>
     )
 }
