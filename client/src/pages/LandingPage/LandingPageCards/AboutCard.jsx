5ec2655b1b0867e08b02e58af0f90c9ab33fe479
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 14:18:53 +0800

    Subjetc:Update

--- client/src/pages/LandingPage/LandingPageCards/AboutCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/AboutCard.jsx
@@ -6,9 +6,7 @@ function AboutCard() {
             <h1>About Us</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
 
-            <div>
-                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</h2>
-            </div>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</h2>
 
             <div className={AboutCardscss.aboutCards}>
                 <div className={AboutCardscss.card}>
