49252a3b3a662b55ed5af4c1f80f517db130b22f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 11:42:24 +0800

    Subjetc:Update

--- client/src/pages/LandingPage/LandingPageCards/AboutCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/AboutCard.jsx
@@ -18,7 +18,31 @@ function AboutCard() {
                             <Card.Title>Card Title</Card.Title>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
+                                bulk of the cards content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the cards content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the cards content.
                             </Card.Text>
                             <Button variant="primary">Go somewhere</Button>
                         </Card.Body>
