cf91c13f11f3d7d3ccf018f749ace13a445ba44d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 15:56:16 +0800

    Subjetc:Update

--- client/src/pages/LandingPage/LandingPageCards/LandingSIMS.jsx
+++ client/src/pages/LandingPage/LandingPageCards/LandingSIMS.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 function LandingSIMS() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default LandingSIMS
