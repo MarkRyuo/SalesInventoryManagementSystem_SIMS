b945111043772065a750784972ec0ab14f10c495
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 11:41:21 +0800

    Subjetc:Update

--- client/src/pages/LandingPage/LandingPageCards/AboutCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/AboutCard.jsx
@@ -1,4 +1,4 @@
-import {Card, Col, Row} from 'react-bootstrap';
+import {Card,} from 'react-bootstrap';
 
 function AboutCard() {
     return (
@@ -12,23 +12,17 @@ function AboutCard() {
                 </div>
 
                 <div>
-                    <Row xs={1} md={2} className="g-4">
-                        {Array.from({ length: 4 }).map((_, idx) => (
-                            <Col key={idx}>
-                                <Card>
-                                    <Card.Img variant="top" src="holder.js/100px160" />
-                                    <Card.Body>
-                                        <Card.Title>Card title</Card.Title>
-                                        <Card.Text>
-                                            This is a longer card with supporting text below as a natural
-                                            lead-in to additional content. This content is a little bit
-                                            longer.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        ))}
-                    </Row>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
                 </div>
             </div>
         </div>
