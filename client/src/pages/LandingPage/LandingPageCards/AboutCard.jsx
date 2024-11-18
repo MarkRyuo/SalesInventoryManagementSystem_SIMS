4bf0dd51da713c4d30e8e3311bdaac19e238fed6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 13:45:00 +0800

    Subjetc:Update

--- client/src/pages/LandingPage/LandingPageCards/AboutCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/AboutCard.jsx
@@ -1,4 +1,3 @@
-import { Card } from 'react-bootstrap';
 import AboutCardscss from './LandingPageCards.module.scss';
 
 function AboutCard() {
@@ -12,40 +11,45 @@ function AboutCard() {
             </div>
 
             <div className={AboutCardscss.aboutCards}>
-                <div>
+                <div className={AboutCardscss.card}>
                     <div>
                         <img src="https://i.pinimg.com/736x/1f/50/92/1f50928b00d6291a8969a3651908ffd2.jpg" />
-                        <div>
-                            <h5>Card Title</h5>
-                            <p>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the cards content.
-                            </p>
-                        </div>
                     </div>
-
                     <div>
-                        <img src="https://i.pinimg.com/736x/70/e5/6b/70e56be2517516b6fe4add9b3b08a502.jpg" />
-                        <div>
-                            <h5>Card Title</h5>
-                            <p>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the cards content.
-                            </p>
-                        </div>
-                    </div>
-
-                    <div>
-                        <img src="https://i.pinimg.com/736x/ef/d3/05/efd305767de5abb1312622c8b2046397.jpg" />
-                        <div>
-                            <h5>Card Title</h5>
-                            <p>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the cards content.
-                            </p>
-                        </div>
+                        <h5>Card Title</h5>
+                        <p>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the cards content.
+                        </p>
                     </div>
                 </div>
+
+                <div className={AboutCardscss.card}>
+                    <div>
+                        <img src="https://i.pinimg.com/736x/70/e5/6b/70e56be2517516b6fe4add9b3b08a502.jpg" />
+                    </div>
+                    <div>
+                        <h5>Card Title</h5>
+                        <p>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the cards content.
+                        </p>
+                    </div>
+                </div>
+
+                <div className={AboutCardscss.card}>
+                    <div>
+                        <img src="https://i.pinimg.com/736x/ef/d3/05/efd305767de5abb1312622c8b2046397.jpg" />
+                    </div>
+                    <div>
+                        <h5>Card Title</h5>
+                        <p>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the cards content.
+                        </p>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
