5cc23898988ecfafd62f449741dbe73a6d706223
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-18 12:34:20 +0800

    Subjetc:Update

--- client/src/pages/LandingPage/LandingPageCard.jsx
+++ client/src/pages/LandingPage/LandingPageCard.jsx
@@ -5,7 +5,13 @@ function LandingPageCard({card}) {
     return (
         <>
             {card.map((cards) => (
-                <div key={cards.cardId} style={{ width: '100%', height: '100%', background: 'rgb(6, 249, 119, 15%)', borderRadius: 15}}>
+                <div 
+                    key={cards.cardId} 
+                    style={{ 
+                        background: 'rgb(6, 249, 119, 15%)', 
+                        borderRadius: 15,
+                        
+                        }}>
                     <span className='text-center'>{cards.icon}</span>
                     <div>
                         <h4>{cards.cardTitle}</h4>
