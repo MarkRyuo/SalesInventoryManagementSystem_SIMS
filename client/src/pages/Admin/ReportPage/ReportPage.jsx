bf0c2eaf819973219efea0c55bd39a3d5329684e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-19 10:43:54 +0800

    Subject: Update

--- client/src/pages/Admin/ReportPage/ReportPage.jsx
+++ client/src/pages/Admin/ReportPage/ReportPage.jsx
@@ -18,33 +18,35 @@ export const ReportPage = () => {
 
     return (
         <MainLayout>
+            <div className={Reportcss.reportMain}>
+                <div>
+                    <p className='fs-4 m-4'><span><TbReport /></span> Sales Report</p>
+                </div>
+                <Row className={Reportcss.rowReport}>
+                    <Col lg={6} md={12}>
+                        <ReportChart1 />
+                    </Col>
+                    <Col lg={5} md={12}>
+                        <ReportChart2 />
+                    </Col>
+                    <Col lg={6} md={12}>
+                        <ReportChart3 />
+                    </Col>
+                    <Col lg={5} md={12}>
+                        <ReportChart4 />
+                    </Col>
+                </Row>
 
-            <p className='fs-4 m-4'><span><TbReport /></span> Sales Report</p>
-
-            <Row className={Reportcss.rowReport}>
-                <Col lg={6} md={12}>
-                    <ReportChart1 />
-                </Col>
-                <Col lg={5} md={12}>
-                    <ReportChart2 />
-                </Col>
-                <Col lg={6} md={12}>
-                    <ReportChart3 />
-                </Col>
-                <Col lg={5} md={12}>
-                    <ReportChart4 />
-                </Col>
-            </Row>
-
-            {/* Big-Charts */}
-            <Row className={Reportcss.rowContainerLg}>
-                <Col className={Reportcss.colContainerLg} sm={12} lg={8}>
-                    <ReportChartLg1 />
-                </Col>
-                <Col className={Reportcss.colContainerLg} xs={10} lg={5} >
-                    <ReportChartLg2 />
-                </Col>
-            </Row>
+                {/* Big-Charts */}
+                <Row className={Reportcss.rowContainerLg}>
+                    <Col className={Reportcss.colContainerLg} sm={12} lg={8}>
+                        <ReportChartLg1 />
+                    </Col>
+                    <Col className={Reportcss.colContainerLg} xs={10} lg={5} >
+                        <ReportChartLg2 />
+                    </Col>
+                </Row>
+            </div>
 
         </MainLayout>
     )
