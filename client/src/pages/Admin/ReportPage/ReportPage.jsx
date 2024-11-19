4b48b5d418597bde517a9d21f06a9a08ba30624b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-19 10:49:27 +0800

    Subject: Update

--- client/src/pages/Admin/ReportPage/ReportPage.jsx
+++ client/src/pages/Admin/ReportPage/ReportPage.jsx
@@ -21,7 +21,7 @@ export const ReportPage = () => {
                 <div>
                     <p className='fs-4 m-4'><span><TbReport /></span> Sales Report</p>
                 </div>
-                <div className={Reportcss.conteinerReportsmall}>
+                <div className={Reportcss.conteinerReportSmall}>
                     <div>
                         <ReportChart1 />
                     </div>
