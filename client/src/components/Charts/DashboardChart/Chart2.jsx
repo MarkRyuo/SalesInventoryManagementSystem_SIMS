5fd814078c7ac698d6fab6bba0547d78187bbcdc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-19 09:19:15 +0800

    Subject: Update

--- client/src/components/Charts/DashboardChart/Chart2.jsx
+++ client/src/components/Charts/DashboardChart/Chart2.jsx
@@ -10,7 +10,7 @@ function Chart2() {
                 <p className='m-0 p-0'>Text Here</p>
             </div>
             <div className={Chartcss.contentChart}>
-                <p>000000</p>
+                <p className='m-0 mt-2'>000000</p>
                 <p>From the running month</p>
             </div>
         </div>
