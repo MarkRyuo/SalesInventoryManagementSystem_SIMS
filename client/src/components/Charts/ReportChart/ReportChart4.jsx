8374b2a1dc91d732f749c674b2d453004e943cc9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-19 12:03:18 +0800

    Subject: Update

--- client/src/components/Charts/ReportChart/ReportChart4.jsx
+++ client/src/components/Charts/ReportChart/ReportChart4.jsx
@@ -6,7 +6,7 @@ function ReportChart4() {
         <div className={ReportChartcss.containerChart4}>
             <div className={ReportChartcss.containerText}>
                 <FaReact size={25} />
-                <p className='fs-5 m-0'>Text Here</p>
+                <p className='m-0 p-0'>Text Here</p>
             </div>
             <div className={ReportChartcss.contentChart}>
                 <p className="fs-4">00000</p>
