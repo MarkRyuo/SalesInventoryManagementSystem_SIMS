42e1f0867fc75ba37c530b65f511edf177ce78ed
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-20 11:52:22 +0800

    Subject: Update

--- client/src/components/Charts/DashboardChart/ChartLg1.jsx
+++ client/src/components/Charts/DashboardChart/ChartLg1.jsx
@@ -1,12 +1,40 @@
-import Chartcss from './Charts.module.scss';
+import { useState, useEffect } from 'react';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import ActivityFeedCSS from './Charts.module.scss';
 
+function ActivityFeed() {
+    const [activities, setActivities] = useState([]);
+    const db = getDatabase();
 
-function ChartLg1() {
+    useEffect(() => {
+        const activityRef = ref(db, 'activityLogs');
+
+        const unsubscribe = onValue(activityRef, snapshot => {
+            const data = snapshot.val();
+            if (data) {
+                const fetchedActivities = Object.keys(data).map(key => ({
+                    id: key,
+                    ...data[key],
+                }));
+                setActivities(fetchedActivities);
+            } else {
+                setActivities([]);
+            }
+        });
+
+        return () => unsubscribe();
+    }, [db]);
+
     return (
-        <div className={Chartcss.containerChartLg1}>
-            {/* Code for Chart Here */}
+        <div className={ActivityFeedCSS.containerChartLg1}>
+            {activities.map(activity => (
+                <div key={activity.id} className={ActivityFeedCSS.activityItem}>
+                    <p><strong>{activity.action}:</strong> {activity.details}</p>
+                    <p>{new Date(activity.timestamp).toLocaleString()}</p>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
-export default ChartLg1 ;
+export default ActivityFeed;
