9c4054f9ec8e6a580017edcda1e1f33476d80a7d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-21 15:49:22 +0800

    Subject: Update

--- client/src/pages/LandingPage/LandingPageCards/LogInCardPage.jsx
+++ client/src/pages/LandingPage/LandingPageCards/LogInCardPage.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 function LogInCardPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
-export default LogInCardPage
+export default LogInCardPage;
