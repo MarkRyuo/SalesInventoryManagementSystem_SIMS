8181d16b615b05abf6006d2f1e8d5341020c793d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-21 16:10:33 +0800

    Subject: Update

--- client/src/pages/LandingPage/LandingPageCards/LogInCardPage.jsx
+++ client/src/pages/LandingPage/LandingPageCards/LogInCardPage.jsx
@@ -8,7 +8,7 @@ function LogInCardPage() {
                 <input type="password" placeholder="password" />
             </form>
 
-            <Button variant="" class>
+            <Button variant="" className="">
                 Login
             </Button>
 
