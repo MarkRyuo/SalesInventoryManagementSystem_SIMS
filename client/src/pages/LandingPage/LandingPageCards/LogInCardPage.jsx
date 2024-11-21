1ba02da650081a0c4e466f7863c6fb2999522f92
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-21 17:05:39 +0800

    Subject: Update

--- client/src/pages/LandingPage/LandingPageCards/LogInCardPage.jsx
+++ client/src/pages/LandingPage/LandingPageCards/LogInCardPage.jsx
@@ -12,21 +12,21 @@ function LogInCardPage() {
             </div>
 
             <div className={LogInCardPagecss.LoginFormContainer}>
-            <div className={LogInCardPagecss.LoginForm}>
-                <form>
-                    <div>
-                        <label>Username*</label>
-                        <input type="text" placeholder="username" />
-                    </div>
-                    <div>
-                        <label>Password*</label>
-                        <input type="password" placeholder="password" />
-                    </div>
-                </form>
+                <div className={LogInCardPagecss.LoginForm}>
+                    <form>
+                        <div>
+                            <label>Username*</label>
+                            <input type="text" placeholder="username" />
+                        </div>
+                        <div>
+                            <label>Password*</label>
+                            <input type="password" placeholder="password" />
+                        </div>
+                    </form>
 
-                <Button variant="" className="">
-                    Login
-                </Button>
+                    <Button variant="" className="">
+                        Login
+                    </Button>
                 </div>
             </div>
         </div>
