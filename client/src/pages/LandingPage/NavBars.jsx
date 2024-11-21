07f2b8b47fd6f225ba9a5f5b7785b26be70867f4
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-21 13:48:37 +0800

    Subject: Update

--- client/src/pages/LandingPage/NavBars.jsx
+++ client/src/pages/LandingPage/NavBars.jsx
@@ -5,13 +5,13 @@ function NavBars() {
     return (
         <Navbar expand="lg" className={LandingPagecss.Navbar}>
                 <Container>
-                <Navbar.Brand href="#home" style={{ color: '#4b4b4b' }}>REYES ELECTRONICS</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Brand href="#home" style={{ color: '#4b4b4b'}}>REYES ELECTRONICS</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                         <Nav.Link href="#home" style={{ color: '#4b4b4b' }}>Home</Nav.Link>
                         <Nav.Link href="#Aboutus" style={{ color: '#4b4b4b' }}>About us</Nav.Link>
-                        <NavDropdown title={<span style={{ color: '#4b4b4b' }}>User Type</span>} id="basic-nav-dropdown">
+                        <NavDropdown title={<span style={{ color: '#4b4b4b' }}>User Type</span>} id="basic-nav-dropdown" >
                                 <NavDropdown.Item href="LoginPage">Admin Portal</NavDropdown.Item>
                                 <NavDropdown.Item href="SLogin">Staff Portal</NavDropdown.Item>
                                 <NavDropdown.Divider />
