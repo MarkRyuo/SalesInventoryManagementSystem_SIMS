df64bbebf1c20334d56c6c7c97119530b8a6c68b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-21 15:45:01 +0800

    Subject: Update

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -13,7 +13,7 @@ function LandingPage() {
             <Container fluid className="m-0 p-0">
                 <main className={LandingPagecss.main}>
                     <div className={LandingPagecss.heroContainer}>
-                        <div className={LandingPagecss.heroSection}> {/* Main Component */}
+                        <div className={LandingPagecss.heroSection}> {/* Main Component Hero Section */}
 
                             <div className={LandingPagecss.heroText}> {/* Child */}
                                 <p>Power Your Life with <span>REYES ELECTRONICS</span></p>
