173b1e61fcd7ec3c4f0f073202e9209ae5be50dc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-23 20:49:45 +0800

    Subject: Update

--- client/src/services/ReportService.jsx
+++ client/src/services/ReportService.jsx
@@ -1,4 +1,4 @@
-import { getDatabase, ref, set, get, update, remove, onValue, push, runTransaction } from 'firebase/database';
+import { getDatabase, ref, set, get, onValue} from 'firebase/database';
 
 
 // Function to fetch a product by barcode
