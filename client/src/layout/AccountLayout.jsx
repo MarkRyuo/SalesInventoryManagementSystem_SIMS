7d0474c21cf39ebffb4aaba3f00b32c854d71a37
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-24 20:39:58 +0800

    Subject: Update

--- client/src/layout/AccountLayout.jsx
+++ client/src/layout/AccountLayout.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 import { NavDashboard } from "../components/NavBar/NavDashboard"
 import { Container } from "react-bootstrap"
-import MainLayoutscss from './MainLayout.module.scss'
+import AccountLayoutscss from './SCSS/AccountLayout.module.scss'
 
 export const AccountLayout = ({ children }) => {
 
     return (
-        <Container fluid className={MainLayoutscss.mainLayoutContainer}>
+        <Container fluid className={AccountLayoutscss.mainLayoutContainer}>
             <NavDashboard />
             <Container fluid='lg' className='p-0'>
                 {children}
