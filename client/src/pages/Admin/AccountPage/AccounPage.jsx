7ce1a4645a25913a3a0a974e56e1f4ca6cfe7d5b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-24 21:20:30 +0800

    Subject: Update

--- client/src/pages/Admin/AccountPage/AccounPage.jsx
+++ client/src/pages/Admin/AccountPage/AccounPage.jsx
@@ -5,36 +5,34 @@ import { AiFillProduct } from "react-icons/ai";
 import { Container } from "react-bootstrap";
 import { FaHistory } from "react-icons/fa";
 import { PiHandArrowUpBold } from "react-icons/pi";
-import AccountPagescss from './AccountPage.module.scss'
-
+import AccountPagescss from './AccountPage.module.scss';
 import { LiaQrcodeSolid } from "react-icons/lia";
 import MyAccount from '../../../components/Account/MyAccount';
-
 
 // eslint-disable-next-line react/prop-types
 function ProductNavbarTabs({ setActiveTab }) {
     return (
-        <Nav justify variant="tabs" defaultActiveKey="/MyAccount" className="" onSelect={setActiveTab}>
+        <Nav variant="tabs" onSelect={setActiveTab}>
             <Nav.Item className={AccountPagescss.mainTabs}>
-                <Nav.Link eventKey="/">
+                <Nav.Link eventKey="/MyAccount">
                     <AiFillProduct size={20} />
                     <p className='m-0 p-0'>My Account</p>
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item className={AccountPagescss.mainTabs}>
-                <Nav.Link eventKey="/">
+                <Nav.Link eventKey="/Profile">
                     <FaHistory size={20} />
                     <p className='m-0 p-0'>Profile</p>
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item className={AccountPagescss.mainTabs}>
-                <Nav.Link eventKey="/">
+                <Nav.Link eventKey="/StaffComp">
                     <PiHandArrowUpBold size={20} />
-                    <p className='m-0 p-0'></p>
+                    <p className='m-0 p-0'>Re-ordering</p>
                 </Nav.Link>
             </Nav.Item>
             <Nav.Item className={AccountPagescss.mainTabs}>
-                <Nav.Link eventKey="/">
+                <Nav.Link eventKey="/SetQrcode">
                     <LiaQrcodeSolid size={20} />
                     <p className='m-0 p-0'>Set Qrcode</p>
                 </Nav.Link>
@@ -44,31 +42,31 @@ function ProductNavbarTabs({ setActiveTab }) {
 }
 
 export const ProductPage = () => {
-    const [activeTab, setActiveTab] = useState('/Product');
+    const [activeTab, setActiveTab] = useState('/MyAccount');
 
     return (
         <MainLayout>
             <Container className={AccountPagescss.MyAccount}>
                 <ProductNavbarTabs setActiveTab={setActiveTab} />
                 <div className="ProductContent">
-                    {activeTab === '/' && (
+                    {activeTab === '/MyAccount' && (
                         <div className="Product">
                             <MyAccount />
                         </div>
                     )}
-                    {activeTab === '/AdminTransactionHistory' && (
+                    {activeTab === '/Profile' && (
                         <div className="Unknown">
-
+                            {/* Add content for Profile */}
                         </div>
                     )}
                     {activeTab === '/ReOrdering' && (
                         <div className="Unknown">
-
+                            {/* Add content for Re-ordering */}
                         </div>
                     )}
                     {activeTab === '/SetQrcode' && (
                         <div className="Unknown">
-
+                            {/* Add content for Set Qrcode */}
                         </div>
                     )}
                 </div>
