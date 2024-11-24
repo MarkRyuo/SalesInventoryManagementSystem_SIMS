04dab4c7ecea5b3dfb7ea3ae95b3cd1e9a93060f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-24 18:29:27 +0800

    Subject: Update

--- client/src/components/Charts/ReportChart/ReportChart2.jsx
+++ client/src/components/Charts/ReportChart/ReportChart2.jsx
@@ -75,7 +75,7 @@ function ReportChart2() {
 
     return (
         <div className={ReportChartcss.containerChart2}>
-                <p className="m-0 p-0">Total Sales</p>
+            <p className="m-0 p-0">Total Sales</p>
             <div className={ReportChartcss.contentChart}>
                 <p className="m-0 p-2">{`₱${totalSales.toFixed(2)}`}</p>
                 <p className="m-0 pb-2">Filtered by date range</p>
