c07029226200f44c1e78e84cb49f8c9f09f94e3a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-24 20:24:19 +0800

    Subject: Update

--- client/src/components/Account/MyAccount.jsx
+++ client/src/components/Account/MyAccount.jsx
@@ -1,15 +1,10 @@
-import Sidebar from './SideBar'; // Import Sidebar component
 
-const MyAccount = () => {
+function MyAccount() {
     return (
-        <div style={{ display: 'flex' }}>
-            <Sidebar />
-            <div style={{ marginLeft: '250px', padding: '20px' }}>
-                <h2>My Account Settings</h2>
-                <p>Here you can manage your account details.</p>
-            </div>
+        <div>
+            
         </div>
-    );
-};
+    )
+}
 
 export default MyAccount;
--- client/src/components/Account/SideBar.jsx
+++ client/src/components/Account/SideBar.jsx
@@ -1,63 +1,10 @@
-import { Nav, Navbar } from 'react-bootstrap';
-import { FaUser, FaCogs, FaBell, FaLock, FaRegAddressBook } from 'react-icons/fa'; // You can use FontAwesome icons
 
 const Sidebar = () => {
     return (
         <div style={sidebarStyles}>
-            <Navbar variant="dark" className="flex-column" style={navbarStyles}>
-                <Nav className="flex-column w-100">
-                    <Nav.Item>
-                        <Nav.Link href="#account">
-                            <FaUser style={iconStyles} />
-                            My Account
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="#settings">
-                            <FaCogs style={iconStyles} />
-                            Settings
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="#notifications">
-                            <FaBell style={iconStyles} />
-                            Notifications
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="#privacy">
-                            <FaLock style={iconStyles} />
-                            Privacy & Safety
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link href="#contacts">
-                            <FaRegAddressBook style={iconStyles} />
-                            Connections
-                        </Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </Navbar>
+            
         </div>
     );
-};
-
-const sidebarStyles = {
-    height: '100vh',
-    width: '250px',
-    backgroundColor: '#2f3136', // Dark theme
-    color: 'white',
-    display: 'flex',
-    flexDirection: 'column',
-};
-
-const navbarStyles = {
-    flex: 1,
-    paddingTop: '20px',
-};
-
-const iconStyles = {
-    marginRight: '10px',
 };
 
 export default Sidebar;
