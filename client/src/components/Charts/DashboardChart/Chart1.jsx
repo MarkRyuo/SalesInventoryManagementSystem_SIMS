37eddbdc54f01a29f9742856ace0c19c3c4d6713
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-24 13:19:00 +0800

    Subject: Update

--- client/src/components/Charts/DashboardChart/Chart1.jsx
+++ client/src/components/Charts/DashboardChart/Chart1.jsx
@@ -30,16 +30,16 @@ function Chart1() {
         <div className={Chartcss.containerChart1}>
             {/* <p>From the running {timeRange.toLowerCase()}</p> */}
             <h5>Stock Quantity</h5>
-
-            <div className={Chartcss.contentChart1}>
-                <p>{quantity}</p>
-            </div>
             <select onChange={(e) => handleTimeRangeChange(e.target.value)} value={timeRange}>
                 <option value="Today">Today</option>
                 <option value="Week">Week</option>
                 <option value="Month">Month</option>
                 <option value="Year">Year</option>
             </select>
+
+            <div className={Chartcss.contentChart1}>
+                <p>{quantity}</p>
+            </div>
         </div>
 
     );
