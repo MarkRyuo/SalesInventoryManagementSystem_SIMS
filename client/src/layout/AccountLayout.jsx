5af4190edaf04b287828e13763c66a930e1d0860
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-24 20:42:07 +0800

    Subject: Update

--- client/src/layout/AccountLayout.jsx
+++ client/src/layout/AccountLayout.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { NavDashboard } from "../components/NavBar/NavDashboard"
 import { Container } from "react-bootstrap"
 import AccountLayoutscss from './SCSS/AccountLayout.module.scss'
