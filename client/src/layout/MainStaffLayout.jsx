66557056f9f7174e2037d9aa6c61936da8c3ae58
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-25 06:50:16 +0800

    Subject: Update

--- client/src/layout/MainStaffLayout.jsx
+++ client/src/layout/MainStaffLayout.jsx
@@ -9,7 +9,7 @@ function MainStaffLayout({children}) {
     return (
         <Container fluid style={{ margin: "0px", padding: "0px" }} className={MainStaffLayoutScss.mainLayoutStaff}>
 
-            <div className="Staff-container">
+            <div className="Staff-container" style={{ height: '100vh' }}>
                 <NavbarStaffDashboard />
                 <Container fluid='lg' className="p-0 mt-5"> {/**Content */}
                     {children}
