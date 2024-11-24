1a6b716db0c0616db50f73d35e92bbde44b1e449
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-24 18:43:55 +0800

    Subject: Update

--- client/src/components/Charts/ReportChart/ReportChart4.jsx
+++ client/src/components/Charts/ReportChart/ReportChart4.jsx
@@ -67,8 +67,7 @@ function LowStockReport() {
         <div>
             {/* Low Stock Overview */}
             <div className={ReportChartcss.containerChart4}>
-                    <p className="m-0 p-0">Low Stock Overview</p>
-                <div className={ReportChartcss.contentChart}>
+                <h1 className="m-0 p-0">Low Stock Overview</h1>
                     <p className="m-0 p-2">{lowStockData.length}</p> {/* Dynamic low stock count */}
                     <p className="m-0 pb-2">From {startDate} to {endDate}</p> {/* Dynamic date range */}
                 </div>
