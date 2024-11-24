96f060992b1047bd8a7bcab5324bfb3ed6739e83
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-24 23:17:06 +0800

    Subject: Update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -35,6 +35,7 @@ import ReOrdering from './pages/Admin/ProductPage/ReOrdering.jsx';
 import SetQrcode from './pages/Admin/ProductPage/SetQrcode.jsx' ;
 import ReportPage from './pages/Admin/ReportPage/ReportPage.jsx';
 import AccountPage from './pages/Admin/AccountPage/AccountPage.jsx'
+import ProfileComp from './components/Account/ProfileComp.jsx';
 
 const router = createBrowserRouter([
   { //? ROOT
