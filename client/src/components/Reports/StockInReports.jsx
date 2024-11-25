52461cc37766bdef869526091d2a59e5f20fd11e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-25 16:50:16 +0800

    Subject: Update

--- client/src/components/Reports/StockInReports.jsx
+++ client/src/components/Reports/StockInReports.jsx
@@ -3,7 +3,8 @@ function StockInReports() {
     return (
         <div>
             <div>
-                <h1></h1>
+                <h1>Stock In Report</h1>
+                {/* Filtering Date Start to End Note: if the same date filtered, filtered that of course*/}
             </div>
         </div>
     )
