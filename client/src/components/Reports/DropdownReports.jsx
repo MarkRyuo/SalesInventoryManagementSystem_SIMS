f2a21ed4d1284a0a22c6a5d04c7d5bab3517844c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-25 22:34:54 +0800

    Subject: Update

--- client/src/components/Reports/DropdownReports.jsx
+++ client/src/components/Reports/DropdownReports.jsx
@@ -1,12 +1,11 @@
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import { Link } from 'react-router-dom';
+import { DropdownButton, Dropdown } from 'react-bootstrap'; import { Link } from 'react-router-dom';
 
 function DropdownReports() {
     return (
-        <DropdownButton id="dropdown-basic-button" title="Reports" variant='' className=''>
-            <Link to={"/StockInReports"}>Stock In Report</Link>
-            <Link to={"/LowStockReports"}>Low Stock Report</Link>
-            <Link to={"/TotalSalesReports"}>Total Sales Report</Link>
+        <DropdownButton id="dropdown-basic-button" title="Reports" variant="secondary">
+            <Dropdown.Item as={Link} to="/StockInReports">Stock In Report</Dropdown.Item>
+            <Dropdown.Item as={Link} to="/LowStockReports">Low Stock Report</Dropdown.Item>
+            <Dropdown.Item as={Link} to="/TotalSalesReports">Total Sales Report</Dropdown.Item>
         </DropdownButton>
     )
 }
