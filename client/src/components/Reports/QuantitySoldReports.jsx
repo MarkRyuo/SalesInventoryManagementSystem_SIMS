7612f530b1bc74df5799c486070d8e4461514394
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-25 20:24:28 +0800

    Subject: Update

---
+++ client/src/components/Reports/QuantitySoldReports.jsx
@@ -0,0 +1,21 @@
+
+function QuantitySoldReports() {
+    return (
+        <div>
+
+            <div>
+                <h1>Quantity Sold</h1>
+                {/* Filter date (Start to End) */}
+                {/* Dropdown  button for file type (Pdf and xlsx) */}
+            </div>
+
+            <div>
+                {/* Table */}
+                {/* Inside the table is the filtered data */}
+            </div>
+
+        </div>
+    )
+}
+
+export default QuantitySold;
