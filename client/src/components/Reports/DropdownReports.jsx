b728a9db90b10e2d2db6ae9702f2556a40c41719
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-25 16:57:31 +0800

    Subject: Update

--- client/src/components/Reports/DropdownReports.jsx
+++ client/src/components/Reports/DropdownReports.jsx
@@ -4,7 +4,7 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 function DropdownReports() {
     return (
         <DropdownButton id="dropdown-basic-button" title="Reports" variant=''>
-            <Dropdown.Item href="#">Stock In Report</Dropdown.Item>
+            <Dropdown.Item href="/StockInReports">Stock In Report</Dropdown.Item>
             <Dropdown.Item href="/LowStockReports">Low Stock Report</Dropdown.Item>
             <Dropdown.Item href="#">Total Sales Report</Dropdown.Item>
             <Dropdown.Item href="#">Quantity Sold Report</Dropdown.Item>
