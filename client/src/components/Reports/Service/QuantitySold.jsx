7cb3335fdd18a6d280282ddeda5fee8d52676e32
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-25 20:23:55 +0800

    Subject: Update

--- client/src/components/Reports/Service/QuantitySold.jsx
+++ client/src/components/Reports/Service/QuantitySold.jsx
@@ -6,7 +6,12 @@ function QuantitySold() {
         <div>
             <h1>Quantity Sold</h1>
             {/* Filter date (Start to End) */}
-            {/* Dropdown  button for file type (Pdf and xlsx*/}
+            {/* Dropdown  button for file type (Pdf and xlsx) */}
+        </div>
+
+        <div>
+            {/* Table */}
+            {/* Inside the table is the filtered data */}
         </div>
 
         </div>
