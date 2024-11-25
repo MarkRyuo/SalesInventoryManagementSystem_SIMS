de0e4187da39f69178df31d9bd9179bdde75e099
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-25 20:21:40 +0800

    Subject: Update

--- client/src/components/Reports/Service/QuantitySold.jsx
+++ client/src/components/Reports/Service/QuantitySold.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 function QuantitySold() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
-export default QuantitySold
+export default QuantitySold ;
