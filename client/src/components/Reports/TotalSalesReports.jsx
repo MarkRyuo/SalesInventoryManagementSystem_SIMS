2119c1d5ae443f61ab252dd6e81293dabf1988a3
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-25 18:42:07 +0800

    Subject: Update

--- client/src/components/Reports/TotalSalesReports.jsx
+++ client/src/components/Reports/TotalSalesReports.jsx
@@ -75,7 +75,9 @@ function TotalSalesReports() {
     }, [startDate, endDate]);
 
     return (
-        <MainLayout
+        <MainLayout>
+            
+        </MainLayout>
 
         <div>
             <h1>Total Sales</h1>
