d666fc69fed8846d411451dc00397b35c76b58ca
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-25 21:23:31 +0800

    Subject: Update

--- client/src/components/Reports/TotalSalesReports.jsx
+++ client/src/components/Reports/TotalSalesReports.jsx
@@ -3,8 +3,19 @@
 function TotalSalesReports() {
     return (
         <div>
+            <div>
+                <h1>Total Sales</h1>
+                {/*Filter date (Start to End)  */}
+                {/* Dropdown button for file (Pdf and xlsx) */}
+            </div>
+
+            <div>
+                {/* Table */}
+                {/* Inside the table is the filtered data */}
+            </div>
 
         </div>
+
     )
 }
 
