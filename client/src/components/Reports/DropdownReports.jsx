98a91880a1281c017f27dbd3e2b23eff37931fbd
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-25 15:34:35 +0800

    Subject: Update

--- client/src/components/Reports/DropdownReports.jsx
+++ client/src/components/Reports/DropdownReports.jsx
@@ -1,9 +1,13 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 function DropdownReports() {
     return (
-        <div>
-
-        </div>
+        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </DropdownButton>
     )
 }
 
