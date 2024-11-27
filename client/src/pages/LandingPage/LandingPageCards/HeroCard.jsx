73c20807e55b3c54de9033f7ea6d3d4c01e1a5f2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-27 14:09:48 +0800

    updatye herE

--- client/src/pages/LandingPage/LandingPageCards/HeroCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/HeroCard.jsx
@@ -4,8 +4,8 @@ import HeroCardScss from './SCSS/HeroCard.module.scss';
 function HeroCard() {
     return (
         <div className={HeroCardScss.heroSection}> {/* Main Component Hero Section */}
-            <h1>Power Your Life with 
-                <span>REYES ELECTRONICS</span>
+            <h1>Power Your Life with  
+                <span> REYES ELECTRONICS</span>
             </h1>
             <h3>Elevate Your Experience with Top-Tier Electronic Products.</h3>
 
