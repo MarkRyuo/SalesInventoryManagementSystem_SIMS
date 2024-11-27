3ea8f2af454dc3c4d70a4537143fd6ebbad7c393
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-27 13:42:21 +0800

    updatye herE

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,8 @@
 import { Container} from "react-bootstrap"
 import NavBars from "./NavBars"
 import LandingPageScss from './LandingPage.module.scss';
+import HeroCard from './LandingPageCards/HeroCard' ;
+
 
 function LandingPage() {
 
@@ -8,7 +10,9 @@ function LandingPage() {
         <Container fluid className="m-0 p-0" id="home">
             <NavBars />
             <Container fluid className={LandingPageScss.MainContainer}>
-
+                <div>
+                    <HeroCard />
+                </div>
             </Container>
         </Container>
     )
