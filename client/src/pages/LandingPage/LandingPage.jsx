e20cba1d70727c86c6e0bfd16e603ac25bf49ec8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-27 13:43:27 +0800

    updatye herE

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import { Container} from "react-bootstrap"
 import NavBars from "./NavBars"
 import LandingPageScss from './LandingPage.module.scss';
-import HeroCard from './LandingPageCards/HeroCard' ;
+import HeroCard from './LandingPageCards/HeroCard';
 
 
 function LandingPage() {
