fb2503c34caa8556506af1cd91f44e28df205622
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-27 14:38:16 +0800

    updatye herE

--- client/src/pages/LandingPage/LandingPageCards/HeroCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/HeroCard.jsx
@@ -1,24 +1,20 @@
 import { Button } from "react-bootstrap";
 import HeroCardScss from './SCSS/HeroCard.module.scss';
+import { GoGlobe } from "react-icons/go";
 
 function HeroCard() {
     return (
         <div className={HeroCardScss.heroSection}> {/* Main Component Hero Section */}
-            <h1>Power Your Life with  
+            <h1>Power Your Life with
                 <span> REYES ELECTRONICS</span>
             </h1>
             <h3>Elevate Your Experience with Top-Tier Electronic Products.</h3>
 
-            <div className={HeroCardScss.HeroButtons}> {/* Child */}
-                <Button variant='' href="#footer" id="btn" className={HeroCardScss.btnExplore}>
-                    Explore
-                </Button>
-                <Button variant='' href="#Logins" id="btn" className={HeroCardScss.btnLogin}>
-                    Login
-                </Button>
-            </div>
-
+            <Button variant='' href="#footer" id="btn" className={HeroCardScss.btnExplore}>
+                Explore <GoGlobe/>
+            </Button>
         </div>
+
     )
 }
 
