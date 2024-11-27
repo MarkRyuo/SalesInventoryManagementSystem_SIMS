f6b28c753077de240ad831f900abca54b2bd904b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-27 13:39:29 +0800

    updatye herE

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,13 @@
 import { Container} from "react-bootstrap"
 import NavBars from "./NavBars"
+import LandingPageScss from './LandingPage.module.scss';
 
 function LandingPage() {
 
     return (
         <Container fluid className="m-0 p-0" id="home">
             <NavBars />
-            <Container fluid className="m-0 p-0" >
+            <Container fluid className={LandingPageScss.MainContainer}>
 
             </Container>
         </Container>
