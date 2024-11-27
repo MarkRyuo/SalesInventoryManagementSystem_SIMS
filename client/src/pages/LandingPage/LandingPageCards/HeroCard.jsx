d988695fdf63b5ca841be77e667580bf5f7beac8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-27 13:52:34 +0800

    updatye herE

--- client/src/pages/LandingPage/LandingPageCards/HeroCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/HeroCard.jsx
@@ -1,8 +1,36 @@
+import { Button } from "react-bootstrap";
+import HeroCardScss from './SCSS/HeroCard.module.scss' ;
 
 function HeroCard() {
     return (
         <div>
-            <h1>Hero Card</h1>
+            <div className={LandingPagecss.heroContainer}>
+
+                <div className={LandingPagecss.heroSection}> {/* Main Component Hero Section */}
+
+                    <div className={LandingPagecss.heroText}> {/* Child */}
+                        <p>Power Your Life with <span>REYES ELECTRONICS</span></p>
+                        <p className="mb-3">Elevate Your Experience with Top-Tier Electronic Products.</p>
+                        <Button variant='' href="#footer" id="btn" className={LandingPagecss.Btn}>
+                            <span><CiGlobe /></span> Explore
+                        </Button>
+                        <Button variant='' href="#Logins" id="btn" className={LandingPagecss.Btn}>
+                            <span><TbLogin2 /></span> Login
+                        </Button>
+                    </div>
+
+                    <div className={LandingPagecss.heroImage}> {/* Hero Image Logo */}
+                        <img src="/Reyes_Electronics_LogoBg.png" />
+                    </div>
+                </div>
+                <div className={LandingPagecss.partners}> {/* Floating Partners */}
+                    <img src="https://www.fireflyelectric.com/wp-content/uploads/2021/05/FELCO-logo-2021-cropped-1.png" />
+                    <img src="https://www.mosca-elektronik.de/images/template/logo_mosca.png" />
+                    <img src="https://www.chinafsl.com/en/templates/specialty/images/logo.png" />
+                    <img src="https://www.tapo.com/res/images/home/tapo-logo.png" />
+                </div>
+
+            </div>
         </div>
     )
 }
