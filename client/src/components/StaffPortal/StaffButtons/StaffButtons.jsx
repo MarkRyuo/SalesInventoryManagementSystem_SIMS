03b1c070c7b73edf1c4f2be3b783cd6fe4c28678
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-29 19:49:00 +0800

    update herE

--- client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
+++ client/src/components/StaffPortal/StaffButtons/StaffButtons.jsx
@@ -1,38 +1,8 @@
 /* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
 import StaffButtonsScss from './StaffButtons.module.scss' ;
 
 function StaffButtons({ buttons }) {
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    const getStyle = () => {
-        // Default styles for large screens
-        let style = {
-            display: "flex",
-            flexDirection: "column",
-            padding: 30,
-            boxShadow: '2px 2px 5px #e2dfdf',
-            borderRadius: 20,
-            width: 250,
-            height: 125,
-            justifyContent: 'center',
-            color: 'black',
-            textDecoration: 'none',
-            borderLeft: '1px solid #92E3B8',
-            boxSizing: 'border-box',
-        };
-
-        return style;
-    };
 
     return (
         <>
@@ -40,7 +10,7 @@ function StaffButtons({ buttons }) {
                 <Link
                     key={button.id}
                     to={button.path}
-                    style={getStyle()}
+                    className={StaffButtonsScss.MainStaffButtons}
                 >
                     <span className='text-center' style={{ color: '#92E3B8' }}>{button.btnIcon}</span>
                     <span className='text-center'>{button.btnName}</span>
