c675b3d4eb5ae939369c4aa7f65c3f4d2f5c4160
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-29 22:11:25 +0800

    update herE

--- client/src/pages/LandingPage/LandingPageCards/Accordions.jsx
+++ client/src/pages/LandingPage/LandingPageCards/Accordions.jsx
@@ -18,11 +18,7 @@ function Accordions() {
                         <Accordion.Body>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
+                            minim veniam.
                         </Accordion.Body>
                     </Accordion.Item>
                 </Accordion>
