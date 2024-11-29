af09f36013ea5ff801b06667acc995037d25c04d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-11-29 22:41:40 +0800

    update herE

--- client/src/pages/LandingPage/LandingPageCards/AccordionsCard.jsx
+++ client/src/pages/LandingPage/LandingPageCards/AccordionsCard.jsx
@@ -22,6 +22,22 @@ function AccordionsCard() {
                         minim veniam.
                     </Accordion.Body>
                 </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Accordion Item #3</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam.
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                    <Accordion.Header>Accordion Item #4</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam.
+                    </Accordion.Body>
+                </Accordion.Item>
             </Accordion>
 
 
